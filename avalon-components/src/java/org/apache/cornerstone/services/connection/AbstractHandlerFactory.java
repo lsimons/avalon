a58cba77f8034719aec281528cd7d2335f72d029
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2001-02-25 13:26:02 +0000

    Blah - lets put it in the right package.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@10773 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-components/src/java/org/apache/cornerstone/services/connection/AbstractHandlerFactory.java
+++ avalon-components/src/java/org/apache/cornerstone/services/connection/AbstractHandlerFactory.java
@@ -5,7 +5,7 @@
  * version 1.1, a copy of which has been included with this distribution in
  * the LICENSE file.
  */
-package org.apache.astech.handlers;
+package org.apache.cornerstone.services.connection;
 
 import org.apache.avalon.AbstractLoggable;
 import org.apache.avalon.Component;
