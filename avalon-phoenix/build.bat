10d5c55d6a1916885b524a9539846dca5f0cba96
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2001-04-02 06:48:21 +0000

    Cleaned build scripts.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@10914 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-phoenix/build.bat
+++ avalon-phoenix/build.bat
@@ -5,15 +5,10 @@ echo Phoenix Build System
 echo --------------------
 
 set ANT_HOME=tools
-set BUILD_FILE=build.xml
 
-set LOCALCLASSPATH=
-for %%i in (lib\*.jar) do call %ANT_HOME%\bin\lcp.bat %%i
-set CLASSPATH=%LOCALCLASSPATH%
-set LOCALCLASSPATH=
+set CP=%CLASSPATH%
+set CLASSPATH=lib\xerces_1_2_3.jar
 
-:runAnt
 %ANT_HOME%\bin\ant.bat -logger org.apache.tools.ant.NoBannerLogger -emacs %1 %2 %3 %4 %5 %6 %7 %8
-set BUILD_FILE=
-set ANT_HOME=
-set CLASSPATH=
+
+set CLASSPATH=%CP%
--- avalon-phoenix/build.sh
+++ avalon-phoenix/build.sh
@@ -4,17 +4,14 @@ echo
 echo "Phoenix Build System"
 echo "--------------------"
 
-export CLASSPATH=`echo $PWD/lib/*.jar | tr ' ' ':'`
+export CP=$CLASSPATH
+export CLASSPATH=lib/xerces_1_2_3.jar
 
 chmod u+x ./tools/bin/antRun
 chmod u+x ./tools/bin/ant
 
-export PROPOSAL=""
-
 unset ANT_HOME
 
-if [ "$1" = "proposal" ]; then
-    export PROPOSAL="-buildfile proposal/make/proposal.xml"
-fi
+$PWD/tools/bin/ant -logger org.apache.tools.ant.NoBannerLogger -emacs $@ 
 
-$PWD/tools/bin/ant -logger org.apache.tools.ant.NoBannerLogger -emacs $PROPOSAL $@ 
+export CLASSPATH=$CP
