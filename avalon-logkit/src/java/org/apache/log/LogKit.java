723ea3b9de303d0b3eabc837996e4ade6d26b0a2
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2001-04-11 09:49:38 +0000

    Removed some archaic cruft.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@10925 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-logkit/src/java/org/apache/log/LogKit.java
+++ avalon-logkit/src/java/org/apache/log/LogKit.java
@@ -7,12 +7,6 @@
  */
 package org.apache.log;
 
-import java.io.File;
-import java.lang.reflect.*;
-import java.util.*;
-import org.apache.log.output.*;
-import org.apache.log.output.DefaultOutputLogTarget;
-
 /**
  * The LogKit provides the access to static methods to
  * manipulate the logging sub-system
@@ -23,62 +17,6 @@ public final class LogKit
 {
     protected static final ThreadLocal     c_context                = new ThreadLocal();
     protected static final LogEngine       c_engine                 = new LogEngine();
-
-    /*
-      //Need to add this in at LogManager level rather than here ....
-      public static Logger createLogger(String name, URL destination, Priority.Enum priority)
-      {
-
-      // create the new category. If it already exist return the cached one.
-      Category category = createCategory(name, priority );
-      // create the new target. if already exist uses the chached one.
-      // the target name is destination based so if a caller ask for a
-      // log name but aiming to the same destination, the same target is used.
-      LogTarget logTarget = createLogTarget("" + destination, destination);
-      LogTarget logTargets[] = new LogTarget[] { logTarget };
-      LogKit.createLogger( category, logTargets );
-      return LogKit.getLoggerFor(name);
-      }
-
-      private static LogTarget createLogTarget(String name, URL destination) {
-
-      LogTarget logTarget = getLogTarget(name);
-      if (logTarget == null) {
-      String protocol = destination.getProtocol();
-      String logTargetClassName = null;
-
-      // bad hack here... need to read from Properties?
-      if (protocol.equals("file")) {
-      logTarget = new FileOutputLogTarget();
-      } else if (protocol.equals("null")) {
-      logTarget = new NullOutputLogTarget();
-      } else {
-      logTarget = new GenericOutputLogTarget();
-//                logTarget = c_defaultLogTarget;
-}
-//            if (protocol.equals("file")) {
-//                logTargetClassName = "FileOutputLogTarget";
-//            } else if (protocol.equals("null")) {
-//                logTargetClassName = "NullOutputLogTarget";
-//            }
-
-//            if (logTargetClassName != null) {
-//                try {
-//                    logTarget = (LogTarget) Class.forName( "org.apache.log.output." + logTargetClassName ).newInstance();
-//                } catch(Exception e ) {
-//                    logTarget = c_defaultLogTarget;
-//                }
-//                logTarget.setDestination(destination);
-//            } else {
-//                logTarget = c_defaultLogTarget;
-//            }/
-logTarget.setDestination(destination);
-LogKit.addLogTarget( name, logTarget );
-}
-return logTarget;
-}
-    */
-
 
     /**
      * Retrieve the default log engine which all the static methods in LogKit deferr to.
