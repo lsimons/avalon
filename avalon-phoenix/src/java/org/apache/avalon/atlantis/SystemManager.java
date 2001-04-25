fef0b2ed846f583d096fb24ee89144886a7d5e6a
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2001-04-25 03:17:04 +0000

    Added javadoc and methods to interface to reflect basic management capabilities.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@11035 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-phoenix/src/java/org/apache/avalon/atlantis/SystemManager.java
+++ avalon-phoenix/src/java/org/apache/avalon/atlantis/SystemManager.java
@@ -23,4 +23,40 @@ import org.apache.avalon.component.Component;
 public interface SystemManager
     extends Component, Initializable, Startable, Stoppable, Disposable
 {
+    /**
+     * Register an object for management.
+     * The object is exported through some management scheme
+     * (typically JMX) and the management is restricted
+     * to the interfaces passed in as a parameter to method.
+     *
+     * @param name the name to register object under
+     * @param object the object
+     * @param interfaces the interfaces to register the component under
+     * @exception Exception if an error occurs. An error could occur if the object doesn't 
+     *            implement the interfaces, the interfaces parameter contain non-instance 
+     *            classes, the name is already registered etc.
+     */
+    void register( String name, Object object, Class[] interfaces )
+        throws Exception;
+
+    /**
+     * Register an object for management.
+     * The object is exported through some management scheme
+     * (typically JMX).
+     *
+     * @param name the name to register object under
+     * @param object the object
+     * @exception Exception if an error occurs such as name being already registered.
+     */
+    void register( String name, Object object )
+        throws Exception;
+
+    /**
+     * Unregister named object.
+     *
+     * @param name the name of object to unregister
+     * @exception Exception if an error occurs such as when no such object registered.
+     */
+    void unregister( String name )
+        throws Exception;
 }
