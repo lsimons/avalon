a55d11ab42d9df281ab7adea562434e02117aa45
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2001-04-25 01:08:05 +0000

    Made manager load mBeanServer and RMIAdaptor.
    The Adaptor is exported on vanilla TCP and exported via a name registry.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@11028 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-phoenix/src/java/org/apache/phoenix/engine/PhoenixManager.java
+++ avalon-phoenix/src/java/org/apache/phoenix/engine/PhoenixManager.java
@@ -9,6 +9,16 @@ package org.apache.phoenix.engine;
 
 import org.apache.avalon.atlantis.SystemManager;
 import org.apache.avalon.logger.AbstractLoggable;
+import org.apache.avalon.parameters.ParameterException;
+import org.apache.avalon.parameters.Parameterizable;
+import org.apache.avalon.parameters.Parameters;
+import org.apache.jmx.adaptor.RMIAdaptorImpl;
+import java.rmi.server.RemoteObject;
+import java.rmi.server.UnicastRemoteObject;
+import java.rmi.registry.Registry;
+import java.rmi.Remote;
+import java.rmi.registry.LocateRegistry;
+import javax.management.MBeanServer;
 
 /**
  * This component is responsible for managing phoenix instance.
@@ -19,25 +29,82 @@ import org.apache.avalon.logger.AbstractLoggable;
  */
 public class PhoenixManager
     extends AbstractLoggable
-    implements SystemManager
+    implements SystemManager, Parameterizable
 {
+    private Parameters      m_parameters;
+    private MBeanServer     m_mBeanServer;
+    private RMIAdaptorImpl  m_rmiAdaptor;
+    private Registry        m_rmiRegistry;
+
+    ///Name Adaptor registered with
+    private String          m_name;
+
+    public void parameterize( final Parameters parameters )
+        throws ParameterException
+    {
+        m_parameters = parameters;
+    }
+
     public void init()
         throws Exception
     {
+        m_mBeanServer = createMBeanServer();
+        m_rmiAdaptor = new RMIAdaptorImpl( m_mBeanServer );
+
+        //TODO: Register everything here or in embeddor???
     }
 
     public void start()
         throws Exception
     {
+        final int port = m_parameters.getParameterAsInteger( "manager-registry-port", 1111 );
+        m_name = m_parameters.getParameter( "manager-name", "Phoenix.JMXAdaptor" );
+        
+        m_rmiRegistry = LocateRegistry.createRegistry( port );
+        
+        final Remote exported = UnicastRemoteObject.exportObject( m_rmiAdaptor );
+        final Remote stub = RemoteObject.toStub( exported );
+        
+        //TODO: should this do a lookup and refuse to lauch if existing server registered???
+        m_rmiRegistry.bind( m_name, stub );
     }
 
     public void stop()
         throws Exception
     {
+        m_rmiRegistry.unbind( m_name );
+        UnicastRemoteObject.unexportObject( m_rmiAdaptor, true );
+
+        //TODO: How do you shutdown registry???
     }
 
     public void dispose()
         throws Exception
     {
+        //TODO: Unregister everything here or in embeddor???
+        m_rmiAdaptor = null;
+        m_mBeanServer = null;
+    }
+
+    /**
+     * Creates a new Manager. The mBeanServer it uses is determined from
+     * the Parameters's manager-mBeanServer-class variable.
+     */
+    private MBeanServer createMBeanServer() 
+        throws Exception
+    {
+        final String className = 
+            m_parameters.getParameter( "manager-mBeanServer-class", 
+                                       "org.apache.jmx.MBeanServerImpl" );
+
+        try
+        {
+            Thread.currentThread().setContextClassLoader( getClass().getClassLoader() );
+            return (MBeanServer)Class.forName( className ).newInstance();
+        }
+        catch( final Exception e )
+        {
+            throw new ParameterException( "Failed to create MBean Server of class " + className, e );
+        }
     }
 }
