6980d9eb5c43bf4e29e09ab56f973c505ee0b692
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2001-06-21 14:28:27 +0000

    set ANT_HOME var as old scripts used to
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@11229 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-phoenix/build.bat
+++ avalon-phoenix/build.bat
@@ -8,5 +8,6 @@ if not "%AVALON_TOOLS%"=="" goto runAnt
 set AVALON_TOOLS=..\jakarta-avalon\tools
 
 :runAnt
+set ANT_HOME=%AVALON_TOOLS%
 %AVALON_TOOLS%/bin/ant -logger org.apache.tools.ant.NoBannerLogger -emacs -Dtools.dir=%AVALON_TOOLS% %1 %2 %3 %4 %5 %6 %7 %8
 
