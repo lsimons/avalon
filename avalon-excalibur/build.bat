dead267be5b1f6ed6024610625bdc9705a9d69a3
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2001-07-19 07:41:49 +0000

    Initial import of build files.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@11350 13f79535-47bb-0310-9956-ffa450edef68

---
+++ avalon-excalibur/build.bat
@@ -0,0 +1,12 @@
+@echo off
+
+echo -----------------------------
+echo Avalon/Excalibur Build System
+echo -----------------------------
+
+if not "%AVALON_TOOLS%"=="" goto runAnt
+set AVALON_TOOLS=..\jakarta-avalon\tools
+
+:runAnt
+set ANT_HOME=%AVALON_TOOLS%
+%AVALON_TOOLS%/bin/ant -logger org.apache.tools.ant.NoBannerLogger -emacs -Dtools.dir=%AVALON_TOOLS% %1 %2 %3 %4 %5 %6 %7 %8
---
+++ avalon-excalibur/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo
+echo "Avalon/Excalibur Build System"
+echo "-----------------------------"
+
+if [ "$AVALON_TOOLS" = "" ] ; then
+    AVALON_TOOLS=../jakarta-avalon/tools
+fi
+
+chmod u+x $AVALON_TOOLS/bin/antRun
+chmod u+x $AVALON_TOOLS/bin/ant
+
+$AVALON_TOOLS/bin/ant -logger org.apache.tools.ant.NoBannerLogger -emacs -Dtools.dir=$AVALON_TOOLS $@ 
