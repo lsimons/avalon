a9005ba3725ac1b752b1192ba17e46b5b4f59e42
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2001-07-19 08:52:18 +0000

    Made it possible to use the same script for distributed versions and CVS version . This change is required as tools dir is only included in main AValon CVS now.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@11357 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-phoenix/build.bat
+++ avalon-phoenix/build.bat
@@ -1,13 +1,23 @@
 @echo off
 
-echo --------------------
-echo Phoenix Build System
-echo --------------------
+echo ------------
+echo Build System
+echo ------------
 
 if not "%AVALON_TOOLS%"=="" goto runAnt
-set AVALON_TOOLS=..\jakarta-avalon\tools
+
+if exist "..\jakarta-avalon\tools" set AVALON_TOOLS=..\jakarta-avalon\tools
+if exist "tools" set AVALON_TOOLS=tools
+
+if not "%AVALON_TOOLS%"=="" goto runAnt
+
+echo "Unable to locate tools directory at "
+echo "../jakarta-avalon/tools/ or tools/. "
+echo "Aborting."
+goto end
 
 :runAnt
 set ANT_HOME=%AVALON_TOOLS%
 %AVALON_TOOLS%/bin/ant -logger org.apache.tools.ant.NoBannerLogger -emacs -Dtools.dir=%AVALON_TOOLS% %1 %2 %3 %4 %5 %6 %7 %8
 
+:end
