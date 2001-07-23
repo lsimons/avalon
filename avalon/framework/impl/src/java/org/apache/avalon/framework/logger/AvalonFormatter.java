663b246bf87ef2ee2aa5187b30cfb3d99f93d30e
bloritsch <bloritsch@13f79535-47bb-0310-9956-ffa450edef68>
2001-07-23 14:13:44 +0000

    Deprecated AvalonFormatter, to place it in Framework.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@11411 13f79535-47bb-0310-9956-ffa450edef68

--- avalon/framework/impl/src/java/org/apache/avalon/framework/logger/AvalonFormatter.java
+++ avalon/framework/impl/src/java/org/apache/avalon/framework/logger/AvalonFormatter.java
@@ -5,7 +5,7 @@
  * version 1.1, a copy of which has been included with this distribution in
  * the LICENSE file.
  */
-package org.apache.log.format;
+package org.apache.avalon.framework.logger;
 
 import java.util.Date;
 import org.apache.avalon.framework.ExceptionUtil;
@@ -13,9 +13,6 @@ import org.apache.avalon.framework.ExceptionUtil;
 /**
  * This formatter extends PatternFormatter so that 
  * CascadingExceptions are formatted with all nested exceptions.
- *
- * @deprecated Use <code>org.apache.avalon.framework.logger.AvalonFormatter</code>
-               instead of this one.
  *
  * @author <a href="mailto:bloritsch@apache.org">Berin Loritsch</a>
  */
