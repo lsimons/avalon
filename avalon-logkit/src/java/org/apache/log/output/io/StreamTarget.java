06035f5777538ab6d2fc076d32dfaa358b18f3d2
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2001-07-30 15:38:50 +0000

    Starting to move file/stream related targets to separate package
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@11525 13f79535-47bb-0310-9956-ffa450edef68

---
+++ avalon-logkit/src/java/org/apache/log/output/io/StreamTarget.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included with this distribution in
+ * the LICENSE file.
+ */
+package org.apache.log.output.io;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import org.apache.log.Formatter;
+import org.apache.log.output.AbstractOutputTarget;
+
+/**
+ * A basic target that writes to an OutputStream.
+ *
+ * @author <a href="mailto:donaldp@apache.org">Peter Donald</a>
+ */
+public class StreamTarget
+    extends AbstractOutputTarget
+{
+    ///OutputStream we are writing to
+    private OutputStream   m_outputStream;
+
+    /**
+     * Constructor that writes to a stream and uses a particular formatter.
+     *
+     * @param outputStream the OutputStream to write to
+     * @param formatter the Formatter to use
+     */
+    public StreamTarget( final OutputStream outputStream, final Formatter formatter )
+    {
+        super( formatter );
+
+        if( null != outputStream )
+        {
+            setOutputStream( outputStream );
+            open();
+        }
+    }
+
+    /**
+     * Set the output stream.
+     * Close down old stream and write tail if appropriate.
+     *
+     * @param outputStream the new OutputStream
+     */
+    protected synchronized void setOutputStream( final OutputStream outputStream )
+    {
+        if( null == outputStream )
+        {
+            throw new NullPointerException( "outputStream property must not be null" );
+        }
+
+        m_outputStream = outputStream;
+    }
+
+    /**
+     * Abstract method that will output event.
+     *
+     * @param data the data to be output
+     */
+    protected void write( final String data )
+    {
+        //Cache method local version
+        //so that can be replaced in another thread
+        final OutputStream outputStream = m_outputStream;
+
+        if( null == outputStream )
+        {
+            error( "Attempted to write data '" + data + "' to Null OutputStream", null );
+            return;
+        }
+
+        try
+        {
+            //TODO: We should be able to specify encoding???
+            outputStream.write( data.getBytes() );
+            outputStream.flush();
+        }
+        catch( final IOException ioe )
+        {
+            error( "Error writing data '" + data + "' to OutputStream", ioe );
+        }
+    }
+
+    /**
+     * Shutdown target.
+     * Attempting to write to target after close() will cause errors to be logged.
+     *
+     */
+    public synchronized void close()
+    {
+        super.close();
+        shutdownStream();
+    }
+
+    /**
+     * Shutdown output stream.
+     */
+    protected synchronized void shutdownStream()
+    {
+        final OutputStream outputStream = m_outputStream;
+        m_outputStream = null;
+
+        try
+        {
+            if( null != outputStream )
+            {
+                outputStream.close();
+            }
+        }
+        catch( final IOException ioe )
+        {
+            error( "Error closing OutputStream", ioe );
+        }
+    }
+}
---
+++ avalon-logkit/src/java/org/apache/log/output/io/WriterTarget.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included with this distribution in
+ * the LICENSE file.
+ */
+package org.apache.log.output.io;
+
+import java.io.IOException;
+import java.io.Writer;
+import org.apache.log.Formatter;
+import org.apache.log.output.AbstractOutputTarget;
+
+/**
+ * This target outputs to a writer.
+ *
+ * @author <a href="mailto:donaldp@apache.org">Peter Donald</a>
+ */
+public class WriterTarget
+    extends AbstractOutputTarget
+{
+    /**
+     * @deprecated Accessing this variable in subclasses is no longer supported
+     *             and will become private in the future.
+     */
+    protected Writer             m_output;
+
+    /**
+     * Construct target with a specific writer and formatter.
+     *
+     * @param writer the writer
+     * @param formatter the formatter
+     */
+    public WriterTarget( final Writer writer, final Formatter formatter )
+    {
+        super( formatter );
+
+        if( null != writer )
+        {
+            setWriter( writer );
+            open();
+        }
+    }
+
+    /**
+     * Set the writer.
+     * Close down writer and write tail if appropriate.
+     *
+     * @param writer the new writer
+     */
+    protected synchronized void setWriter( final Writer writer )
+    {
+        if( null == writer )
+        {
+            throw new NullPointerException( "writer property must not be null" );
+        }
+
+        m_output = writer;
+    }
+
+    /**
+     * Concrete implementation of output that writes out to underlying writer.
+     *
+     * @param data the data to output
+     */
+    protected void write( final String data )
+    {
+        try
+        {
+            m_output.write( data );
+            m_output.flush();
+        }
+        catch( final IOException ioe )
+        {
+            error( "Caught an IOException", ioe );
+        }
+    }
+
+
+    /**
+     * Shutdown target.
+     * Attempting to write to target after close() will cause errors to be logged.
+     */
+    public synchronized void close()
+    {
+        super.close();
+        shutdownWriter();
+    }
+
+    /**
+     * Shutdown Writer.
+     */
+    protected synchronized void shutdownWriter()
+    {
+        final Writer writer = m_output;
+        m_output = null;
+
+        try
+        {
+            if( null != writer )
+            {
+                writer.close();
+            }
+        }
+        catch( final IOException ioe )
+        {
+            error( "Error closing Writer", ioe );
+        }
+    }
+}
