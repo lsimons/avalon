92a7f3fbf98175f01faa31c2f8698f9291bdcb54
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2001-07-30 15:21:19 +0000

    Move Datagram to new package and deprecate old version. This is done because there is other net code that will be following it RSN.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@11521 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-logkit/src/java/org/apache/log/output/net/DatagramOutputTarget.java
+++ avalon-logkit/src/java/org/apache/log/output/net/DatagramOutputTarget.java
@@ -5,22 +5,27 @@
  * version 1.1, a copy of which has been included with this distribution in
  * the LICENSE file.
  */
-package org.apache.log.output;
+package org.apache.log.output.net;
 
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import org.apache.log.Formatter;
+import org.apache.log.output.AbstractOutputTarget;
 
 /**
  * A datagram output target.
  * Useful for writing using custom protocols or writing to syslog daemons.
  *
  * @author <a href="mailto:donaldp@apache.org">Peter Donald</a>
- * @deprecated Use org.apache.log.output.net.DatagramOutputTarget instead
  */
 public class DatagramOutputTarget
-    extends org.apache.log.output.net.DatagramOutputTarget
+    extends AbstractOutputTarget
 {
+    ///Socket on which to send datagrams
+    private DatagramSocket m_socket;
+
     /**
      * Create a output target with end point specified by address and port.
      *
@@ -28,12 +33,15 @@ public class DatagramOutputTarget
      * @param port the address port
      * @exception IOException if an error occurs
      */
-    public DatagramOutputTarget( final InetAddress address, 
+    public DatagramOutputTarget( final InetAddress address,
                                  final int port,
                                  final Formatter formatter )
         throws IOException
     {
-        super( address, port, formatter );
+        super( formatter );
+        m_socket = new DatagramSocket();
+        m_socket.connect( address, port );
+        open();
     }
 
     /**
@@ -46,6 +54,36 @@ public class DatagramOutputTarget
     public DatagramOutputTarget( final InetAddress address, final int port )
         throws IOException
     {
-        super( address, port );
+        this( address, port, null );
+    }
+
+    /**
+     * Method to write output to datagram.
+     *
+     * @param stringData the data to be output
+     */
+    protected void write( final String stringData )
+    {
+        final byte[] data = stringData.getBytes();
+
+        try
+        {
+            final DatagramPacket packet = new DatagramPacket( data, data.length );
+            m_socket.send( packet );
+        }
+        catch( final IOException ioe )
+        {
+            error( "Error sending datagram.", ioe );
+        }
+    }
+
+    /**
+     * Shutdown target.
+     * Attempting to write to target after close() will cause errors to be logged.
+     */
+    public synchronized void close()
+    {
+        super.close();
+        m_socket = null;
     }
 }
