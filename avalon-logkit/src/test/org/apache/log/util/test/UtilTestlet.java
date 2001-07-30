7ea7975de4b32eeab345d8301ae33f9ae4e32869
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2001-07-30 06:15:34 +0000

    Incorporated more unit tests.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@11506 13f79535-47bb-0310-9956-ffa450edef68

---
+++ avalon-logkit/src/test/org/apache/log/util/test/UtilTestlet.java
@@ -0,0 +1,58 @@
+package org.apache.log.util.test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.apache.log.Hierarchy;
+import org.apache.log.LogTarget;
+import org.apache.log.Logger;
+import org.apache.log.Priority;
+import org.apache.log.format.PatternFormatter;
+import org.apache.log.format.RawFormatter;
+import org.apache.log.output.StreamTarget;
+import org.apache.log.util.OutputStreamLogger;
+import org.apache.testlet.AbstractTestlet;
+
+/**
+ * Test suite for utility features of Logger.
+ *
+ * @author <a href="mailto:donaldp@apache.org">Peter Donald</a>
+ */
+public final class UtilTestlet
+    extends AbstractTestlet
+{
+    private final static String EOL = System.getProperty( "line.separator", "\n" );
+    private final static RawFormatter FORMATTER = new RawFormatter();
+
+    private final static String MSG = "No soup for you!";
+    private final static String RMSG = MSG;
+
+    private String getResult( final ByteArrayOutputStream output )
+    {
+        final String result = output.toString();
+        output.reset();
+        return result;
+    }
+
+    public void testOutputStreamLogger()
+    {
+        final ByteArrayOutputStream output = new ByteArrayOutputStream();
+        final StreamTarget target = new StreamTarget( output, FORMATTER );
+
+        final Hierarchy hierarchy = new Hierarchy();
+        hierarchy.setDefaultLogTarget( target );
+
+        final Logger logger = hierarchy.getLoggerFor( "myLogger" );
+        final OutputStreamLogger outputStream = new OutputStreamLogger( logger, Priority.DEBUG );
+        final PrintStream printer = new PrintStream( outputStream, true );
+
+        printer.println( MSG );
+        assertEquality( "OutputStreamLogger", RMSG + EOL, getResult( output ) );
+
+        //unbuffered output
+        printer.print( MSG );
+        printer.flush();
+        assertEquality( "OutputStreamLogger", RMSG, getResult( output ) );
+
+        printer.close();
+    }
+}
