8518a2e0b3097f263706f8a26e6895a12ba9d2a4
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2001-08-26 17:10:21 +0000

    Move all the container classes to a potential resting place.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@11711 13f79535-47bb-0310-9956-ffa450edef68

---
+++ avalon-phoenix/src/java/org/apache/avalon/framework/container/State.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included with this distribution in
+ * the LICENSE file.
+ */
+package org.apache.avalon.framework.container;
+
+import org.apache.avalon.framework.ValuedEnum;
+
+/**
+ * Defines possible states for contained components.
+ *
+ * @author <a href="mailto:donaldp@apache.org">Peter Donald</a>
+ */
+public final class State
+    extends ValuedEnum
+{
+    //A list of constants representing phases in Comonents lifecycle.
+    //Each phase is made up of a number of stages.
+    public final static State  BASE       = new State( "BASE", 0 );
+    public final static State  STARTEDUP  = new State( "STARTEDUP", 10 );
+    public final static State  SHUTDOWN   = new State( "SHUTDOWN", 20 );
+
+    protected State( final String name, final int value )
+    {
+        super( name, value );
+    }
+}
