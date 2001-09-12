a2bec6968570d257f2db3dd1485f34705ab20667
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2001-09-12 12:42:27 +0000

    Made it less than 80 columns wide.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@11817 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-components/src/java/org/apache/avalon/cornerstone/services/soapification/SOAPification.java
+++ avalon-components/src/java/org/apache/avalon/cornerstone/services/soapification/SOAPification.java
@@ -26,7 +26,8 @@ public interface SOAPification extends Service
      * @param publicationName The name to publish it as.
      * @param interfacesToExpose an array of interfaces that the synamic proxy should expose
      */
-    void publish(Object obj, String publicationName, Class[] interfacesToExpose) throws SOAPificationException;
+    void publish( Object obj, String publicationName, Class[] interfacesToExpose) 
+        throws SOAPificationException;
     
     /**
      * Publish an interface 
@@ -35,20 +36,23 @@ public interface SOAPification extends Service
      * @param publicationName The name to publish it as.
      * @param interfaceToExpose an interface that the synamic proxy should expose
      */    
-    void publish(Object obj, String publicationName, Class interfaceToExpose) throws SOAPificationException;
+    void publish(Object obj, String publicationName, Class interfaceToExpose) 
+        throws SOAPificationException;
+
     /**
      * Publish an object (no interfaces(s)).
      *
      * @param obj the object to publish
      * @param publicationName The name to publish it as.
      */    
-    void publish(Object obj, String publicationName) throws SOAPificationException;
+    void publish(Object obj, String publicationName) 
+        throws SOAPificationException;
     
     /**
      * Unpublish 
      *
      * @param publicationName the name of the object to unpublish 
      */    
-    void unpublish(String publicationName) throws SOAPificationException;
-
+    void unpublish(String publicationName) 
+        throws SOAPificationException;
 }
