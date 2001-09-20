4e694fb8c4aeaf76f71b7b86f70e7a8f7f3f8e1e
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2001-09-20 01:13:04 +0000

    Made build.sh behave like build.bat by allowing you to go
    ./build.sh [subproject] [subprojects-target]
    
    
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@11885 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-components/build.sh
+++ avalon-components/build.sh
@@ -21,4 +21,4 @@ fi
 chmod u+x $AVALON_TOOLS/bin/antRun
 chmod u+x $AVALON_TOOLS/bin/ant
 
-$AVALON_TOOLS/bin/ant -logger org.apache.tools.ant.NoBannerLogger -emacs -Dtools.dir=$AVALON_TOOLS $@ 
+$AVALON_TOOLS/bin/ant -logger org.apache.tools.ant.NoBannerLogger -emacs -Dtools.dir=$AVALON_TOOLS $1 -Dapps-target=$2 $3 $4 $5 $6 $7 $8
