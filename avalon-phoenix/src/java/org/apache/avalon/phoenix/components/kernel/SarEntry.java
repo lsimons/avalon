9ff9998c06df424d27571c4f4c9b17f987ce0d9d
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2001-09-24 14:37:47 +0000

    Made class package access and added get/set methods for Application object.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@11999 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-phoenix/src/java/org/apache/avalon/phoenix/components/kernel/SarEntry.java
+++ avalon-phoenix/src/java/org/apache/avalon/phoenix/components/kernel/SarEntry.java
@@ -11,27 +11,39 @@ import java.net.URL;
 import org.apache.avalon.excalibur.container.Entry;
 import org.apache.avalon.framework.configuration.Configuration;
 import org.apache.avalon.phoenix.metadata.SarMetaData;
+import org.apache.avalon.phoenix.components.application.Application;
 
 /**
  * This is the structure describing each server application before it is loaded.
  *
  * @author <a href="mailto:donaldp@apache.org">Peter Donald</a>
  */
-public class SarEntry
+final class SarEntry
     extends Entry
 {
     private SarMetaData     m_metaData;
     private Configuration   m_configuration;
     private ClassLoader     m_classLoader;
+    private Application     m_application;
 
-    public SarEntry( final SarMetaData metaData, 
-                     final ClassLoader classLoader,
-                     final Configuration configuration )
+    protected SarEntry( final SarMetaData metaData, 
+                        final ClassLoader classLoader,
+                        final Configuration configuration )
     {
         m_metaData = metaData;
         m_classLoader = classLoader;
         m_configuration = configuration;
     }
+
+    public Application getApplication()
+    {
+        return m_application;
+    } 
+
+    public void setApplication( final Application application )
+    {
+        m_application = application;
+    } 
 
     public ClassLoader getClassLoader()
     {
