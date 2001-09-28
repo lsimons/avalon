895fc52f85bb07ba8ef4d3ad30d303a299828226
colus <colus@13f79535-47bb-0310-9956-ffa450edef68>
2001-09-28 00:35:10 +0000

    Added for SingleAppEmbeddor. Load classes with context's ClassLoader.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@12105 13f79535-47bb-0310-9956-ffa450edef68

---
+++ avalon-phoenix/src/java/org/apache/avalon/phoenix/components/classloader/ContextClassLoaderManager.java
@@ -0,0 +1,23 @@
+package org.apache.avalon.phoenix.components.classloader;
+
+import java.io.File;
+import java.net.URL;
+import org.apache.avalon.framework.configuration.Configuration;
+import org.apache.avalon.framework.logger.AbstractLoggable;
+import org.apache.avalon.phoenix.interfaces.ClassLoaderManager;
+
+/**
+ * @author <a href="mailto:colus@apache.org">Eung-ju Park</a>
+ */
+public class ContextClassLoaderManager
+    extends AbstractLoggable
+    implements ClassLoaderManager
+{
+    public ClassLoader createClassLoader( final Configuration server,
+                                          final File homeDirectory,
+                                          final URL[] classPath )
+        throws Exception
+    {
+        return Thread.currentThread().getContextClassLoader();
+    }
+}
