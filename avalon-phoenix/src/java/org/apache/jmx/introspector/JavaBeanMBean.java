24161696b2ae305e0665158653945f3f55e9c4e5
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2001-09-29 01:21:34 +0000

    Make it possible to restrict attributes and operations based on interfaces.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@12132 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-phoenix/src/java/org/apache/jmx/introspector/JavaBeanMBean.java
+++ avalon-phoenix/src/java/org/apache/jmx/introspector/JavaBeanMBean.java
@@ -10,6 +10,7 @@ package org.apache.jmx.introspector;
 import java.beans.BeanInfo;
 import java.beans.Introspector;
 import java.beans.MethodDescriptor;
+import java.beans.ParameterDescriptor;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -24,7 +25,7 @@ import javax.management.MBeanOperationInfo;
  *
  * @author <a href="mailto:mail@leosimons.com">Leo Simons</a>
  * @author <a href="mailto:donaldp@apache.org">Peter Donald</a>
- * @version CVS $Revision: 1.3 $ $Date: 2001/09/29 00:07:01 $
+ * @version CVS $Revision: 1.4 $ $Date: 2001/09/29 01:21:34 $
  */
 public class JavaBeanMBean
     extends AbstractMBean
@@ -34,10 +35,27 @@ public class JavaBeanMBean
      */
     private BeanInfo m_beanInfo;
 
+    /**
+     * Methods for Operations that are allowed to operate on object.
+     */
+    private MethodDescriptor[] m_allowedOperations;
+
+    /**
+     * Propertys for Attributes that are allowed for object.
+     */
+    private PropertyDescriptor[] m_allowedAttributes;
+
     public JavaBeanMBean( final Object object )
         throws IllegalArgumentException
     {
+        this( object, null );
+    }
+
+    public JavaBeanMBean( final Object object, final Class[] interfaces )
+        throws IllegalArgumentException
+    {
         super( object );
+
         final Class clazz = object.getClass();
         try
         {
@@ -48,16 +66,13 @@ public class JavaBeanMBean
             throw new IllegalArgumentException( "Not a compliant javabean!" );
         }
 
+        m_allowedOperations = getAllowedOperations( interfaces );
+        m_allowedAttributes = getAllowedAttributes( interfaces );
+
         initialize();
-    }
-
-    JavaBeanMBean( final Object object, final Class[] interfaces )
-        throws IllegalArgumentException
-    {
-        this( object );
-
-        //restrictOperationsTo( interfaces );
-        //restrictAttributesTo( interfaces );
+        
+        m_allowedOperations = null;
+        m_allowedAttributes = null;
     }
 
     /**
@@ -78,20 +93,24 @@ public class JavaBeanMBean
     protected OperationEntry[] createOperations()
     {
         final MethodDescriptor[] methods = m_beanInfo.getMethodDescriptors();
-        final OperationEntry[] entrys = new OperationEntry[ methods.length ];
-
-        for( int i = 0; i < entrys.length; i++ )
+        final ArrayList entrys = new ArrayList();
+        
+        for( int i = 0; i < methods.length; i++ )
         {
             final MethodDescriptor descriptor = methods[ i ];
+
+            //Skip disallowed operations
+            if( !isAllowedOperation( descriptor ) ) continue;
+
             final Method method = descriptor.getMethod();
             final MBeanOperationInfo info = 
                 new MBeanOperationInfo( descriptor.getShortDescription(),
                                         method );
 
-            entrys[ i ] = new OperationEntry( info, method );
+            entrys.add( new OperationEntry( info, method ) );
         }
 
-        return entrys;
+        return (OperationEntry[])entrys.toArray( new OperationEntry[ 0 ] );
     }
 
     /**
@@ -103,40 +122,44 @@ public class JavaBeanMBean
         throws IllegalArgumentException
     {
         final PropertyDescriptor[] propertys = m_beanInfo.getPropertyDescriptors();
-        final AttributeEntry[] entrys = new AttributeEntry[ propertys.length ];
+        final ArrayList entrys = new ArrayList();
 
-        for( int i = 0; i < entrys.length; i++ )
+        for( int i = 0; i < propertys.length; i++ )
         {
             final PropertyDescriptor property = propertys[ i ];
+
+            //Skip disallowed attributes
+            if( !isAllowedAttribute( property ) ) continue;
+
             try
             {
-                entrys[ i ] = 
+                final AttributeEntry attribute =
                     new AttributeEntry( property.getName(),
                                         property.getShortDescription(),
                                         property.getReadMethod(),
                                         property.getWriteMethod() );
+                entrys.add( attribute );
             }
             catch( final IntrospectionException ie )
             {
                 throw new IllegalArgumentException( "Error introspecting properties." );
             }
         }
-        
-        return entrys;
+
+        return (AttributeEntry[])entrys.toArray( new AttributeEntry[ 0 ] );
     }
 
     /**
-     * Strips the operations array from unwanted entries.
-     *
-     * @param interfaces DOC: Insert Description of Parameter
-     * @exception IllegalArgumentException DOC: Insert Description of Exception
+     * Retrieve a list of allowed operations. 
+     * Allowed operations are based on interfaces passed in.
      */
-/*
-    protected void restrictOperationsTo( Class[] interfaces )
+    private MethodDescriptor[] getAllowedOperations( final Class[] interfaces )
         throws IllegalArgumentException
     {
-        ArrayList allowedOperations = new ArrayList();
-        ArrayList operationInfo = new ArrayList();
+        if( null == interfaces ) return null;
+
+        final ArrayList operations = new ArrayList();
+        final ArrayList names = new ArrayList();
 
         for( int i = 0; i < interfaces.length; i++ )
         {
@@ -145,41 +168,34 @@ public class JavaBeanMBean
             {
                 beanInfo = Introspector.getBeanInfo( interfaces[i] );
             }
-            catch( Exception e )
+            catch( final Exception e )
             {
-                throw new IllegalArgumentException(
-                                                   "The supplied interfaces are not all valid javabeans!" );
+                throw new IllegalArgumentException( "The supplied interfaces are " + 
+                                                    "not all valid javabeans!" );
             }
-            final MethodDescriptor[] md = beanInfo.getMethodDescriptors();
-            for( int j = 0; j < md.length; j++ )
-            {
-                final MBeanOperationInfo info = new MBeanOperationInfo(
-                                                                       md[i].getShortDescription(),
-                                                                       md[i].getMethod() );
-                allowedOperations.add( info );
-            }
-        }
-        MBeanOperationInfo[] m =
-            (MBeanOperationInfo[])allowedOperations.toArray( new MBeanOperationInfo[0] );
 
-        for( int i = 0; i < m.length; i++ )
-        {
-            for( int j = 0; j < m_operations.length; j++ )
+            final MethodDescriptor[] methods = beanInfo.getMethodDescriptors();
+            for( int j = 0; j < methods.length; j++ )
             {
-                if( m[i].equals( m_operations[j] ) )
-                {
-                    operationInfo.add( m_operations[j] );
-                }
+                operations.add( methods[ j ] );
+                names.add( methods[ j ].getName() );
             }
         }
-        m_operations = (MBeanOperationInfo[])operationInfo.toArray( new MBeanOperationInfo[0] );
+
+        return (MethodDescriptor[])operations.toArray( new MethodDescriptor[ 0 ] );
     }
 
-    protected void restrictAttributesTo( Class[] interfaces )
+    /**
+     * Retrieve a list of allowed attributes. 
+     * Allowed attributes are based on interfaces passed in.
+     */
+    private PropertyDescriptor[] getAllowedAttributes( final Class[] interfaces )
         throws IllegalArgumentException
     {
-        ArrayList allowedAttributes = new ArrayList();
-        ArrayList attributeInfo = new ArrayList();
+        if( null == interfaces ) return null;
+
+        final ArrayList attributes = new ArrayList();
+        final ArrayList names = new ArrayList();
 
         for( int i = 0; i < interfaces.length; i++ )
         {
@@ -188,43 +204,103 @@ public class JavaBeanMBean
             {
                 beanInfo = Introspector.getBeanInfo( interfaces[i] );
             }
-            catch( Exception e )
+            catch( final Exception e )
             {
-                throw new IllegalArgumentException(
-                                                   "The supplied interfaces are not all valid javabeans!" );
+                throw new IllegalArgumentException( "The supplied interfaces are " + 
+                                                    "not all valid javabeans!" );
             }
-            final PropertyDescriptor[] pd = beanInfo.getPropertyDescriptors();
-            for( int j = 0; j < pd.length; j++ )
-            {
-                try
-                {
-                    final MBeanAttributeInfo info = new MBeanAttributeInfo(
-                                                                           pd[i].getName(),
-                                                                           pd[i].getShortDescription(),
-                                                                           pd[i].getReadMethod(),
-                                                                           pd[i].getWriteMethod() );
-                    allowedAttributes.add( info );
-                }
-                catch( IntrospectionException ie )
-                {
-                    throw new IllegalArgumentException( "Error introspecting properties" );
-                }
-            }
-        }
-        MBeanAttributeInfo[] m =
-            (MBeanAttributeInfo[])allowedAttributes.toArray( new MBeanAttributeInfo[0] );
 
-        for( int i = 0; i < m.length; i++ )
-        {
-            for( int j = 0; j < m_attributes.length; j++ )
+            final PropertyDescriptor[] propertys = beanInfo.getPropertyDescriptors();
+            for( int j = 0; j < propertys.length; j++ )
             {
-                if( m[i].equals( m_attributes[j] ) )
-                {
-                    attributeInfo.add( m_attributes[j] );
-                }
+                attributes.add( propertys[ j ] );
+                names.add( propertys[ j ].getName() );
             }
         }
-        m_attributes = (MBeanAttributeInfo[])attributeInfo.toArray( new MBeanAttributeInfo[0] );
+
+        return (PropertyDescriptor[])attributes.toArray( new PropertyDescriptor[ 0 ] );
     }
-*/
+
+    /**
+     * Determine if specified descriptor describes an allowed operation.
+     * If no interfaces were passed in then all operations are allowed.
+     *
+     * @param method the operation descriptor
+     * @return true if allowed, false otherwise
+     */
+    private boolean isAllowedOperation( final MethodDescriptor method )
+    {
+        if( null == m_allowedOperations ) return true;
+
+        final ParameterDescriptor[] params = method.getParameterDescriptors();
+
+        for( int i = 0; i < m_allowedOperations.length; i++ )
+        {
+            final MethodDescriptor other = m_allowedOperations[ i ];
+            final ParameterDescriptor[] otherParams = other.getParameterDescriptors();
+
+            //If operation doesn't have same name and 
+            //same number of parameters then it is 
+            //not the droids we are looking for
+            if( !other.getName().equals( method.getName() ) )
+            {
+                continue;
+            }
+            else if( null == params && null == otherParams )
+            {
+                return true;
+            }
+            else if ( otherParams.length != params.length )
+            {
+                continue;
+            }
+
+            boolean found = true;
+            for( int j = 0; j < otherParams.length; j++ )
+            {
+                final String type = params[ j ].getName();
+                final String otherType = otherParams[ j ].getName();
+                if( !otherType.equals( type ) )
+                {
+                    found = false;
+                    break;
+                }
+            }
+
+            //If all the parameters have same 
+            //type then we have found a match 
+            //so return it
+            if( found )
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine if specified descriptor describes an allowed attribute.
+     * If no interfaces were passed in then all attributes are allowed.
+     *
+     * @param method the attribute descriptor
+     * @return true if allowed, false otherwise
+     */
+    private boolean isAllowedAttribute( final PropertyDescriptor property )
+    {
+        if( null == m_allowedOperations ) return true;
+
+        for( int i = 0; i < m_allowedAttributes.length; i++ )
+        {
+            final PropertyDescriptor other = m_allowedAttributes[ i ];
+
+            //If attribute has same name then we have a match
+            if( other.getName().equals( property.getName() ) )
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
