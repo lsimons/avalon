1c31be7cb2eb87c55778c8b02efa15a2e9c57ce8
mirceatoma <mirceatoma@13f79535-47bb-0310-9956-ffa450edef68>
2001-10-24 16:21:11 +0000

    Updated javadocs. Code cosmetics.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@12207 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-phoenix/src/java/org/apache/avalon/phoenix/tools/installer/FileDigest.java
+++ avalon-phoenix/src/java/org/apache/avalon/phoenix/tools/installer/FileDigest.java
@@ -2,43 +2,50 @@ package org.apache.avalon.phoenix.tools.installer;
 
 import java.io.File;
 
+/**
+ * FileDigest is a deployment information holder for a file. The information 
+ * is used to verify if file was modified since deployed.
+ */
 class FileDigest
 {
-        
-    /** Holds value of property name. */
-    private File m_file;
-
-    /** Holds value of property checksum.  */
-    private long m_checksum;
+    private final File m_file;
+    private final long m_checksum;
+    private final long m_modified;
     
-    /** Holds value of property modified.  */
-    private long m_modified;
-    
-    public FileDigest(final File file, final long checksum, final long modified)
+    /** Create a new FileDigest object.
+     *
+     * @param the file.
+     * @param the checksum value of the file.
+     * @param the modification time of the file.
+     */
+    public FileDigest( final File file, final long checksum, final long modified )
     {
         m_file = file;
         m_checksum = checksum;
         m_modified = modified;
     }
         
-    /** Getter for property name.
-     * @return Value of property name.
+    /** Retrieve the file.
+     *
+     * @return the file.
      */
     public File getFile()
     {
         return m_file;
     }
 
-    /** Getter for property checksum.
-     * @return Value of property checksum.
+    /** Retrieve the checksum calculated at deployment time.
+     *
+     * @return the checksum value.
      */
     public long getChecksum()
     {
         return m_checksum;
     }
     
-    /** Getter for property modified.
-     * @return Value of property modified.
+    /** Retrieve the modification time at deployment time.
+     *
+     * @return the modification time.
      */
     public long getModified()
     {
