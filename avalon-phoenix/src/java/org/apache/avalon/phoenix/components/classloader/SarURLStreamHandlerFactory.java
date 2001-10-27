b1e42155a3635b009df533bddc0f713bf23f3e8a
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2001-10-27 15:39:21 +0000

    Integrate the StreamHandler and StreamHandlerFactory.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@12229 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-phoenix/src/java/org/apache/avalon/phoenix/components/classloader/SarURLStreamHandlerFactory.java
+++ avalon-phoenix/src/java/org/apache/avalon/phoenix/components/classloader/SarURLStreamHandlerFactory.java
@@ -7,18 +7,29 @@
  */
 package org.apache.avalon.phoenix.components.classloader;
 
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.net.URLStreamHandler;
 import java.net.URLStreamHandlerFactory;
+import org.apache.avalon.excalibur.i18n.ResourceManager;
+import org.apache.avalon.excalibur.i18n.Resources;
+import org.apache.avalon.phoenix.tools.protocols.sar.SarURLConnection;
 
 /**
- * Factory for <code>sar:</code> URL stream protocol handler.
+ * Implementation and "factory" and for <code>sar:</code> URL stream
+ * protocol handler.
  *
  * @author <a href="mailto:mirceatoma@home.com">Mircea Toma</a>
- * @version CVS $Revision: 1.2 $ $Date: 2001/10/27 15:20:38 $
+ * @version CVS $Revision: 1.3 $ $Date: 2001/10/27 15:39:21 $
  */
 class SarURLStreamHandlerFactory
+    extends URLStreamHandler
     implements URLStreamHandlerFactory
 {
+    private static final Resources REZ =
+        ResourceManager.getPackageResources( DefaultPolicy.class );
+
     /**
      * Creates a new <code>URLStreamHandler</code> instance with the specified
      * protocol.
@@ -32,9 +43,27 @@ class SarURLStreamHandlerFactory
     {
         if ( "sar".equals( protocol ) )
         {
-            return new SarURLStreamHandler();
+            return this;
         }
 
         return null;
+    }
+
+    /**
+     * Creates a new URLConnection.
+     *
+     * @param url the URL where to connect.
+     * @return the URLConnection.
+     */
+    public URLConnection openConnection( final URL url )
+        throws IOException
+    {
+        if ( "sar".equals( url.getProtocol() ) )
+        {
+            return new SarURLConnection( url );
+        }
+
+        final String message = REZ.getString( "create-connection", url.getProtocol() );
+        throw new IOException( message );
     }
 }
