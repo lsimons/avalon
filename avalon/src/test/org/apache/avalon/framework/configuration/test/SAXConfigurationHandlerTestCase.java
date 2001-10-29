1355ddf367a806fc66f1c85b871c42cd9b11cac1
bloritsch <bloritsch@13f79535-47bb-0310-9956-ffa450edef68>
2001-10-29 15:46:58 +0000

    debug SAXConfigurationHandler.  It was emitting incorrect SAX events
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@12264 13f79535-47bb-0310-9956-ffa450edef68

--- avalon/src/test/org/apache/avalon/framework/configuration/test/SAXConfigurationHandlerTestCase.java
+++ avalon/src/test/org/apache/avalon/framework/configuration/test/SAXConfigurationHandlerTestCase.java
@@ -10,6 +10,7 @@ package org.apache.avalon.framework.configuration.test;
 import java.util.List;
 import junit.framework.TestCase;
 import org.apache.avalon.framework.configuration.Configuration;
+import org.apache.avalon.framework.configuration.Namespace;
 import org.apache.avalon.framework.configuration.SAXConfigurationHandler;
 import org.xml.sax.Attributes;
 import org.xml.sax.helpers.AttributesImpl;
@@ -22,7 +23,7 @@ import org.xml.sax.helpers.AttributesImpl;
 public final class SAXConfigurationHandlerTestCase extends TestCase
 {
     private SAXConfigurationHandler m_handler;
-        
+
     public SAXConfigurationHandlerTestCase()
     {
         this("SAXConfigurationHandler Test Case ");
@@ -43,40 +44,52 @@ public final class SAXConfigurationHandlerTestCase extends TestCase
         m_handler = null;
     }
 
+    /**
+     * Test the ContentHandler.  The XML created should look like this:
+     *
+     * <pre>
+     *   &lt;rawName attqName="attValue"&gt;
+     *     &lt;child:localName xmlns:child="namespaceURI"&gt;value&lt;/child:localName&gt;
+     *   &lt;/rawName&gt;
+     * </pre>
+     */
     public void testHandling() throws Exception
     {
-        final String namespaceURI = "namespaceURI";
-        final String localName = "localName";
-        final String rawName = "rawName";
-        final String value = "value";
-        final String attUri = "attUri";
-        final String attLocalName = "attLocalName";
+        final String rootURI = "";
+        final String rootlocal = "rawName";
+        final String rootraw = "rawName";
+        final String childURI = "namespaceURI";
+        final String childlocal = "localName";
+        final String childraw = "child:" + childlocal;
+        final String childvalue = "value";
         final String attqName = "attqName";
-        final String attType = "attType";
         final String attValue = "attValue";
-        final String childfix = "child";
 
         final AttributesImpl attributes  = new AttributesImpl();
-        attributes.addAttribute(attUri,attLocalName,attqName,
-                                attType,attValue);
+        attributes.addAttribute(null,null,attqName,
+                                "CDATA",attValue);
 
         final AttributesImpl childAttributes  = new AttributesImpl();
+
         m_handler.startDocument();
-        m_handler.startElement( namespaceURI, localName, rawName, attributes );
-        m_handler.startElement( namespaceURI + childfix,  
-                                localName + childfix,
-                                rawName + childfix, 
+        m_handler.startElement( rootURI, rootlocal, rootraw, attributes );
+        m_handler.startPrefixMapping( "child", childURI );
+        m_handler.startElement( childURI,
+                                childlocal,
+                                childraw,
                                 childAttributes );
 
-        m_handler.characters( value.toCharArray(), 0, value.length() );
-        m_handler.endElement( namespaceURI + childfix, localName + childfix, rawName + childfix );
-        m_handler.endElement( namespaceURI, localName, rawName);
+        m_handler.characters( childvalue.toCharArray(), 0, childvalue.length() );
+        m_handler.endElement( childURI, childlocal, childraw );
+        m_handler.endPrefixMapping( "child" );
+        m_handler.endElement( null, null, rootraw);
         m_handler.endDocument();
 
         final Configuration configuration = m_handler.getConfiguration();
         assertEquals( attValue, configuration.getAttribute(attqName));
-        assertEquals( value, configuration.getChild(rawName+childfix).getValue());
-        assertEquals( rawName, configuration.getName());
+        assertEquals( childvalue, configuration.getChild(childlocal).getValue());
+        assertEquals( Namespace.getNamespace("child", childURI), configuration.getChild(childlocal).getNamespace() );
+        assertEquals( rootraw, configuration.getName());
     }
 }
 
