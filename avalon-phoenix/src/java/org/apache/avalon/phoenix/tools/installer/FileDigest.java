17d99e13ac605a7283993a8b0750bb3903b852f8
mirceatoma <mirceatoma@13f79535-47bb-0310-9956-ffa450edef68>
2001-10-31 21:54:35 +0000

    Change file's 'modified' attribute to a global Installation 'timestamp' attribute. Change class access to package level.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@12296 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-phoenix/src/java/org/apache/avalon/phoenix/tools/installer/FileDigest.java
+++ avalon-phoenix/src/java/org/apache/avalon/phoenix/tools/installer/FileDigest.java
@@ -3,33 +3,30 @@ package org.apache.avalon.phoenix.tools.installer;
 import java.io.File;
 
 /**
- * FileDigest is a deployment information holder for a file. The information 
- * is used to verify if file was modified since deployed.
+ * FileDigest holds file deployment information. Information 
+ * used to verify if file was modified since deployment.
  */
 class FileDigest
 {
     private final File m_file;
     private final long m_checksum;
-    private final long m_modified;
-    
+
     /** Create a new FileDigest object.
      *
      * @param the file.
      * @param the checksum value of the file.
-     * @param the modification time of the file.
      */
-    public FileDigest( final File file, final long checksum, final long modified )
+    FileDigest(final File file, final long checksum)
     {
         m_file = file;
         m_checksum = checksum;
-        m_modified = modified;
     }
         
     /** Retrieve the file.
      *
      * @return the file.
      */
-    public File getFile()
+    File getFile()
     {
         return m_file;
     }
@@ -38,18 +35,9 @@ class FileDigest
      *
      * @return the checksum value.
      */
-    public long getChecksum()
+    long getChecksum()
     {
         return m_checksum;
-    }
-    
-    /** Retrieve the modification time at deployment time.
-     *
-     * @return the modification time.
-     */
-    public long getModified()
-    {
-        return m_modified;
     }
 }
 
