7bdf04b219fe83468a0f3dbda4773f1d31e76b1f
bloritsch <bloritsch@13f79535-47bb-0310-9956-ffa450edef68>
2001-11-02 18:58:45 +0000

    Add throws clauses
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@12320 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-sandbox/excalibur-old-materials/datasource/src/java/org/apache/avalon/excalibur/datasource/DataSourceComponent.java
+++ avalon-sandbox/excalibur-old-materials/datasource/src/java/org/apache/avalon/excalibur/datasource/DataSourceComponent.java
@@ -17,7 +17,7 @@ import org.apache.avalon.framework.configuration.Configurable;
  * The standard interface for DataSources in Avalon.
  *
  * @author <a href="mailto:bloritsch@apache.org">Berin Loritsch</a>
- * @version CVS $Revision: 1.2 $ $Date: 2001/08/07 10:57:07 $
+ * @version CVS $Revision: 1.3 $ $Date: 2001/11/02 18:58:45 $
  * @since 4.0
  */
 public interface DataSourceComponent
@@ -30,6 +30,12 @@ public interface DataSourceComponent
 
     /**
      * Gets the Connection to the database
+     *
+     * @throws NoValidConnectionException when there is no valid Connection wrapper
+     *         available in the classloader.
+     *
+     * @throws NoAvailableConnectionException when there are no more available
+     *         Connections in the pool.
      */
     Connection getConnection()
         throws SQLException;
