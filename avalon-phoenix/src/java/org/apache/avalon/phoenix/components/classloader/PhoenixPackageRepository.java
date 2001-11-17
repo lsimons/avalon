b60b14bb5bcc371ddc55081c181adc35e5635ec1
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2001-11-17 11:50:13 +0000

    Add a basic implementation of PackageRepository that inherits most functionality from base class.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@12424 13f79535-47bb-0310-9956-ffa450edef68

---
+++ avalon-phoenix/src/java/org/apache/avalon/phoenix/components/classloader/PhoenixPackageRepository.java
@@ -0,0 +1,72 @@
+package org.apache.avalon.phoenix.components.classloader;
+
+import java.io.File;
+import org.apache.avalon.excalibur.extension.DefaultPackageRepository;
+import org.apache.avalon.excalibur.util.StringUtil;
+import org.apache.avalon.framework.parameters.Parameters;
+import org.apache.avalon.framework.parameters.ParameterException;
+import org.apache.avalon.framework.parameters.Parameterizable;
+import org.apache.avalon.framework.activity.Initializable;
+import org.apache.avalon.framework.activity.Disposable;
+import org.apache.avalon.framework.logger.LogEnabled;
+import org.apache.avalon.framework.logger.Logger;
+import org.apache.avalon.phoenix.interfaces.PackageRepository;
+
+/**
+ * PhoenixPackageRepository
+ *
+ * @author <a href="mailto:donaldp@apache.org">Peter Donald</a>
+ * @version $Revision: 1.1 $ $Date: 2001/11/17 11:50:13 $
+ */
+public class PhoenixPackageRepository
+    extends DefaultPackageRepository
+    implements LogEnabled, Parameterizable, Initializable, Disposable, PackageRepository
+{
+    private Logger m_logger;
+    
+    private String m_path;
+
+    public PhoenixPackageRepository()
+    {
+        super( new File[ 0 ] );
+    }
+
+    public void enableLogging( final Logger logger )
+    {
+        m_logger = logger;
+    }
+
+    public void parameterize( final Parameters parameters )
+        throws ParameterException
+    {
+        final String phoenixHome = parameters.getParameter( "phoenix.home" );
+        final String defaultExtPath = phoenixHome + File.separator + "ext";
+        m_path = parameters.getParameter( "phoenix.ext.path", defaultExtPath );
+    }
+
+    public void initialize()
+        throws Exception
+    {
+        final String[] pathElements = StringUtil.split( m_path, "|" );
+
+        final File[] dirs = new File[ pathElements.length ];
+        for( int i = 0; i < dirs.length; i++ )
+        {
+            dirs[ i ] = new File( pathElements[ i ] );
+        }
+
+        setPath( dirs );
+
+        scanPath();
+    }
+
+    public void dispose()
+    {
+        clearCache();
+    }
+
+    protected void debug( final String message )
+    {
+        m_logger.debug( message );
+    }
+}
