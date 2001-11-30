3052f5e68a5e2dff7b47042c6751e062ae151888
colus <colus@13f79535-47bb-0310-9956-ffa450edef68>
2001-11-30 12:32:25 +0000

    Added ComposableServlet for easy life with PhoenixServlet.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@12491 13f79535-47bb-0310-9956-ffa450edef68

---
+++ avalon-phoenix/src/java/org/apache/avalon/phoenix/frontends/ComposableServlet.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included with this distribution in
+ * the LICENSE file.
+ */
+package org.apache.avalon.phoenix.frontends;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.avalon.framework.component.Component;
+import org.apache.avalon.framework.component.ComponentException;
+import org.apache.avalon.framework.component.ComponentManager;
+import org.apache.avalon.framework.component.Composable;
+import org.apache.avalon.excalibur.i18n.Resources;
+import org.apache.avalon.excalibur.i18n.ResourceManager;
+import org.apache.avalon.phoenix.interfaces.Embeddor;
+import org.apache.avalon.phoenix.components.embeddor.SingleAppEmbeddor;
+
+/**
+ * Composable servlet for easy life with <code>PhoenixServlet</code>.
+ *
+ * @author <a href="mailto:colus@apache.org">Eung-ju Park</a>
+ */
+public abstract class ComposableServlet
+    extends HttpServlet
+    implements Composable
+{
+    private static final Resources REZ =
+        ResourceManager.getPackageResources( ComposableServlet.class );
+
+    private SingleAppEmbeddor m_embeddor;
+
+    public void init()
+        throws ServletException
+    {
+        super.init();
+
+        m_embeddor = (SingleAppEmbeddor)getServletContext().getAttribute( Embeddor.ROLE );
+        if ( null == m_embeddor )
+        {
+            final String message = REZ.getString( "servlet.error.load" );
+            throw new ServletException( message );
+        }
+
+        try
+        {
+            compose( m_embeddor );
+        }
+        catch ( final ComponentException ce )
+        {
+            throw new ServletException( ce );
+        }
+    }
+
+    public void compose( final ComponentManager componentManager )
+        throws ComponentException
+    {
+    }
+}
