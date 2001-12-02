44928a1218e84184c31ecb55d334c51715e3000d
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2001-12-02 03:47:48 +0000

    Add in interface via which management module interacts with embeddor.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@12502 13f79535-47bb-0310-9956-ffa450edef68

---
+++ avalon-phoenix/src/java/org/apache/avalon/phoenix/interfaces/EmbeddorMBean.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included with this distribution in
+ * the LICENSE file.
+ */
+package org.apache.avalon.phoenix.interfaces;
+
+/**
+ * This is the interface via which the Management interface interacts 
+ * with the Embeddor.
+ *
+ * @author <a href="peter@apache.org">Peter Donald</a>
+ */
+public interface EmbeddorMBean
+{
+    String ROLE = "org.apache.avalon.phoenix.interfaces.EmbeddorMBean";
+
+    /**
+     * Get name by which the server is know.
+     * Usually this defaults to "Phoenix" but the admin
+     * may assign another name. This is useful when you
+     * are managing a cluster of Phoenix servers.
+     *
+     * @return the name of server
+     */
+    String getName();
+
+    /**
+     * Get location of Phoenix installation
+     *
+     * @return the home directory of phoenix
+     */
+    String getHomeDirectory();
+
+    /**
+     * Retrieve the number of millisecond
+     * the server has been up.
+     *
+     * @return the the number of millisecond the server has been up
+     */
+    long getUpTime();
+
+    /**
+     * Retrieve a string identifying version of server.
+     * Usually looks like "v4.0.1a".
+     *
+     * @return version string of server.
+     */
+    String getVersion();
+
+    /**
+     * Get a string defining the build.
+     * Possibly the date on which it was built, where it was built,
+     * with what features it was built and so forth.
+     *
+     * @return the string describing build
+     */
+    String getBuildData();
+
+    /**
+     * Request the Embeddor shutsdown.
+     */
+    void shutdown();
+}
