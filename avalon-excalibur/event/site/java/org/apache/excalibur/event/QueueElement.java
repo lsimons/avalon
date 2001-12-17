c7655198efcddcea275962a29dce1a102b0511fe
bloritsch <bloritsch@13f79535-47bb-0310-9956-ffa450edef68>
2001-12-17 18:43:40 +0000

    add some methods to make using QueueElements easier
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@12601 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-excalibur/event/site/java/org/apache/excalibur/event/QueueElement.java
+++ avalon-excalibur/event/site/java/org/apache/excalibur/event/QueueElement.java
@@ -15,4 +15,32 @@ package org.apache.avalon.excalibur.event;
  */
 public interface QueueElement
 {
+    /**
+     * Get the type id of the QueueElement.  This allows simplified processing
+     * without alot of explicit casting.  In order to manage the types allowed
+     * for a subsystem, each subsystem should declare an interface that lists
+     * all the types available with constant names.  Another rule of thumb is
+     * to map all the signals into a type of address space.  For instance, the
+     * code snippet below will help:
+     *
+     * <pre>
+     *   public interface AsyncFileTypes
+     *   {
+     *       long ADDRESS = $af100000;
+     *       long FILE_OPEN = ADDRESS + 1;
+     *       long FILE_CLOSE = FILE_OPEN + 1;
+     *   }
+     * </pre>
+     */
+    long getType();
+
+    /**
+     * Get a handle to an attached object.
+     */
+    Object getAttachment();
+
+    /**
+     * Attach an object to the element.
+     */
+    void attach( Object attachment );
 }
