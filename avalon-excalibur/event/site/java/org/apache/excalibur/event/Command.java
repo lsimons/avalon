1aa0144b4a33c9043897202d67b80db0bb18f945
bloritsch <bloritsch@13f79535-47bb-0310-9956-ffa450edef68>
2001-12-19 18:13:49 +0000

    add new QueueElement types
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@12611 13f79535-47bb-0310-9956-ffa450edef68

---
+++ avalon-excalibur/event/site/java/org/apache/excalibur/event/Command.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.apache.avalon.excalibur.event;
+
+import org.apache.avalon.framework.activity.Executable;
+
+/**
+ * A Signal is a specific type of QueueElement that denotes a Control code for
+ * the Queue system.
+ *
+ * @author <a href="mailto:bloritsch@apache.org">Berin Loritsch</a>
+ */
+public interface Command extends Signal, Executable
+{
+    /**
+     * Test whether this command is repeatable.  If so, it will be placed back
+     * on the CommandQueue to be issued again when it is time.
+     */
+    boolean isRepeatable();
+
+    /**
+     * Gets the repeat interval so that the CommandQueue keeps it for the specified
+     * amount of time before enqueuing it.  If the Command is not repeatable,
+     * then the repeat interval is -1.
+     */
+    long getRepeatInterval();
+}
