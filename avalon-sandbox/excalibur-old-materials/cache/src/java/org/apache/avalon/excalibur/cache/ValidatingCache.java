3c98d5053525b4ba9c42d85fc23fcce1e06e7e3b
colus <colus@13f79535-47bb-0310-9956-ffa450edef68>
2002-01-08 13:01:49 +0000

    Added license banner.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@12664 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-sandbox/excalibur-old-materials/cache/src/java/org/apache/avalon/excalibur/cache/ValidatingCache.java
+++ avalon-sandbox/excalibur-old-materials/cache/src/java/org/apache/avalon/excalibur/cache/ValidatingCache.java
@@ -1,7 +1,14 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included  with this distribution in
+ * the LICENSE.txt file.
+ */
 package org.apache.avalon.excalibur.cache;
 
 /**
- * Cache.
+ * Validating cache proxy.
  *
  * @author <a href="mailto:colus@isoft.co.kr">Eung-ju Park</a>
  * @author <a href="mailto:anryoshi@user.sourceforge.net">Antti Koivunen"</a>
