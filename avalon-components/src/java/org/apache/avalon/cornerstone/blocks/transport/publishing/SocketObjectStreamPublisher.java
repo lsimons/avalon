98206322499ad90ae0fb35ce00a5d8dad53d41cc
hammant <hammant@13f79535-47bb-0310-9956-ffa450edef68>
2002-01-09 22:48:45 +0000

    Use cornerstone's own Connection Manager
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@12671 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-components/src/java/org/apache/avalon/cornerstone/blocks/transport/publishing/SocketObjectStreamPublisher.java
+++ avalon-components/src/java/org/apache/avalon/cornerstone/blocks/transport/publishing/SocketObjectStreamPublisher.java
@@ -8,11 +8,21 @@
  */
 package org.apache.avalon.cornerstone.blocks.transport.publishing;
 
-
-
 import org.apache.avalon.framework.configuration.Configuration;
 import org.apache.avalon.framework.configuration.ConfigurationException;
-import org.apache.commons.altrmi.server.impl.socket.PlainSocketServer;
+import org.apache.avalon.framework.component.Composable;
+import org.apache.avalon.framework.component.ComponentManager;
+import org.apache.avalon.framework.component.ComponentException;
+import org.apache.avalon.cornerstone.services.sockets.SocketManager;
+import org.apache.avalon.cornerstone.services.sockets.ServerSocketFactory;
+import org.apache.avalon.cornerstone.services.connection.ConnectionManager;
+import org.apache.avalon.cornerstone.services.connection.ConnectionHandlerFactory;
+import org.apache.avalon.cornerstone.services.connection.ConnectionHandler;
+import org.apache.commons.altrmi.server.impl.socket.PartialSocketObjectStreamServer;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.net.ServerSocket;
 
 
 /**
@@ -20,11 +30,14 @@ import org.apache.commons.altrmi.server.impl.socket.PlainSocketServer;
  *
  *
  * @author Paul Hammant <a href="mailto:Paul_Hammant@yahoo.com">Paul_Hammant@yahoo.com</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
-public class SocketObjectStreamPublisher extends AbstractPublisher {
+public class SocketObjectStreamPublisher extends AbstractPublisher implements Composable, ConnectionHandlerFactory {
 
-    private int mPort;
+    private SocketManager m_socketManager;
+    private ConnectionManager m_connectionManager;
+    private int m_port;
+    private InetAddress m_bindTo;
 
     /**
      * Pass the <code>Configuration</code> to the <code>Configurable</code>
@@ -36,8 +49,42 @@ public class SocketObjectStreamPublisher extends AbstractPublisher {
     public void configure(Configuration configuration) throws ConfigurationException {
 
         super.configure(configuration);
+        m_port = configuration.getChild("port").getValueAsInteger();
+        try
+        {
+            final String bindAddress = configuration.getChild( "bind" ).getValue();
+            m_bindTo = InetAddress.getByName( bindAddress );
+        }
+        catch( final UnknownHostException unhe )
+        {
+            throw new ConfigurationException( "Malformed bind parameter", unhe );
+        }
 
-        mPort = configuration.getChild("port").getValueAsInteger();
+    }
+
+    public void compose(ComponentManager manager) throws ComponentException {
+        m_socketManager = (SocketManager)manager.lookup( SocketManager.ROLE );
+        m_connectionManager = (ConnectionManager)manager.lookup( ConnectionManager.ROLE );
+    }
+
+    /**
+     * Construct an appropriate ConnectionHandler.
+     *
+     * @return the new ConnectionHandler
+     * @exception Exception if an error occurs
+     */
+    public ConnectionHandler createConnectionHandler()
+            throws Exception {
+        final SocketObjectStreamConnectionHandler handler = new SocketObjectStreamConnectionHandler( (PartialSocketObjectStreamServer) mAltrmiServer );
+        setupLogger( handler );
+        return handler;
+    }
+
+    /**
+     * Release a previously created ConnectionHandler.
+     * e.g. for spooling.
+     */
+    public void releaseConnectionHandler(ConnectionHandler connectionHandler) {
     }
 
     /**
@@ -49,8 +96,15 @@ public class SocketObjectStreamPublisher extends AbstractPublisher {
      */
     public void initialize() throws Exception {
 
-        mAltrmiServer = new PlainSocketServer(mPort);
+        mAltrmiServer = new PartialSocketObjectStreamServer();
 
         super.initialize();
+
+        final ServerSocketFactory factory =
+            m_socketManager.getServerSocketFactory( "plain" );
+        final ServerSocket serverSocket = factory.createServerSocket( m_port, 5, m_bindTo );
+
+        m_connectionManager.connect( "SocketObjectStreamListener", serverSocket, this );
+
     }
 }
