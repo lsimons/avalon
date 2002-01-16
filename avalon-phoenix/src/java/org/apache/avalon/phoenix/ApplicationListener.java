bf35ac19d64434fae132472ba8f324e63336d46a
hammant <hammant@13f79535-47bb-0310-9956-ffa450edef68>
2002-01-16 13:48:15 +0000

    new interfaces to support Application listening. Peter to approve.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@12713 13f79535-47bb-0310-9956-ffa450edef68

---
+++ avalon-phoenix/src/java/org/apache/avalon/phoenix/ApplicationListener.java
@@ -0,0 +1,64 @@
+
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included  with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.apache.avalon.phoenix;
+
+/**
+ * Implementations of this interface receive notifications about
+ * changes to the state of Application.
+ * The implementation <em>must</em> have a zero argument
+ * constructor and is instantiated before any other component of the Server
+ * Application. To receive notification events, the implementation class
+ * should be specified in the <code>assembly.xml</code> descriptor.
+ *
+ * @author <a href="mailto:peter@apache.org">Peter Donald</a>
+ */
+public interface ApplicationListener extends BlockListener
+{
+
+    /**
+     * Notification that an application is being started.
+     *
+     * @param event the ApplicationEvent
+     *
+     */
+    void applicationStarting(ApplicationEvent applicationEvent);
+
+    /**
+     * Notification that an application has now started.
+     *
+     *
+     */
+    void applicationStarted();
+
+    /**
+     * Notification that an application is being stopped.
+     *
+     *
+     */
+    void applicationStopping();
+
+    /**
+     * Notification that an application has stopped.
+     *
+     *
+     */
+    void applicationStopped();
+
+    /**
+     * Notification that an application has failed at some moment.
+     * This is for information only as Phoenix will do the right
+     * thing for correct shutdown both before and after this method
+     * is called.  The user of this method should NOT call System.exit()
+     *
+     *
+     * @param causeOfFailure
+     *
+     */
+    void applicationFailure(Exception causeOfFailure);
+}
