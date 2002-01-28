f4dc83c7a6e964090f5106fe6a7f974dc15cb696
bloritsch <bloritsch@13f79535-47bb-0310-9956-ffa450edef68>
2002-01-28 15:15:34 +0000

    start of CommandManager support
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@12759 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-excalibur/event/site/java/org/apache/excalibur/event/command/Command.java
+++ avalon-excalibur/event/site/java/org/apache/excalibur/event/command/Command.java
@@ -5,9 +5,10 @@
  * version 1.1, a copy of which has been included with this distribution in
  * the LICENSE.txt file.
  */
-package org.apache.avalon.excalibur.event;
+package org.apache.avalon.excalibur.command;
 
 import org.apache.avalon.framework.activity.Executable;
+import org.apache.avalon.excalibur.event.Signal;
 
 /**
  * A Signal is a specific type of QueueElement that denotes a Control code for
