a2f3d7d7b81b189d4fdcfabebac1cee875133bea
bloritsch <bloritsch@13f79535-47bb-0310-9956-ffa450edef68>
2002-01-28 15:43:28 +0000

    add EventHandler interface here
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@12761 13f79535-47bb-0310-9956-ffa450edef68

---
+++ avalon-excalibur/event/src/java/org/apache/excalibur/event/EventHandler.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.apache.avalon.excalibur.event;
+
+import org.apache.avalon.framework.activity.Executable;
+import org.apache.avalon.excalibur.event.Signal;
+
+/**
+ * An EventHandler takes care of processing specific events in an Event Based
+ * architecture.
+ *
+ * @author <a href="mailto:bloritsch@apache.org">Berin Loritsch</a>
+ */
+public interface EventHandler
+{
+    /**
+     * Handle one event at a time.
+     */
+    handleEvent( QueueElement element );
+
+    /**
+     * Handle a whole array of events at a time.
+     */
+    handleEvents( QueueElement[] elements );
+}
