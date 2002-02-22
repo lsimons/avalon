137075daee76cd53e2af337a456984786074f977
bloritsch <bloritsch@13f79535-47bb-0310-9956-ffa450edef68>
2002-02-22 15:17:52 +0000

    provide test for windows environment so that the separators are correct
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@12873 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-excalibur/test.sh
+++ avalon-excalibur/test.sh
@@ -2,6 +2,16 @@
 
 LOCAL_AVALON_TOOLS=$AVALON_TOOLS
 
+S=":"
+
+if [ "$TERM" = "dumb" ] ; then
+    S=";"
+fi
+
+if [ "$TERM" = "cygwin" ] ; then
+    S=";"
+fi
+
 if [ "$AVALON_TOOLS" = "" ] ; then
     if [ -d ../jakarta-avalon/tools ] ; then
         LOCAL_AVALON_TOOLS=../jakarta-avalon/tools
@@ -15,18 +25,18 @@ if [ "$AVALON_TOOLS" = "" ] ; then
     fi
 fi
 
-CLASSPATH="./build/scratchpad/;./build/classes/;./build/testclasses/;$CLASSPATH"
+CLASSPATH="./build/scratchpad/$S./build/classes/$S./build/testclasses/$S$CLASSPATH"
 
 for i in $LOCAL_AVALON_TOOLS/ext/*.jar; do
-    CLASSPATH="$CLASSPATH;$i"
+    CLASSPATH="$CLASSPATH$S$i"
 done
 
 for i in $LOCAL_AVALON_TOOLS/lib/*.jar; do
-    CLASSPATH="$CLASSPATH;$i"
+    CLASSPATH="$CLASSPATH$S$i"
 done
 
 for i in lib/*.jar; do
-    CLASSPATH="$CLASSPATH;$i"
+    CLASSPATH="$CLASSPATH$S$i"
 done
 
 java -classpath $CLASSPATH $@
