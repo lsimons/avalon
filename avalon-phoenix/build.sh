5385e7f0265105126ce055a19923a4f91c5221c8
jefft <jefft@13f79535-47bb-0310-9956-ffa450edef68>
2002-03-24 01:39:31 +0000

    Synch build scripts with jakarta-avalon
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@13029 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-phoenix/build.sh
+++ avalon-phoenix/build.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 
+# Script for invoking the Ant bundled with jakarta-avalon, from Avalon
+# projects. Location-independent; will run build.xml in whichever directory
+# you're in.
+
+BASE=`dirname $0`  # Directory containing this script. Not the same as $PWD.
+                   # The rest of this script assumes it's in a directory
+                   # at the level of jakarta-avalon
+
 LOCAL_AVALON_TOOLS=$AVALON_TOOLS
 
 if [ "$AVALON_TOOLS" = "" ] ; then
-    if [ -d ../jakarta-avalon/tools ] ; then
-        LOCAL_AVALON_TOOLS=../jakarta-avalon/tools
+    # Absolutize directory; it's not safe to pass relative dirs to buildfiles
+    MAYBE_TOOLS=$PWD/$BASE/../jakarta-avalon/tools 
+    if [ -d $MAYBE_TOOLS ] ; then
+        LOCAL_AVALON_TOOLS=$MAYBE_TOOLS
     elif [ -d tools ] ; then
-        LOCAL_AVALON_TOOLS=tools
+        LOCAL_AVALON_TOOLS=$PWD/$BASE/tools
     else
         echo "Unable to locate tools directory at "
-        echo "../jakarta-avalon/tools/ or tools/. "
+        echo "$MAYBE_TOOLS or $BASE/tools/. "
         echo "Aborting."
         exit 1
     fi
