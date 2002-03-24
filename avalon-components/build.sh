b77968e15710da5daa98a2a8c536922652f9a181
jefft <jefft@13f79535-47bb-0310-9956-ffa450edef68>
2002-03-24 01:01:43 +0000

    Synch build scripts with jakarta-avalon
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@13028 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-components/build.sh
+++ avalon-components/build.sh
@@ -1,24 +1,31 @@
 #!/bin/sh
 
-echo
-echo "Cornerstone Build System"
-echo "------------------------"
+# Script for invoking the Ant bundled with jakarta-avalon, from Avalon
+# projects. Location-independent; will run build.xml in whichever directory
+# you're in.
 
+BASE=`dirname $0`  # Directory containing this script. Not the same as $PWD.
+                   # The rest of this script assumes it's in a directory
+                   # at the level of jakarta-avalon
+
+LOCAL_AVALON_TOOLS=$AVALON_TOOLS
 
 if [ "$AVALON_TOOLS" = "" ] ; then
-    if [ -d ../jakarta-avalon/tools ] ; then 
-        AVALON_TOOLS=../jakarta-avalon/tools
-    elif [ -d tools ] ; then 
-        AVALON_TOOLS=tools
+    # Absolutize directory; it's not safe to pass relative dirs to buildfiles
+    MAYBE_TOOLS=$PWD/$BASE/../jakarta-avalon/tools 
+    if [ -d $MAYBE_TOOLS ] ; then
+        LOCAL_AVALON_TOOLS=$MAYBE_TOOLS
+    elif [ -d tools ] ; then
+        LOCAL_AVALON_TOOLS=$PWD/$BASE/tools
     else
         echo "Unable to locate tools directory at "
-        echo "../jakarta-avalon/tools/ or tools/. "
+        echo "$MAYBE_TOOLS or $BASE/tools/. "
         echo "Aborting."
         exit 1
     fi
 fi
 
-chmod u+x $AVALON_TOOLS/bin/antRun
-chmod u+x $AVALON_TOOLS/bin/ant
+chmod u+x $LOCAL_AVALON_TOOLS/bin/antRun
+chmod u+x $LOCAL_AVALON_TOOLS/bin/ant
 
-$AVALON_TOOLS/bin/ant -logger org.apache.tools.ant.NoBannerLogger -emacs -Dtools.dir=$AVALON_TOOLS $1 -Dapps-target=$2 $3 $4 $5 $6 $7 $8
+$LOCAL_AVALON_TOOLS/bin/ant -logger org.apache.tools.ant.NoBannerLogger -emacs -Dtools.dir=$LOCAL_AVALON_TOOLS $@
--- avalon-logkit/build.bat
+++ avalon-logkit/build.bat
@@ -1,13 +1,26 @@
 @echo off
 
-if exist "tools\bin\ant.bat" set LOCAL_AVALON_TOOLS=tools
-if exist "..\jakarta-avalon\tools\bin\ant.bat" set LOCAL_AVALON_TOOLS=..\jakarta-avalon\tools
+rem Script to invoke the Ant that comes with jakarta-avalon
+rem
+rem Tries to guess location of jakarta-avalon\tools directory containing Ant.
+rem To specify this directly, set the AVALON_TOOLS variable, eg:
+rem set AVALON_TOOLS=c:\Jakarta\jakarta-avalon
+rem
+rem This can also be called from other batch scripts. If so, the BASE variable
+rem should be preset to the path from the caller to this script (generally
+rem '..\'s)
+
+
+if "%BASE%"=="" set BASE=.
+if exist "%BASE%\tools\bin\ant.bat" set LOCAL_AVALON_TOOLS=tools
+if exist "%BASE%\..\jakarta-avalon\tools\bin\ant.bat" set LOCAL_AVALON_TOOLS=%BASE%\..\jakarta-avalon\tools
+if exist "%BASE%\..\..\jakarta-avalon\tools\bin\ant.bat" set LOCAL_AVALON_TOOLS=%BASE%\..\..\jakarta-avalon\tools
 if not "%AVALON_TOOLS%"=="" set LOCAL_AVALON_TOOLS=%AVALON_TOOLS%
 
 if not "%LOCAL_AVALON_TOOLS%"=="" goto runAnt
 
 echo "Unable to locate tools directory at "
-echo "../jakarta-avalon/tools/ or tools/. "
+echo "..\jakarta-avalon\tools or ..\..\jakarta-avalon\tools or tools\. "
 echo "Aborting."
 goto end
 
@@ -20,3 +33,4 @@ set OLD_ANT_HOME=
 
 :end
 set LOCAL_AVALON_TOOLS=
+set BASE=
