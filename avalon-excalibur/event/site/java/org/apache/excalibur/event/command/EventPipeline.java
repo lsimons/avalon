563463dbfd23167dec85837e8ac20fa33e93abe1
bloritsch <bloritsch@13f79535-47bb-0310-9956-ffa450edef68>
2002-04-11 17:36:23 +0000

    Move command to a child of event as we agreed upon much earlier
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@13339 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-excalibur/event/site/java/org/apache/excalibur/event/command/EventPipeline.java
+++ avalon-excalibur/event/site/java/org/apache/excalibur/event/command/EventPipeline.java
@@ -5,7 +5,7 @@
  * version 1.1, a copy of which has been included with this distribution in
  * the LICENSE.txt file.
  */
-package org.apache.avalon.excalibur.command;
+package org.apache.avalon.excalibur.event.command;
 
 import org.apache.avalon.excalibur.event.EventHandler;
 import org.apache.avalon.excalibur.event.Source;
