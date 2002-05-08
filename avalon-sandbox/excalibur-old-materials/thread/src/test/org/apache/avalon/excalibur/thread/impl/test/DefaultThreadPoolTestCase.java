784f66b96856433564d77501cb931706adcd8506
colus <colus@13f79535-47bb-0310-9956-ffa450edef68>
2002-05-08 11:02:34 +0000

    TestCase for DefaultThreadPool.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@13511 13f79535-47bb-0310-9956-ffa450edef68

---
+++ avalon-sandbox/excalibur-old-materials/thread/src/test/org/apache/avalon/excalibur/thread/impl/test/DefaultThreadPoolTestCase.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.apache.avalon.excalibur.thread.impl.test;
+
+import org.apache.avalon.framework.logger.ConsoleLogger;
+import org.apache.avalon.excalibur.thread.ThreadPool;
+import org.apache.avalon.excalibur.thread.impl.DefaultThreadPool;
+import org.apache.excalibur.threadcontext.ThreadContext;
+import junit.framework.TestCase;
+
+/**
+ * TestCase for DefaultThreadPool.
+ *
+ * @author <a href="mailto:colus@apache.org">Eung-ju Park</a>
+ */
+public class DefaultThreadPoolTestCase
+    extends TestCase
+{
+    public DefaultThreadPoolTestCase( final String name )
+    {
+        super( name );
+    }
+
+    public void testWithThreadContext()
+        throws Exception
+    {
+        final ThreadContext threadContext = ThreadContext.getThreadContext();
+        final DefaultThreadPool pool = new DefaultThreadPool( "default", 10, threadContext );
+        pool.setDaemon( false );
+        pool.enableLogging( new ConsoleLogger() );
+        pool.execute( new DummyRunnable() );
+    }
+
+    private static class DummyRunnable
+        implements Runnable
+    {
+        public void run()
+        {
+        }
+    }
+}
