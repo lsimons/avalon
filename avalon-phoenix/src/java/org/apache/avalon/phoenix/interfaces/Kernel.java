db662e8a5a4b974347754349e17f53c9784bbc44
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2002-05-10 13:10:07 +0000

    Remove unused imports
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@13539 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-phoenix/src/java/org/apache/avalon/phoenix/interfaces/Kernel.java
+++ avalon-phoenix/src/java/org/apache/avalon/phoenix/interfaces/Kernel.java
@@ -7,8 +7,6 @@
  */
 package org.apache.avalon.phoenix.interfaces;
 
-import org.apache.avalon.framework.activity.Disposable;
-import org.apache.avalon.framework.activity.Initializable;
 import org.apache.avalon.framework.configuration.Configuration;
 import org.apache.avalon.phoenix.metadata.SarMetaData;
 import org.apache.log.Hierarchy;
