737f43222feebdc863b5a9e4b8e4d04284c7de3d
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2002-05-11 06:25:40 +0000

    No need to extend AbstractLogEnabled as it does no logging.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@13556 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-phoenix/src/java/org/apache/avalon/phoenix/components/classloader/ContextClassLoaderManager.java
+++ avalon-phoenix/src/java/org/apache/avalon/phoenix/components/classloader/ContextClassLoaderManager.java
@@ -2,14 +2,15 @@ package org.apache.avalon.phoenix.components.classloader;
 
 import java.io.File;
 import org.apache.avalon.framework.configuration.Configuration;
-import org.apache.avalon.framework.logger.AbstractLogEnabled;
 import org.apache.avalon.phoenix.interfaces.ClassLoaderManager;
 
 /**
+ * Basic ClassLoaderManager that just returns current
+ * ContextClassLoader.
+ *
  * @author <a href="mailto:colus@apache.org">Eung-ju Park</a>
  */
 public class ContextClassLoaderManager
-    extends AbstractLogEnabled
     implements ClassLoaderManager
 {
     public ClassLoader createClassLoader( final Configuration server,
