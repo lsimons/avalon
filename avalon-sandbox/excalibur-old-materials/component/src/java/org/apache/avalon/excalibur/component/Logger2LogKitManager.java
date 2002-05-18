f1d287be915023062aa784d3ec400563ff1a6765
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2002-05-18 05:55:04 +0000

    Add in some adapters between LogkitManager and LoggerManager
    git-svn-id: https://svn.apache.org/repos/asf/avalon/branches/ECM_LOGENABLED@13650 13f79535-47bb-0310-9956-ffa450edef68

---
+++ avalon-sandbox/excalibur-old-materials/component/src/java/org/apache/avalon/excalibur/component/Logger2LogKitManager.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included  with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.apache.avalon.excalibur.component;
+
+import org.apache.avalon.excalibur.logger.LogKitManager;
+import org.apache.avalon.excalibur.logger.LoggerManager;
+import org.apache.avalon.framework.logger.Logger;
+import org.apache.log.Hierarchy;
+import org.apache.log.LogTarget;
+
+/**
+ * An adaptor between LogkitManager and LoggerManager.
+ *
+ * @author <a href="mailto:peter@apache.org">Peter Donald</a>
+ * @version $Revision: 1.1.2.1 $ $Date: 2002/05/18 05:55:04 $
+ */
+public class Logger2LogKitManager
+    implements LogKitManager
+{
+    private final Hierarchy m_hierarchy = new Hierarchy();
+    private final LoggerManager m_loggerManager;
+
+    public Logger2LogKitManager( final LoggerManager loggerManager )
+    {
+        m_loggerManager = loggerManager;
+    }
+
+    public org.apache.log.Logger getLogger( final String categoryName )
+    {
+        final Logger logger =
+            m_loggerManager.getLoggerForCategory( categoryName );
+        final org.apache.log.Logger logkitLogger =
+            getHierarchy().getLoggerFor( categoryName );
+        final LogKit2LoggerTarget target =
+            new LogKit2LoggerTarget( logger );
+        logkitLogger.setLogTargets( new LogTarget[ ] { target } );
+        return logkitLogger;
+    }
+
+    public Hierarchy getHierarchy()
+    {
+        return m_hierarchy;
+    }
+}
