3823560d007068989ebb1a7333f973b0d6e40010
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2002-06-23 11:26:13 +0000

    Fix javadoc errors
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@13959 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-sandbox/excalibur-old-materials/sourceresolve/src/java/org/apache/excalibur/source/SourceValidity.java
+++ avalon-sandbox/excalibur-old-materials/sourceresolve/src/java/org/apache/excalibur/source/SourceValidity.java
@@ -15,18 +15,17 @@ package org.apache.excalibur.source;
  * The other possibility needs another (newer) validity object to compare
  * against (e.g. to test a last modification date).
  * To avoid testing, what the actual implementation of the validity
- * object supports, the invocation order is to first call {@link isValid()} and only if
- * this results in <code>0</code>, then to call {@link isValid(SourceValidity)}.
+ * object supports, the invocation order is to first call {@link #isValid()} and only if
+ * this results in <code>0</code>, then to call {@link #isValid}.
  * But remember to call the second isValid(SourceValidity) when <code>0</code>
  * is returned by the first invocation!
  *
  * @author <a href="mailto:cziegeler@apache.org">Carsten Ziegeler</a>
- * @version CVS $Revision: 1.2 $ $Date: 2002/06/04 08:42:13 $
+ * @version CVS $Revision: 1.3 $ $Date: 2002/06/23 11:26:13 $
  */
 public interface SourceValidity
     extends java.io.Serializable
 {
-
     /**
      * Check if the component is still valid.
      * If <code>0</code> is returned the isValid(SourceValidity) must be
