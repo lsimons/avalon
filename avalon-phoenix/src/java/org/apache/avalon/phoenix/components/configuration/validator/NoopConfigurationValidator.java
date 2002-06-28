566eea7c2765aa5520e864b6f089cbfe39449696
proyal <proyal@13f79535-47bb-0310-9956-ffa450edef68>
2002-06-28 05:14:40 +0000

    ConfigurationValidator that doesn't do a thing.
    Placeholder for testing interface hooks, or useful
    if you don't want/care for validation
    
    
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@14005 13f79535-47bb-0310-9956-ffa450edef68

---
+++ avalon-phoenix/src/java/org/apache/avalon/phoenix/components/configuration/validator/NoopConfigurationValidator.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.apache.avalon.phoenix.components.configuration.validator;
+
+import org.apache.avalon.phoenix.interfaces.ConfigurationValidator;
+import org.apache.avalon.framework.configuration.Configuration;
+import org.apache.avalon.framework.configuration.ConfigurationException;
+
+/**
+ * A ConfigurationValidator that always says everything is okay
+ *
+ * @author <a href="mailto:proyal@apache.org">Peter Royal</a>
+ */
+public class NoopConfigurationValidator implements ConfigurationValidator
+{
+    public void storeSchema( String application, String block, Configuration schema )
+      throws ConfigurationException
+    {
+    }
+
+    public boolean isValid( String application, String block, Configuration configuration )
+      throws ConfigurationException
+    {
+        return true;
+    }
+
+    public boolean isFeasiblyValid( String application, String block, Configuration configuration )
+      throws ConfigurationException
+    {
+        return true;
+    }
+}
