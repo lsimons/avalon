33ec7d22f3772e5c934e4092f474ed16ad155752
bloritsch <bloritsch@13f79535-47bb-0310-9956-ffa450edef68>
2002-08-07 23:08:26 +0000

    Update the comments so that they are formatted for printing, and have the proper header.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@14498 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-excalibur/event/site/java/org/apache/excalibur/event/DefaultQueue.java
+++ avalon-excalibur/event/site/java/org/apache/excalibur/event/DefaultQueue.java
@@ -1,10 +1,52 @@
 /*
- * Copyright (C) The Apache Software Foundation. All rights reserved.
- *
- * This software is published under the terms of the Apache Software License
- * version 1.1, a copy of which has been included with this distribution in
- * the LICENSE.txt file.
- */
+
+ ============================================================================
+                   The Apache Software License, Version 1.1
+ ============================================================================
+
+ Copyright (C) @year@ The Apache Software Foundation. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modifica-
+ tion, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of  source code must  retain the above copyright  notice,
+    this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ 3. The end-user documentation included with the redistribution, if any, must
+    include  the following  acknowledgment:  "This product includes  software
+    developed  by the  Apache Software Foundation  (http://www.apache.org/)."
+    Alternately, this  acknowledgment may  appear in the software itself,  if
+    and wherever such third-party acknowledgments normally appear.
+
+ 4. The names "Jakarta", "Avalon", "Excalibur" and "Apache Software Foundation"
+    must not be used to endorse or promote products derived from this  software
+    without  prior written permission. For written permission, please contact
+    apache@apache.org.
+
+ 5. Products  derived from this software may not  be called "Apache", nor may
+    "Apache" appear  in their name,  without prior written permission  of the
+    Apache Software Foundation.
+
+ THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
+ APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
+ DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
+ ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
+ (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ This software  consists of voluntary contributions made  by many individuals
+ on  behalf of the Apache Software  Foundation. For more  information on the
+ Apache Software Foundation, please see <http://www.apache.org/>.
+
+*/
 package org.apache.excalibur.event;
 
 import org.apache.avalon.excalibur.collections.Buffer;
@@ -13,7 +55,8 @@ import org.apache.avalon.excalibur.concurrent.Mutex;
 
 /**
  * The default queue implementation is a variable size queue.  This queue is
- * ThreadSafe, however the overhead in synchronization costs a few extra millis.
+ * ThreadSafe, however the overhead in synchronization costs a few extra
+ * millis.
  *
  * @author <a href="mailto:bloritsch@apache.org">Berin Loritsch</a>
  */
@@ -229,8 +272,8 @@ public final class DefaultQueue extends AbstractQueue
     /**
      * Removes the given number of elements from the given <code>buf</code>
      * and returns them in an array. Trusts the caller to pass in a buffer
-     * full of <code>QueueElement</code>s and with at least <code>count</code>
-     * elements available.
+     * full of <code>QueueElement</code>s and with at least
+     * <code>count</code> elements available.
      * <p>
      * @param buf to remove elements from, the caller is responsible
      *            for synchronizing access
@@ -240,7 +283,7 @@ public final class DefaultQueue extends AbstractQueue
     private static QueueElement[] retrieveElements( Buffer buf, int count )
     {
         QueueElement[] elements = new QueueElement[ count ];
- 
+
         for( int i = 0; i < count; i++ )
         {
             elements[ i ] = (QueueElement) buf.remove();
--- avalon-excalibur/event/src/java/org/apache/excalibur/event/Sink.java
+++ avalon-excalibur/event/src/java/org/apache/excalibur/event/Sink.java
@@ -1,17 +1,59 @@
 /*
- * Copyright (C) The Apache Software Foundation. All rights reserved.
- *
- * This software is published under the terms of the Apache Software License
- * version 1.1, a copy of which has been included with this distribution in
- * the LICENSE.txt file.
- */
+
+ ============================================================================
+                   The Apache Software License, Version 1.1
+ ============================================================================
+
+ Copyright (C) @year@ The Apache Software Foundation. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modifica-
+ tion, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of  source code must  retain the above copyright  notice,
+    this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ 3. The end-user documentation included with the redistribution, if any, must
+    include  the following  acknowledgment:  "This product includes  software
+    developed  by the  Apache Software Foundation  (http://www.apache.org/)."
+    Alternately, this  acknowledgment may  appear in the software itself,  if
+    and wherever such third-party acknowledgments normally appear.
+
+ 4. The names "Jakarta", "Avalon", "Excalibur" and "Apache Software Foundation"
+    must not be used to endorse or promote products derived from this  software
+    without  prior written permission. For written permission, please contact
+    apache@apache.org.
+
+ 5. Products  derived from this software may not  be called "Apache", nor may
+    "Apache" appear  in their name,  without prior written permission  of the
+    Apache Software Foundation.
+
+ THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
+ APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
+ DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
+ ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
+ (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ This software  consists of voluntary contributions made  by many individuals
+ on  behalf of the Apache Software  Foundation. For more  information on the
+ Apache Software Foundation, please see <http://www.apache.org/>.
+
+*/
 package org.apache.excalibur.event;
 
 /**
- * A Sink implements the end of a finite-length event queue where QueueElements
- * are enqueued. These operations can throw a SinkException if the sink is
- * closed or becomes full, allowing event queues to support thresholding and
- * backpressure.
+ * A Sink implements the end of a finite-length event queue where
+ * <code>QueueElement</code>s are enqueued. These operations can throw a
+ * <code>SinkException</code> if the sink is closed or becomes full, allowing
+ * event queues to support thresholding and backpressure.
  *
  * @author <a href="mailto:bloritsch@apache.org">Berin Loritsch</a>
  */
@@ -46,8 +88,8 @@ public interface Sink
         throws SinkException;
 
     /**
-     * Tries to enqueue an event, but instead of throwing exceptions, it returns
-     * a boolean value of whether the attempt was successful.
+     * Tries to enqueue an event, but instead of throwing exceptions, it
+     * returns a boolean value of whether the attempt was successful.
      *
      * @param element The element to attempt to enqueue
      * @return <code>true</code> if successful, <code>false</code> if not.
@@ -59,13 +101,14 @@ public interface Sink
      *
      * <p>This method allows a client to provisionally enqueue a number
      * of elements onto the queue, and then later commit the enqueue (with
-     * a <pre>commitEnqueue</code> call), or abort (with an
+     * a <code>commitEnqueue</code> call), or abort (with an
      * <code>abortEnqueue</code> call). This mechanism can be used to
      * perform "split-phase" enqueues, where a client first enqueues a
      * set of elements on the queue and then performs some work to "fill in"
      * those elements before performing a commit. This can also be used
      * to perform multi-queue transactional enqueue operations, with an
-     * "all-or-nothing" strategy for enqueueing events on multiple queues.</p>
+     * "all-or-nothing" strategy for enqueueing events on multiple queues.
+     * </p>
      *
      * <p>This method would generally be used in the following manner:</p>
      * <pre>
@@ -84,12 +127,12 @@ public interface Sink
      * after a prepare include either a commit or an abort.</p>
      *
      * @param elements The element array to provisionally enqueue
-     * @return A <code>PreparedEnqueue</code that may be used to commit or abort
-     *         the provisional enqueue
-     * @throws SinkFullException Indicates that the sink is temporarily full
-     *            and that the requested elements could not be provisionally
-     *            enqueued.
-     * @throws SinkClosedException Indicates that the sink is
+     * @return A <code>PreparedEnqueue</code> that may be used to commit or
+     *         abort the provisional enqueue
+     * @throws <code>SinkFullException</code> Indicates that the sink is
+     *            temporarily full and that the requested elements could not
+     *            be provisionally enqueued.
+     * @throws <code>SinkClosedException</code> Indicates that the sink is
      *            no longer being serviced.
      *
      * @see PreparedEnqueue
