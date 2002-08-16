9d202cee5cab6a6f4f05395ffdf772522b0b7bf8
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2002-08-16 10:42:12 +0000

    No need to pass true into Configuration.getChild()  as it is the default
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@14554 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-sandbox/excalibur-old-materials/xmlutil/src/java/org/apache/excalibur/xml/dom/DefaultDOMHandlerFactory.java
+++ avalon-sandbox/excalibur-old-materials/xmlutil/src/java/org/apache/excalibur/xml/dom/DefaultDOMHandlerFactory.java
@@ -7,49 +7,50 @@
  */
 package org.apache.excalibur.xml.dom;
 
-import org.w3c.dom.Document;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.sax.SAXTransformerFactory;
 import javax.xml.transform.sax.TransformerHandler;
-import javax.xml.transform.dom.DOMResult;
-import org.apache.avalon.framework.configuration.Configurable;
-import org.apache.avalon.framework.configuration.ConfigurationException;
-import org.apache.avalon.framework.configuration.Configuration;
-import org.apache.avalon.framework.logger.AbstractLogEnabled;
 import org.apache.avalon.framework.activity.Initializable;
 import org.apache.avalon.framework.component.Component;
+import org.apache.avalon.framework.configuration.Configurable;
+import org.apache.avalon.framework.configuration.Configuration;
+import org.apache.avalon.framework.configuration.ConfigurationException;
+import org.apache.avalon.framework.logger.AbstractLogEnabled;
 import org.apache.avalon.framework.thread.ThreadSafe;
+import org.w3c.dom.Document;
 
 /**
  * @author <a href="mailto:mirceatoma@apache.org">Mircea Toma</a>
- * @version CVS $Revision: 1.2 $ $Date: 2002/08/12 21:08:35 $
+ * @version CVS $Revision: 1.3 $ $Date: 2002/08/16 10:42:12 $
  */
-public class DefaultDOMHandlerFactory extends AbstractLogEnabled implements DOMHandlerFactory, Configurable, Initializable, Component, ThreadSafe
-{   
+public class DefaultDOMHandlerFactory
+    extends AbstractLogEnabled
+    implements DOMHandlerFactory, Configurable, Initializable, Component, ThreadSafe
+{
     private final SAXTransformerFactory m_transformerFactory = (SAXTransformerFactory)SAXTransformerFactory.newInstance();
     private final DocumentBuilderFactory m_documentBuilderFactory = DocumentBuilderFactory.newInstance();
     private DocumentBuilder m_documentBuilder;
     private boolean m_ignoreWhitespaces;
     private boolean m_ignoreComments;
 
-    public void configure( Configuration configuration ) throws ConfigurationException
+    public void configure( final Configuration configuration )
+        throws ConfigurationException
     {
-        m_ignoreWhitespaces = configuration.getChild( "ignore-whitespaces", true ).getValueAsBoolean( false );
-        m_ignoreComments = configuration.getChild( "ignore-comments", true ).getValueAsBoolean( false );
+        m_ignoreWhitespaces = configuration.getChild( "ignore-whitespaces" ).getValueAsBoolean( false );
+        m_ignoreComments = configuration.getChild( "ignore-comments" ).getValueAsBoolean( false );
     }
 
-    public void initialize() throws Exception    
+    public void initialize() throws Exception
     {
         m_documentBuilder = m_documentBuilderFactory.newDocumentBuilder();
     }
-    
+
     public DOMHandler createDOMHandler() throws Exception
     {
         final Document document = m_documentBuilder.newDocument();
         final TransformerHandler transformerHandler = m_transformerFactory.newTransformerHandler();
-        
+
         return new DefaultDOMHandler( transformerHandler, document, m_ignoreComments, m_ignoreWhitespaces );
-    }        
-        
+    }
 }
