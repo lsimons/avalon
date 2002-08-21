62fc15b2eed8d144c3488b4769c7352b0a15c08c
proyal <proyal@13f79535-47bb-0310-9956-ffa450edef68>
2002-08-21 18:47:02 +0000

    Throw nice error rather than NullPointerException
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@14594 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-excalibur/logger/src/java/org/apache/avalon/excalibur/logger/factory/PriorityFilterTargetFactory.java
+++ avalon-excalibur/logger/src/java/org/apache/avalon/excalibur/logger/factory/PriorityFilterTargetFactory.java
@@ -80,7 +80,7 @@ import org.apache.log.filter.PriorityFilter;
  * </p>
  *
  * @author <a href="mailto:giacomo@apache.org">Giacomo Pati</a>
- * @version CVS $Revision: 1.2 $ $Date: 2002/08/07 13:37:00 $
+ * @version CVS $Revision: 1.3 $ $Date: 2002/08/21 18:47:02 $
  * @since 4.0
  */
 public final class PriorityFilterTargetFactory
@@ -104,6 +104,13 @@ public final class PriorityFilterTargetFactory
         for( int i = 0; i < configs.length; i++ )
         {
             final LogTargetFactory factory = m_logTargetFactoryManager.getLogTargetFactory( configs[ i ].getName() );
+
+            if( null == factory )
+            {
+                throw new ConfigurationException( "Unknown target type '" + configs[ i ].getName()
+                                                  + "' at " + configs[i].getLocation() );
+            }
+
             getLogger().debug( "creating target " + configs[ i ].getName() + ": " + configs[ i ].toString() );
             final LogTarget logtarget = factory.createTarget( configs[ i ] );
             filter.addTarget( logtarget );
