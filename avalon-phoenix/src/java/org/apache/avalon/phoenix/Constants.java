1653db8af525c67c1818012cc6693858f140057c
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2002-09-06 08:46:57 +0000

    Add deprecation warning
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@14711 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-phoenix/src/java/org/apache/avalon/phoenix/Constants.java
+++ avalon-phoenix/src/java/org/apache/avalon/phoenix/Constants.java
@@ -11,12 +11,11 @@ package org.apache.avalon.phoenix;
  * Package access to kernel constants.
  *
  * @author <a href="mailto:peter at apache.org">Peter Donald</a>
+ * @deprecated User code should not be directly referencing this class
  */
 public interface Constants
 {
     String SOFTWARE = "@@NAME@@";
-
     String VERSION = "@@VERSION@@";
-
     String DATE = "@@DATE@@";
 }
