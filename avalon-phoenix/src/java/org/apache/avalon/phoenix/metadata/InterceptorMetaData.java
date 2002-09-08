6002689a480e31407504380c9e1b7e8d2917a130
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2002-09-08 12:51:50 +0000

    Add in the parsing of interceptors out from configuration file. Attach the interceptors to BlockMetaData.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/branches/unlabeled-1.1.2@14802 13f79535-47bb-0310-9956-ffa450edef68

---
+++ avalon-phoenix/src/java/org/apache/avalon/phoenix/metadata/InterceptorMetaData.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included  with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.apache.avalon.phoenix.metadata;
+
+/**
+ * @todo description
+ *
+ * @author ifedorenko
+ */
+public class InterceptorMetaData
+{
+    private final String m_classname;
+
+    public InterceptorMetaData( final String classname )
+    {
+        m_classname = classname;
+    }
+
+    public String getClassname()
+    {
+        return m_classname;
+    }
+}
