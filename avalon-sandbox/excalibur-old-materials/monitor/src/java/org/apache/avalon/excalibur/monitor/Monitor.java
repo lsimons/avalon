4d2c3279b1c9b76126959bd6f1935abc82b61f28
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2002-09-09 21:23:41 +0000

    No need for monitor to extend Component
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@14812 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-sandbox/excalibur-old-materials/monitor/src/java/org/apache/avalon/excalibur/monitor/Monitor.java
+++ avalon-sandbox/excalibur-old-materials/monitor/src/java/org/apache/avalon/excalibur/monitor/Monitor.java
@@ -7,17 +7,14 @@
  */
 package org.apache.avalon.excalibur.monitor;
 
-import org.apache.avalon.framework.component.Component;
-
 /**
  * The Monitor is used to actively check a set of resources to see if they have
  * changed.
  *
  * @author <a href="mailto:bloritsch@apache.org">Berin Loritsch</a>
- * @version $Id: Monitor.java,v 1.9 2002/09/07 12:14:01 donaldp Exp $
+ * @version $Id: Monitor.java,v 1.10 2002/09/09 21:23:41 donaldp Exp $
  */
 public interface Monitor
-    extends Component
 {
     String ROLE = Monitor.class.getName();
 
