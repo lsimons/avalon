c2eb38ac7edc9a5dd9e4328a6534838049434ff1
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2002-09-24 12:13:46 +0000

    Fix some javadocs
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@14915 13f79535-47bb-0310-9956-ffa450edef68

--- avalon/framework/src/api/org/apache/avalon/framework/service/ServiceSelector.java
+++ avalon/framework/src/api/org/apache/avalon/framework/service/ServiceSelector.java
@@ -8,8 +8,8 @@
 package org.apache.avalon.framework.service;
 
 /**
- * A <code>ServiceSelector</code> selects <code>Object</code>s based on a
- * supplied policy.  The contract is that all the <code>Object</code>s implement the
+ * A <code>ServiceSelector</code> selects {@link Object}s based on a
+ * supplied policy.  The contract is that all the {@link Object}s implement the
  * same role.
  *
  * @author <a href="mailto:bloritsch@apache.org">Berin Loritsch</a>
@@ -17,15 +17,14 @@ package org.apache.avalon.framework.service;
  * @version 1.0
  * @see org.apache.avalon.framework.service.Serviceable
  * @see org.apache.avalon.framework.service.ServiceSelector
- *
  */
 public interface ServiceSelector
 {
     /**
-     * Select the <code>Object</code> associated with the given policy.
-     * For instance, If the <code>ServiceSelector</code> has a
-     * <code>Generator</code> stored and referenced by a URL, I would use the
-     * following call:
+     * Select the {@link Object} associated with the given policy.
+     * For instance, If the {@link ServiceSelector} has a
+     * <code>Generator</code> stored and referenced by a URL, the
+     * following call could be used:
      *
      * <pre>
      * try
@@ -39,30 +38,30 @@ public interface ServiceSelector
      * }
      * </pre>
      *
-     * @param policy A criteria against which a <code>Object</code> is selected.
+     * @param policy A criteria against which a {@link Object} is selected.
      *
-     * @return an <code>Object</code> value
-     * @throws ComponentException If the requested <code>Object</code> cannot be supplied
+     * @return an {@link Object} value
+     * @throws ServiceException If the requested {@link Object} cannot be supplied
      */
     Object select( Object policy )
         throws ServiceException;
 
     /**
-     * Check to see if a <code>Object</code> exists relative to the supplied policy.
+     * Check to see if a {@link Object} exists relative to the supplied policy.
      *
-     * @param policy a <code>Object</code> containing the selection criteria
+     * @param policy a {@link Object} containing the selection criteria
      * @return True if the component is available, False if it not.
      */
     boolean isSelectable( Object policy );
 
     /**
-     * Return the <code>Object</code> when you are finished with it.  This
-     * allows the <code>ServiceSelector</code> to handle the End-Of-Life Lifecycle
-     * events associated with the <code>Object</code>.  Please note, that no
+     * Return the {@link Object} when you are finished with it.  This
+     * allows the {@link ServiceSelector} to handle the End-Of-Life Lifecycle
+     * events associated with the {@link Object}.  Please note, that no
      * Exception should be thrown at this point.  This is to allow easy use of the
      * ServiceSelector system without having to trap Exceptions on a release.
      *
-     * @param object The <code>Object</code> we are releasing.
+     * @param object The {@link Object} we are releasing.
      */
     void release( Object object );
 
