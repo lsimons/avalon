26ec72f643743e1c0138b6fc2f22573644f4ee68
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2002-09-30 23:00:43 +0000

    Add in unit test data
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@15000 13f79535-47bb-0310-9956-ffa450edef68

---
+++ avalon-phoenix/src/test/org/apache/avalon/phoenix/tools/assembler/test/data/Component3.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.apache.avalon.phoenix.tools.assembler.test.data;
+
+import org.apache.avalon.framework.service.Serviceable;
+import org.apache.avalon.framework.service.ServiceManager;
+import org.apache.avalon.framework.service.ServiceException;
+
+/**
+ * A test component.
+ *
+ * @author <a href="mailto:peter at apache.org">Peter Donald</a>
+ * @version $Revision: 1.1 $ $Date: 2002/09/30 23:00:43 $
+ */
+public class Component3
+    implements Serviceable
+{
+    public void service( final ServiceManager manager )
+        throws ServiceException
+    {
+    }
+}
---
+++ avalon-phoenix/src/test/org/apache/avalon/phoenix/tools/assembler/test/data/Service2.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.apache.avalon.phoenix.tools.assembler.test.data;
+
+/**
+ * A test service.
+ *
+ * @author <a href="mailto:peter at apache.org">Peter Donald</a>
+ * @version $Revision: 1.1 $ $Date: 2002/09/30 23:00:43 $
+ */
+public interface Service2
+{
+}
