d1dc5e45f8ef899af84b923cd2679aea6d34dd5a
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2002-10-01 06:16:25 +0000

    Update test data to actually check for expected services
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@15015 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-phoenix/src/test/org/apache/avalon/phoenix/test/data/Component3.java
+++ avalon-phoenix/src/test/org/apache/avalon/phoenix/test/data/Component3.java
@@ -10,12 +10,13 @@ package org.apache.avalon.phoenix.test.data;
 import org.apache.avalon.framework.service.Serviceable;
 import org.apache.avalon.framework.service.ServiceManager;
 import org.apache.avalon.framework.service.ServiceException;
+import java.util.Arrays;
 
 /**
  * A test component.
  *
  * @author <a href="mailto:peter at apache.org">Peter Donald</a>
- * @version $Revision: 1.2 $ $Date: 2002/10/01 03:15:19 $
+ * @version $Revision: 1.3 $ $Date: 2002/10/01 06:16:25 $
  */
 public class Component3
     implements Serviceable
@@ -23,6 +24,15 @@ public class Component3
     public void service( final ServiceManager manager )
         throws ServiceException
     {
-        manager.lookup( Service2[].class.getName() );
+        final Service2[] services =
+            (Service2[]) manager.lookup( Service2[].class.getName() );
+        System.out.println( "Passed the following services: " +
+                            Arrays.asList( services ) );
+        if( 3 != services.length )
+        {
+            final String message =
+                "Expected to get 3 services but got " + services.length;
+            throw new ServiceException( message );
+        }
     }
 }
