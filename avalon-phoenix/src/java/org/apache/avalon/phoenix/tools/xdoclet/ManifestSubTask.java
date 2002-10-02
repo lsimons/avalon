7127119969e4fae33ed5dbe1d31caa24fee9f747
hammant <hammant@13f79535-47bb-0310-9956-ffa450edef68>
2002-10-02 04:33:13 +0000

    Backwards compatibility for XDoclet xinfo generator
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@15046 13f79535-47bb-0310-9956-ffa450edef68

---
+++ avalon-phoenix/src/java/org/apache/avalon/phoenix/tools/xdoclet/ManifestSubTask.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.apache.avalon.phoenix.tools.xdoclet;
+
+/**
+ * Generates Manifest file for Blocks
+ *
+ * @author <a href="mailto:vinay_chandran@users.sourceforge.net">Vinay Chandrasekharan</a>
+ * @author <a href="mailto:peter at apache.org">Peter Donald</a>
+ * @author Paul Hammant
+ * @version $Revision: 1.12 $ $Date: 2002/10/02 04:33:13 $
+ */
+public class ManifestSubTask
+{
+    private String m_manifestFile;
+
+    public String getManifestFile()
+    {
+        return m_manifestFile;
+    }
+
+    public void setManifestFile( final String manifestFile )
+    {
+        m_manifestFile = manifestFile;
+    }
+
+}
