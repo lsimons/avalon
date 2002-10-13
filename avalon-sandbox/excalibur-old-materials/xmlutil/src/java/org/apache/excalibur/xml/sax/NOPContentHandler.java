884eb57dd20ed5bbbf8dd43fb5a15c965612ce9a
mirceatoma <mirceatoma@13f79535-47bb-0310-9956-ffa450edef68>
2002-10-13 03:21:37 +0000

    No operation ContentHanlder.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@15124 13f79535-47bb-0310-9956-ffa450edef68

---
+++ avalon-sandbox/excalibur-old-materials/xmlutil/src/java/org/apache/excalibur/xml/sax/NOPContentHandler.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.apache.excalibur.xml.sax;
+
+import org.xml.sax.ContentHandler;
+import org.xml.sax.Attributes;
+import org.xml.sax.Locator;
+import org.xml.sax.SAXException;
+
+/**
+ * This class provides default implementation of the methods specified
+ * by the <code>ContentHandler</code> interface.
+ *
+ * @author <a href="mailto:mirceatoma@apache.org">Mircea Toma</a>
+ * @version CVS $Revision: 1.1 $ $Date: 2002/10/13 03:21:37 $
+ */
+public class NOPContentHandler implements ContentHandler
+{
+    /**
+     * Receive an object for locating the origin of SAX document events.
+     * @param locator An object that can return the location of any SAX
+     *                document event.
+     */
+    public void setDocumentLocator( final Locator locator )
+    {
+    }
+
+    /**
+     * Receive notification of the beginning of a document.
+     */
+    public void startDocument()
+        throws SAXException
+    {
+    }
+
+    /**
+     * Receive notification of the end of a document.
+     */
+    public void endDocument()
+        throws SAXException
+    {
+    }
+
+    /**
+     * Begin the scope of a prefix-URI Namespace mapping.
+     *
+     * @param prefix The Namespace prefix being declared.
+     * @param uri The Namespace URI the prefix is mapped to.
+     */
+    public void startPrefixMapping( final String prefix,
+                                    final String uri )
+        throws SAXException
+    {
+    }
+
+    /**
+     * End the scope of a prefix-URI mapping.
+     *
+     * @param prefix The prefix that was being mapping.
+     */
+    public void endPrefixMapping( final String prefix )
+        throws SAXException
+    {
+    }
+
+    /**
+     * Receive notification of the beginning of an element.
+     *
+     * @param uri The Namespace URI, or the empty string if the element has no
+     *            Namespace URI or if Namespace
+     *            processing is not being performed.
+     * @param loc The local name (without prefix), or the empty string if
+     *            Namespace processing is not being performed.
+     * @param raw The raw XML 1.0 name (with prefix), or the empty string if
+     *            raw names are not available.
+     * @param a The attributes attached to the element. If there are no
+     *          attributes, it shall be an empty Attributes object.
+     */
+    public void startElement( final String uri,
+                              final String loc,
+                              final String raw,
+                              final Attributes a )
+        throws SAXException
+    {
+    }
+
+    /**
+     * Receive notification of the end of an element.
+     *
+     * @param uri The Namespace URI, or the empty string if the element has no
+     *            Namespace URI or if Namespace
+     *            processing is not being performed.
+     * @param loc The local name (without prefix), or the empty string if
+     *            Namespace processing is not being performed.
+     * @param raw The raw XML 1.0 name (with prefix), or the empty string if
+     *            raw names are not available.
+     */
+    public void endElement( final String uri,
+                            final String loc,
+                            final String raw )
+        throws SAXException
+    {
+    }
+
+    /**
+     * Receive notification of character data.
+     *
+     * @param ch The characters from the XML document.
+     * @param start The start position in the array.
+     * @param len The number of characters to read from the array.
+     */
+    public void characters( final char[] ch,
+                            final int start,
+                            final int len )
+        throws SAXException
+    {
+    }
+
+    /**
+     * Receive notification of ignorable whitespace in element content.
+     *
+     * @param ch The characters from the XML document.
+     * @param start The start position in the array.
+     * @param len The number of characters to read from the array.
+     */
+    public void ignorableWhitespace( final char[] ch,
+                                     final int start,
+                                     final int len )
+        throws SAXException
+    {
+    }
+
+    /**
+     * Receive notification of a processing instruction.
+     *
+     * @param target The processing instruction target.
+     * @param data The processing instruction data, or null if none was
+     *             supplied.
+     */
+    public void processingInstruction( final String target,
+                                       final String data )
+        throws SAXException
+    {
+    }
+
+    /**
+     * Receive notification of a skipped entity.
+     *
+     * @param name The name of the skipped entity.  If it is a  parameter
+     *             entity, the name will begin with '%'.
+     */
+    public void skippedEntity( final String name )
+        throws SAXException
+    {
+    }
+}
