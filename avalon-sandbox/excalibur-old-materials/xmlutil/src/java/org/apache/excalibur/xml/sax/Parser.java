88da5dee67517d23cfb33d89ebbcb9f65584cf9c
mirceatoma <mirceatoma@13f79535-47bb-0310-9956-ffa450edef68>
2002-10-13 03:26:09 +0000

    Move from org.apache.avalon.excalibur.xml package. Remove DOM parsing since some parsers implement only the SAX spec.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@15126 13f79535-47bb-0310-9956-ffa450edef68

---
+++ avalon-sandbox/excalibur-old-materials/xmlutil/src/java/org/apache/excalibur/xml/sax/Parser.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.apache.excalibur.xml.sax;
+
+import java.io.IOException;
+import org.apache.avalon.framework.component.Component;
+import org.w3c.dom.Document;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.ext.LexicalHandler;
+
+/**
+ * The parser can be used to parse any XML document given
+ * by a {@link InputSource} object.
+ * It can either send XML events or create a DOM from
+ * the parsed document.
+ *
+ * @author <a href="mailto:cziegeler@apache.org">Carsten Ziegeler</a>
+ * @version CVS $Revision: 1.1 $ $Date: 2002/10/13 03:26:09 $
+ */
+public interface Parser
+    extends Component
+{
+    String ROLE = Parser.class.getName();
+
+    /**
+     * Parse the {@link InputSource} and send
+     * SAX events to the consumer.
+     * Attention: the consumer can  implement the
+     * {@link LexicalHandler} as well.
+     * The parse should take care of this.
+     */
+    void parse( InputSource in, ContentHandler consumer )
+        throws SAXException, IOException;
+
+    /**
+     * Parse the {@link InputSource} and send
+     * SAX events to the content handler and
+     * the lexical handler.
+     */
+    void parse( InputSource in,
+                ContentHandler contentHandler,
+                LexicalHandler lexicalHandler )
+        throws SAXException, IOException;
+}
