5dae966db404f816129aa5a99efc20e71cc65ad1
huw <huw@13f79535-47bb-0310-9956-ffa450edef68>
2002-10-22 06:19:35 +0000

    fixed error in package name
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@15203 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-phoenix/src/java/org/apache/avalon/phoenix/tools/metagenerate/MetaGenerateTask.java
+++ avalon-phoenix/src/java/org/apache/avalon/phoenix/tools/metagenerate/MetaGenerateTask.java
@@ -5,7 +5,7 @@
  * version 1.1, a copy of which has been included with this distribution in
  * the LICENSE.txt file.
  */
-package src.java.org.apache.avalon.phoenix.tools.metagenerate;
+package org.apache.avalon.phoenix.tools.metagenerate;
 
 import com.thoughtworks.qdox.ant.AbstractQdoxTask;
 import com.thoughtworks.qdox.model.DocletTag;
