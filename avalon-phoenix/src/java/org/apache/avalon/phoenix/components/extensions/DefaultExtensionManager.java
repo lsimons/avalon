a80dc90b45f18d012023f0326543845bdfd09714
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2002-11-01 22:41:37 +0000

    Log which files are present in extensionManager after a scan
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@15263 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-phoenix/src/java/org/apache/avalon/phoenix/components/extensions/DefaultExtensionManager.java
+++ avalon-phoenix/src/java/org/apache/avalon/phoenix/components/extensions/DefaultExtensionManager.java
@@ -8,7 +8,11 @@
 package org.apache.avalon.phoenix.components.extensions;
 
 import java.io.File;
+import java.util.ArrayList;
 import org.apache.avalon.excalibur.packagemanager.ExtensionManager;
+import org.apache.avalon.excalibur.packagemanager.OptionalPackage;
+import org.apache.avalon.excalibur.i18n.Resources;
+import org.apache.avalon.excalibur.i18n.ResourceManager;
 import org.apache.avalon.framework.activity.Disposable;
 import org.apache.avalon.framework.activity.Initializable;
 import org.apache.avalon.framework.logger.LogEnabled;
@@ -20,15 +24,17 @@ import org.apache.avalon.phoenix.interfaces.ExtensionManagerMBean;
 
 /**
  * @author <a href="mailto:peter at apache.org">Peter Donald</a>
- * @version $Revision: 1.5 $ $Date: 2002/08/06 11:57:40 $
+ * @version $Revision: 1.6 $ $Date: 2002/11/01 22:41:37 $
  */
 public class DefaultExtensionManager
     extends org.apache.avalon.excalibur.packagemanager.impl.DefaultExtensionManager
     implements LogEnabled, Parameterizable, Initializable, Disposable,
     ExtensionManager, ExtensionManagerMBean
 {
-    private Logger m_logger;
+    private final static Resources REZ =
+        ResourceManager.getPackageResources( DefaultExtensionManager.class );
 
+    private Logger m_logger;
     private String m_rawPath;
 
     public void enableLogging( final Logger logger )
@@ -59,6 +65,21 @@ public class DefaultExtensionManager
     public void rescanPath()
     {
         super.scanPath();
+
+        //Display a list of packages once they have been added.
+        if( m_logger.isDebugEnabled() )
+        {
+            final ArrayList list = new ArrayList();
+            final OptionalPackage[] optionalPackages = getAllOptionalPackages();
+            for( int i = 0; i < optionalPackages.length; i++ )
+            {
+                list.add( optionalPackages[ i ].getFile() );
+            }
+
+            final String message =
+                REZ.getString( "extension.packages.notice", list );
+            m_logger.debug( message );
+        }
     }
 
     protected void debug( final String message )
