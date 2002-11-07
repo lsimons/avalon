4e00070a50578cec6a1ef4ec28d092d6117f68f6
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2002-11-07 04:58:01 +0000

    Optimize imports.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@15288 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-sandbox/tweety/src/java/org/apache/avalon/excalibur/tweety/Tweety.java
+++ avalon-sandbox/tweety/src/java/org/apache/avalon/excalibur/tweety/Tweety.java
@@ -3,34 +3,34 @@
  ============================================================================
                    The Apache Software License, Version 1.1
  ============================================================================
- 
+
  Copyright (c) 2002 The Apache Software Foundation. All rights reserved.
- 
+
  Redistribution and use in source and binary forms, with or without modifica-
  tion, are permitted provided that the following conditions are met:
- 
+
  1. Redistributions of  source code must  retain the above copyright  notice,
     this list of conditions and the following disclaimer.
- 
+
  2. Redistributions in binary form must reproduce the above copyright notice,
     this list of conditions and the following disclaimer in the documentation
     and/or other materials provided with the distribution.
- 
+
  3. The end-user documentation included with the redistribution, if any, must
     include  the following  acknowledgment:  "This product includes  software
     developed  by the  Apache Software Foundation  (http://www.apache.org/)."
     Alternately, this  acknowledgment may  appear in the software itself,  if
     and wherever such third-party acknowledgments normally appear.
 
- 4. The names "Jakarta", "Avalon", "Excalibur" and "Apache Software Foundation"  
-    must not be used to endorse or promote products derived from this  software 
-    without  prior written permission. For written permission, please contact 
+ 4. The names "Jakarta", "Avalon", "Excalibur" and "Apache Software Foundation"
+    must not be used to endorse or promote products derived from this  software
+    without  prior written permission. For written permission, please contact
     apache@apache.org.
- 
+
  5. Products  derived from this software may not  be called "Apache", nor may
     "Apache" appear  in their name,  without prior written permission  of the
     Apache Software Foundation.
- 
+
  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
@@ -41,27 +41,33 @@
  ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
  (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- 
+
  This software  consists of voluntary contributions made  by many individuals
- on  behalf of the Apache Software  Foundation. For more  information on the 
+ on  behalf of the Apache Software  Foundation. For more  information on the
  Apache Software Foundation, please see <http://www.apache.org/>.
 
 */
 
 package org.apache.avalon.excalibur.tweety;
 
-import java.io.*;
-import java.net.*;
-import java.util.*;
-
-import org.apache.avalon.framework.activity.*;
-import org.apache.avalon.framework.component.*;
-import org.apache.avalon.framework.configuration.*;
-import org.apache.avalon.framework.context.*;
-import org.apache.avalon.framework.logger.*;
-import org.apache.avalon.framework.parameters.*;
-import org.apache.avalon.framework.service.*;
-import org.apache.avalon.framework.container.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import org.apache.avalon.framework.activity.Initializable;
+import org.apache.avalon.framework.activity.Startable;
+import org.apache.avalon.framework.component.Component;
+import org.apache.avalon.framework.component.DefaultComponentManager;
+import org.apache.avalon.framework.configuration.Configurable;
+import org.apache.avalon.framework.configuration.Configuration;
+import org.apache.avalon.framework.container.ContainerUtil;
+import org.apache.avalon.framework.context.Context;
+import org.apache.avalon.framework.context.Contextualizable;
+import org.apache.avalon.framework.logger.LogEnabled;
+import org.apache.avalon.framework.logger.Logger;
+import org.apache.avalon.framework.parameters.ParameterException;
+import org.apache.avalon.framework.parameters.Parameterizable;
+import org.apache.avalon.framework.parameters.Parameters;
+import org.apache.avalon.framework.service.DefaultServiceManager;
 
 /**
  * This is the tweety engine. It is an Avalon Container, which can be used
@@ -76,7 +82,6 @@ import org.apache.avalon.framework.container.*;
  *
  * @author   <a href="mailto:nicolaken@krysalis.org">Nicola Ken Barozzi</a>
  * @author   <a href="mailto:leosimons@apache.org">Leo Simons</a>
- * @created  June 20, 2002
  * @version  1.2.1
  * @since    1.0-alpha
  * @see      <a href="http://jakarta.apache.org/avalon/excalibur/tweety">Online Tweety documentation</a>
