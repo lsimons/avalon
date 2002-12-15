99eac61b625f300c15d269a89d1266be36d4105f
cziegeler <cziegeler@13f79535-47bb-0310-9956-ffa450edef68>
2002-12-15 11:56:48 +0000

    SourceFactories now also control the release of a source. This removes all the ugly component handling stuff for source objects. Updated documentation a little bit.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@15830 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-sandbox/excalibur-old-materials/sourceresolve/src/java/org/apache/excalibur/source/SourceFactory.java
+++ avalon-sandbox/excalibur-old-materials/sourceresolve/src/java/org/apache/excalibur/source/SourceFactory.java
@@ -1,20 +1,76 @@
-/*
- * Copyright (C) The Apache Software Foundation. All rights reserved.
+/* ====================================================================
+ * The Apache Software License, Version 1.1
  *
- * This software is published under the terms of the Apache Software License
- * version 1.1, a copy of which has been included with this distribution in
- * the LICENSE.txt file.
+ * Copyright (c) 2002 The Apache Software Foundation. All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *    "This product includes software developed by the
+ *    Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software
+ *    itself, if and wherever such third-party acknowledgments
+ *    normally appear.
+ *
+ * 4. The names "Jakarta", "Avalon", and "Apache Software Foundation"
+ *    must not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation. For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
  */
 package org.apache.excalibur.source;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.Map;
+
 import org.apache.avalon.framework.component.Component;
 
 /**
+ * A source factory creates new source objects.
+ * 
+ * Source factories are used to extend the source resolving mechanism
+ * with a new protocol. A new source factory is added in order to
+ * handle a specific prototol. The {@link SourceResolver} delegates
+ * the handling of a URI containing this new protocol to the factory
+ * and the factory can created a coresponding {@link Source} object.
+ * 
  * @author <a href="mailto:cziegeler@apache.org">Carsten Ziegeler</a>
- * @version $Id: SourceFactory.java,v 1.4 2002/07/06 03:55:06 donaldp Exp $
+ * @version $Id: SourceFactory.java,v 1.5 2002/12/15 11:56:48 cziegeler Exp $
  */
 public interface SourceFactory
     extends Component
@@ -22,9 +78,20 @@ public interface SourceFactory
     String ROLE = SourceFactory.class.getName();
 
     /**
-     * Get a <code>Source</code> object.
+     * Get a {@link Source} object.
+     * The factory creates a new {@link Source} object that can be used
+     * by the application. However, when this source object is not needed
+     * anymore it has to be released again using the {@link #release(Source)}
+     * method.
+     * 
+     * @param location   The URI to resolve - this URI includes the protocol.
      * @param parameters This is optional.
      */
     Source getSource( String location, Map parameters )
         throws MalformedURLException, IOException, SourceException;
+    
+    /**
+     * Release a {@link Source} object.
+     */
+    void release( Source source );
 }
