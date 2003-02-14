f4f8eaea65119acc2206a51f46469f4592aa68dd
proyal <proyal@13f79535-47bb-0310-9956-ffa450edef68>
2003-02-14 19:43:37 +0000

    * Add full license * Remove unused lifecycle methods * Wrap all logged statements with checking to see if category is enabled * Remove output to console * General style cleanup
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@16273 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-components/src/java/org/apache/avalon/cornerstone/blocks/event/DefaultEventManager.java
+++ avalon-components/src/java/org/apache/avalon/cornerstone/blocks/event/DefaultEventManager.java
@@ -1,34 +1,64 @@
 /*
- * Copyright (C) The Apache Software Foundation. All rights reserved.
- *
- * This software is published under the terms of the Apache Software License
- * version 1.1, a copy of which has been included with this distribution in
- * the LICENSE.txt file.
- */
+
+ ============================================================================
+                   The Apache Software License, Version 1.1
+ ============================================================================
+
+ Copyright (C) @year@ The Apache Software Foundation. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modifica-
+ tion, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of  source code must  retain the above copyright  notice,
+    this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ 3. The end-user documentation included with the redistribution, if any, must
+    include  the following  acknowledgment:  "This product includes  software
+    developed  by the  Apache Software Foundation  (http://www.apache.org/)."
+    Alternately, this  acknowledgment may  appear in the software itself,  if
+    and wherever such third-party acknowledgments normally appear.
+
+ 4. The names "Jakarta", "Avalon", "Excalibur" and "Apache Software Foundation"
+    must not be used to endorse or promote products derived from this  software
+    without  prior written permission. For written permission, please contact
+    apache@apache.org.
+
+ 5. Products  derived from this software may not  be called "Apache", nor may
+    "Apache" appear  in their name,  without prior written permission  of the
+    Apache Software Foundation.
+
+ THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
+ APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
+ DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
+ ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
+ (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ This software  consists of voluntary contributions made  by many individuals
+ on  behalf of the Apache Software  Foundation. For more  information on the
+ Apache Software Foundation, please see <http://www.apache.org/>.
+
+*/
 package org.apache.avalon.cornerstone.blocks.event;
 
-import java.util.Hashtable;
 import java.util.Enumeration;
-
-import org.apache.avalon.framework.logger.AbstractLogEnabled;
-import org.apache.avalon.framework.context.Contextualizable;
-import org.apache.avalon.framework.context.Context;
-import org.apache.avalon.framework.context.ContextException;
-import org.apache.avalon.framework.configuration.Configurable;
-import org.apache.avalon.framework.configuration.Configuration;
-import org.apache.avalon.framework.configuration.ConfigurationException;
-import org.apache.avalon.framework.service.Serviceable;
-import org.apache.avalon.framework.service.ServiceManager;
-import org.apache.avalon.framework.service.ServiceException;
-import org.apache.avalon.framework.activity.Initializable;
-import org.apache.avalon.framework.activity.Disposable;
+import java.util.Hashtable;
 
 import org.apache.avalon.cornerstone.services.event.Event;
-import org.apache.avalon.cornerstone.services.event.Filter;
 import org.apache.avalon.cornerstone.services.event.EventManager;
 import org.apache.avalon.cornerstone.services.event.Publisher;
 import org.apache.avalon.cornerstone.services.event.Register;
 import org.apache.avalon.cornerstone.services.event.Subscriber;
+import org.apache.avalon.framework.activity.Initializable;
+import org.apache.avalon.framework.logger.AbstractLogEnabled;
 
 /**
  * EventManager Block
@@ -38,64 +68,42 @@ import org.apache.avalon.cornerstone.services.event.Subscriber;
  *
  * @author Mauro Talevi
  */
-public class DefaultEventManager extends AbstractLogEnabled
-    implements EventManager,
-               Contextualizable, Configurable, Serviceable, Initializable, Disposable
+public class DefaultEventManager extends AbstractLogEnabled implements EventManager
 {
-    private final String m_rootEventType = "Event";
-    private Class m_eventClass;
+    private Class m_eventClass = Event.class;
     private Publisher m_publisher = new DefaultPublisher();
     private Register m_register = new DefaultRegister();
     private Hashtable m_subscribers = new Hashtable();
 
-    public Publisher getPublisher(){
+    public Publisher getPublisher()
+    {
         return m_publisher;
     }
-    public Register getRegister(){
+
+    public Register getRegister()
+    {
         return m_register;
-    }
-
-    public void contextualize( final Context context )
-    {
-    }
-
-    public void configure( final Configuration configuration )
-        throws ConfigurationException
-    {
-    }
-
-    /**
-     * ServiceManager dependencies
-     */
-    public void service( final ServiceManager serviceManager )
-        throws ServiceException
-    {
-    }
-
-    public void initialize()
-        throws Exception
-    {
-        m_eventClass = Class.forName( m_rootEventType );
-        getLogger().info("Initialising eventClass " + m_eventClass);
-    }
-
-    public void dispose()
-    {
     }
 
     class DefaultPublisher implements Publisher
     {
         public void publish( final Event event )
         {
-            getLogger().info("Publishing event " + event.getClass());
-            System.out.println("Publishing event " + event.getClass());
-            for ( Enumeration e = m_subscribers.elements(); e.hasMoreElements(); ){
-                Subscriber subscriber = (Subscriber)e.nextElement();
-                if (subscriber.getEventType().isAssignableFrom(event.getClass())
-                && (subscriber.getFilter() != null ||
-                subscriber.getFilter().filter(event))){
-                    getLogger().info("Informing subscriber "+subscriber+" of event "+event.getClass());
-                    subscriber.inform(event);
+            if( getLogger().isInfoEnabled() )
+                getLogger().info( "Publishing event " + event.getClass() );
+
+            for( Enumeration e = m_subscribers.elements(); e.hasMoreElements(); )
+            {
+                Subscriber subscriber = ( Subscriber ) e.nextElement();
+
+                if( subscriber.getEventType().isAssignableFrom( event.getClass() )
+                    && ( subscriber.getFilter() == null || subscriber.getFilter().filter( event ) ) )
+                {
+                    if( getLogger().isInfoEnabled() )
+                        getLogger().info( "Informing subscriber " + subscriber
+                                          + " of event " + event.getClass() );
+
+                    subscriber.inform( event );
                 }
             }
         }
@@ -106,34 +114,49 @@ public class DefaultEventManager extends AbstractLogEnabled
         public void subscribe( final Subscriber subscriber )
             throws InvalidEventTypeException
         {
-            if ( !m_eventClass.isAssignableFrom( subscriber.getEventType() ) )
+            if( !m_eventClass.isAssignableFrom( subscriber.getEventType() ) )
+            {
                 throw new InvalidEventTypeException();
+            }
 
-            getLogger().info( "Subscribing event " + subscriber.getEventType().getName() );
+            if( getLogger().isInfoEnabled() )
+                getLogger().info( "Subscribing event " + subscriber.getEventType().getName() );
+
             // Add to list but prevent duplicate subscriptions
-            if ( !m_subscribers.containsKey( subscriber.getUID() ) ){
+            if( !m_subscribers.containsKey( subscriber.getUID() ) )
+            {
                 m_subscribers.put( subscriber.getUID(), subscriber );
-                getLogger().info( "Subscribed Event " + subscriber.getEventType().getName() );
-                if ( getLogger().isDebugEnabled() ){
+                if( getLogger().isInfoEnabled() )
+                    getLogger().info( "Subscribed Event " + subscriber.getEventType().getName()
+                                      + ", " + subscriber );
+
+                if( getLogger().isDebugEnabled() )
                     getLogger().debug( "Subscribers now active: " + m_subscribers.size() );
-                }
             }
         }
+
         public void unsubscribe( Subscriber subscriber )
             throws InvalidEventTypeException
         {
-            if ( !m_eventClass.isAssignableFrom( subscriber.getEventType() ) )
+            if( !m_eventClass.isAssignableFrom( subscriber.getEventType() ) )
+            {
                 throw new InvalidEventTypeException();
-            if ( m_subscribers.containsKey( subscriber.getUID() ) ){
+            }
+
+            if( m_subscribers.containsKey( subscriber.getUID() ) )
+            {
                 m_subscribers.remove( subscriber.getUID() );
-                getLogger().info( "Unsubscribed Event " + subscriber.getEventType().getName() );
-                if ( getLogger().isDebugEnabled() ){
+
+                if( getLogger().isInfoEnabled() )
+                    getLogger().info( "Unsubscribed Event " + subscriber.getEventType().getName() );
+
+                if( getLogger().isDebugEnabled() )
                     getLogger().debug( "Subscribers now active: " + m_subscribers.size() );
-                }
-            } else {
+            }
+            else
+            {
                 getLogger().warn( "Subscriber " + subscriber.getUID() + " not found" );
             }
         }
     }
-
 }
