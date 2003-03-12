1b80aeee0354f95c6c4bbdbb2a731e99ed019178
donaldp <donaldp@13f79535-47bb-0310-9956-ffa450edef68>
2003-03-12 13:01:38 +0000

    Remove away from excalibur-cli which has been axed.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@16561 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-phoenix/src/java/org/apache/avalon/phoenix/frontends/CLISetup.java
+++ avalon-phoenix/src/java/org/apache/avalon/phoenix/frontends/CLISetup.java
@@ -8,14 +8,14 @@
 package org.apache.avalon.phoenix.frontends;
 
 import java.util.List;
-import org.apache.avalon.excalibur.cli.CLArgsParser;
-import org.apache.avalon.excalibur.cli.CLOption;
-import org.apache.avalon.excalibur.cli.CLOptionDescriptor;
-import org.apache.avalon.excalibur.cli.CLUtil;
 import org.apache.avalon.excalibur.i18n.ResourceManager;
 import org.apache.avalon.excalibur.i18n.Resources;
 import org.apache.avalon.framework.parameters.Parameters;
 import org.apache.avalon.phoenix.interfaces.SystemManager;
+import org.realityforge.cli.CLArgsParser;
+import org.realityforge.cli.CLOption;
+import org.realityforge.cli.CLOptionDescriptor;
+import org.realityforge.cli.CLUtil;
 
 /**
  * The class prepare parameters based on input options.
