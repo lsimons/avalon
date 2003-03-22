77414415351e9930d8c354380e9583a5f16c01f7
leosimons <leosimons@13f79535-47bb-0310-9956-ffa450edef68>
2003-03-22 12:19:08 +0000

    revert license screwup
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@16651 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-components/src/java/org/apache/avalon/cornerstone/services/rmification/RMIfication.java
+++ avalon-components/src/java/org/apache/avalon/cornerstone/services/rmification/RMIfication.java
@@ -1,53 +1,58 @@
 /*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included with this distribution in
+ * the LICENSE file.
+ */
+package org.apache.avalon.cornerstone.services.rmification;
 
- ============================================================================
-                   The Apache Software License, Version 1.1
- ============================================================================
- 
- Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
- 
- Redistribution and use in source and binary forms, with or without modifica-
- tion, are permitted provided that the following conditions are met:
- 
- 1. Redistributions of  source code must  retain the above copyright  notice,
-    this list of conditions and the following disclaimer.
- 
- 2. Redistributions in binary form must reproduce the above copyright notice,
-    this list of conditions and the following disclaimer in the documentation
-    and/or other materials provided with the distribution.
- 
- 3. The end-user documentation included with the redistribution, if any, must
-    include  the following  acknowledgment:  "This product includes  software
-    developed  by the  Apache Software Foundation  (http://www.apache.org/)."
-    Alternately, this  acknowledgment may  appear in the software itself,  if
-    and wherever such third-party acknowledgments normally appear.
- 
- 4. The names "Jakarta", "Apache Avalon", "Avalon Cornerstone", "Avalon
-    Framework" and "Apache Software Foundation"  must not be used to endorse
-    or promote products derived  from this  software without  prior written
-    permission. For written permission, please contact apache@apache.org.
- 
- 5. Products  derived from this software may not  be called "Apache", nor may
-    "Apache" appear  in their name,  without prior written permission  of the
-    Apache Software Foundation.
- 
- THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
- INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
- APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
- INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
- DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
- OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
- ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
- (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
- THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- 
- This software  consists of voluntary contributions made  by many individuals
- on  behalf of the Apache Software  Foundation. For more  information on the 
- Apache Software Foundation, please see <http://www.apache.org/>.
- 
-*/
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
+/**
+ * This service provides a way to publish an <code>Remote<code> object via RMI.
+ *
+ * @author <a href="mailto:colus@apache.org">Eung-ju Park</a>
+ * @author Mauro Talevi
+ * @version $Revision: 1.8 $
+ */
+public interface RMIfication
+{
+    String ROLE = RMIfication.class.getName();
+
+    /**
+     * Export a Remote object
+     *
+     * @param remote the remote object to export
+     */
+    void export( Remote remote )
+        throws RemoteException;
+
+    /**
+     * Unexport a Remote object
+     *
+     * @param remote the remote object to unexport
+     */
+    void unexport( Remote remote )
+        throws RemoteException;
+
+     /**
+     * Publish a set of interfaces
+     *
+     * @param remote the remote object to publish
+     * @param publicationName The name to publish it as.
+     */
+    void publish( Remote remote, String publicationName )
+        throws RemoteException, MalformedURLException;
+
+    /**
+     * Unpublish
+     *
+     * @param publicationName the name of the object to unpublish
+     */
     void unpublish( final String publicationName )
         throws RemoteException, NotBoundException, MalformedURLException;
 }
