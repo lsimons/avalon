a685fefbe096fa1b41ca847fd85e17156b5b7354
leosimons <leosimons@13f79535-47bb-0310-9956-ffa450edef68>
2003-03-22 11:13:27 +0000

    long license. Used ant: <?xml version="1.0"?>
    <project name="Code Formatter" default="main" basedir=".">
        <property file="${user.home}/build.properties"/>
        <property file="${basedir}/../ant.properties"/>
        <property file="${basedir}/ant.properties"/>
        <property file="${user.home}/.ant.properties"/>
        <property file="${basedir}/../default.properties"/>
        <property file="${basedir}/default.properties"/>
    
        <target name="main" depends="fixlicense"/>
    
        <target name="fixlicense">
            <loadfile property="license.txt" srcFile="LICENSE.txt"/>
            <replaceregexp flags="s">
                <regexp pattern="/\*(.*)LICENSE file(.*)\*/"/>
                <substitution expression="${license.txt}"/>
                <fileset dir=".">
                    <include name="**/*.java"/>
                </fileset>
            </replaceregexp>
        </target>
    
    </project>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@16645 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-sandbox/sevak/src/java/org/apache/avalon/apps/sevak/blocks/catalina/CatalinaSevakBootstrap.java
+++ avalon-sandbox/sevak/src/java/org/apache/avalon/apps/sevak/blocks/catalina/CatalinaSevakBootstrap.java
@@ -1,50 +1,56 @@
-/*
- * Copyright (C) The Apache Software Foundation. All rights reserved.
+/* ====================================================================
+ * The Apache Software License, Version 1.1
  *
- * This software is published under the terms of the Apache Software License
- * version 1.1, a copy of which has been included with this distribution in
- * the LICENSE file.
- */
-package org.apache.avalon.apps.sevak.blocks.catalina;
-
-import java.lang.reflect.Method;
-import java.lang.reflect.InvocationTargetException;
-import org.apache.avalon.apps.sevak.Sevak;
-import org.apache.avalon.apps.sevak.SevakException;
-import org.apache.avalon.apps.sevak.MultihostSevak;
-import org.apache.avalon.framework.activity.Initializable;
-import org.apache.avalon.framework.activity.Startable;
-import org.apache.avalon.framework.configuration.Configurable;
-import org.apache.avalon.framework.configuration.Configuration;
-import org.apache.avalon.framework.configuration.ConfigurationException;
-import org.apache.avalon.framework.context.Context;
-import org.apache.avalon.framework.context.ContextException;
-import org.apache.avalon.framework.context.Contextualizable;
-import org.apache.avalon.framework.logger.AbstractLogEnabled;
-import org.apache.avalon.framework.service.ServiceManager;
-import org.apache.avalon.phoenix.BlockContext;
-
-import java.io.File;
-
-/**
- * @phoenix:block
- * @phoenix:service name="org.apache.avalon.apps.sevak.MultihostSevak"
+ * Copyright (c) 2002,2003 The Apache Software Foundation. All rights
+ * reserved.
  *
- * Catalina Sevak Bootstrapper.  This class will bootstrap all Tomcat libraries dynamically.  It also acts
- * as a Wrapper around the CatalinaSevak class which directly utilizes Tomcat libraries and therefore cannot
- * be loaded directly by Phoenix without creating hard linkages between the two.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
  *
- * Catalina Sevak provides the true service by directly manipulating Tomcat classes.  All Catalina Sevak clients
- * will call to the CatalinaSevakBootstrap which will delegate the true work to CatalinaSevak.
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
  *
- * This bootstrap approach allows Tomcat to be embedded into Phoenix directly and administrated as a standard
- * installation while providing the ability for other Phoenix services to deploy and undeploy Web Applications.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
  *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *    "This product includes software developed by the
+ *    Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software
+ *    itself, if and wherever such third-party acknowledgments
+ *    normally appear.
  *
- * @see <a href="http://jakarta.apache.org/tomcat">Tomcat Project Page</a>
+ * 4. The names "Avalon", and "Apache Software Foundation"
+ *    must not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
  *
- * @author  Daniel Krieg<dkrieg@kc.rr.com>
- * @version 1.0
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation. For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
  */
 public class CatalinaSevakBootstrap
         extends AbstractLogEnabled
