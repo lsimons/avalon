1f81c622610854ecd7dbd8ab2f31faf2bb3d112e
bloritsch <bloritsch@13f79535-47bb-0310-9956-ffa450edef68>
2003-06-12 15:17:10 +0000

    Fix GUMP build.  We need to get rid of our need on the class as it stands, and refactor it into something more friendly. Spice Classman is removing the class.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@17348 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-phoenix/src/java/org/apache/avalon/phoenix/components/classloader/DefaultLoaderResolver.java
+++ avalon-phoenix/src/java/org/apache/avalon/phoenix/components/classloader/DefaultLoaderResolver.java
@@ -54,7 +54,10 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import org.apache.avalon.phoenix.components.extensions.pkgmgr.PackageManager;
-import org.realityforge.classman.util.PathMatcher;
+import org.apache.oro.text.regex.Pattern;
+import org.apache.oro.text.regex.Perl5Compiler;
+import org.apache.oro.text.regex.MalformedPatternException;
+import org.apache.oro.text.regex.Perl5Matcher;
 
 /**
  * This resolver has all the same properties as the
@@ -63,7 +66,7 @@ import org.realityforge.classman.util.PathMatcher;
  * {@link #m_baseDirectory} value.
  *
  * @author <a href="mailto:peter at apache.org">Peter Donald</a>
- * @version $Revision: 1.2 $ $Date: 2003/04/30 10:16:48 $
+ * @version $Revision: 1.3 $ $Date: 2003/06/12 15:17:10 $
  * @deprecated Convert to ClassMan SimpleLoaderResolver when it updates
  *             dependecy to latest Excalibur-Extension
  */
@@ -369,5 +372,187 @@ class DefaultLoaderResolver
         }
 
         return buff.toString();
+    }
+
+    /**
+     * Utility class for scanning a filesystem and matching a
+     * particular set of include and exclude patterns ala ant.
+     * This particular version is taken from Spice ClassMan and
+     * it will be refactored as soon as we can.
+     *
+     * @author <a href="mailto:peter at apache.org">Peter Donald</a>
+     */
+    public final class PathMatcher
+    {
+        /**
+         * The set of patterns that indicate paths to include.
+         */
+        private final Pattern[] m_includes;
+
+        /**
+         * The set of patterns that indicate paths to exclude.
+         * Takes precedence over includes.
+         */
+        private final Pattern[] m_excludes;
+
+        /**
+         * The object that is capable of performing the matching.
+         */
+        private final Perl5Matcher m_matcher = new Perl5Matcher();
+
+        /**
+         * Construct a matcher from ant style includes/excludes.
+         *
+         * @param includes the set of includes
+         * @param excludes the set of excludes
+         */
+        public PathMatcher( final String[] includes,
+                            final String[] excludes )
+        {
+            if ( null == includes )
+            {
+                throw new NullPointerException( "includes" );
+            }
+            if ( null == excludes )
+            {
+                throw new NullPointerException( "excludes" );
+            }
+
+            m_includes = toPatterns( includes );
+            m_excludes = toPatterns( excludes );
+        }
+
+        /**
+         * Test if a virtual path matches any of the ant style
+         * patterns specified in this objects constructor.
+         *
+         * @param vPath the virtual path string
+         * @return true if matches, false otherwise
+         */
+        public boolean match( final String vPath )
+        {
+            if ( isExcluded( vPath ) )
+            {
+                return false;
+            }
+            else if ( isIncluded( vPath ) )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /**
+         * Determine if specified path is excludes by patterns.
+         *
+         * @param vPath the virtual path
+         * @return true if excluded
+         */
+        private boolean isExcluded( final String vPath )
+        {
+            return testMatch( m_excludes, vPath );
+        }
+
+        /**
+         * Determine if specified path is includes by patterns.
+         *
+         * @param vPath the virtual path
+         * @return true if included
+         */
+        private boolean isIncluded( final String vPath )
+        {
+            return testMatch( m_includes, vPath );
+        }
+
+        /**
+         * Determine whether the virtual path matches
+         * specified patterns.
+         *
+         * @param patterns the patterns
+         * @param vPath the virtual path
+         * @return true if matches, false otherwise
+         */
+        private boolean testMatch( final Pattern[] patterns,
+                                   final String vPath )
+        {
+            for ( int i = 0; i < patterns.length; i++ )
+            {
+                if ( m_matcher.matches( vPath, patterns[i] ) )
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /**
+         * Convert a set of ant patterns into ORO Pattern objects.
+         *
+         * @param strs the ant style patterns
+         * @return the ORO Pattern objects
+         */
+        private Pattern[] toPatterns( final String[] strs )
+        {
+            final Perl5Compiler compiler = new Perl5Compiler();
+            final Pattern[] patterns = new Pattern[strs.length];
+            for ( int i = 0; i < patterns.length; i++ )
+            {
+                final String perlPatternStr = toPerlPatternStr( strs[i] );
+                try
+                {
+                    patterns[i] = compiler.compile( perlPatternStr );
+                }
+                catch ( final MalformedPatternException mpe )
+                {
+                    throw new IllegalArgumentException( mpe.toString() );
+                }
+            }
+            return patterns;
+        }
+
+        /**
+         * Convert an ant style fileset pattern to a Perl pattern.
+         *
+         * @param str the ant style pattern
+         * @return the perl pattern
+         */
+        private String toPerlPatternStr( String str )
+        {
+            final StringBuffer sb = new StringBuffer();
+            final int size = str.length();
+
+            for ( int i = 0; i < size; i++ )
+            {
+                final char ch = str.charAt( i );
+                if ( '.' == ch || '/' == ch || '\\' == ch )
+                {
+                    sb.append( '\\' );
+                    sb.append( ch );
+                }
+                else if ( '*' == ch )
+                {
+                    if ( ( i + 2 ) < size &&
+                            '*' == str.charAt( i + 1 ) &&
+                            '/' == str.charAt( i + 2 ) )
+                    {
+                        sb.append( "([^/]*/)*" );
+                        i += 2;
+                    }
+                    else
+                    {
+                        sb.append( "[^/]*" );
+                    }
+                }
+                else
+                {
+                    sb.append( ch );
+                }
+            }
+
+            return sb.toString();
+        }
     }
 }
