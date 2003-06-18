44cb26061fad3b96f51c158890733f7edba93c2c
bloritsch <bloritsch@13f79535-47bb-0310-9956-ffa450edef68>
2003-06-18 20:11:27 +0000

    Update the distrubution signing script to reflect the relative location for distros with Maven
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@17409 13f79535-47bb-0310-9956-ffa450edef68

--- avalon/tools/bin/sign-distributions.sh
+++ avalon/tools/bin/sign-distributions.sh
@@ -3,12 +3,12 @@
 echo -n "Enter gpg password (default key): "
 read PASSWORD
 
-for i in distributions/*.gz; do 
+for i in target/distributions/*.gz; do
  rm -f $i.asc
- echo $PASSWORD | gpg --armour --detach-sign --passphrase-fd 0 --sign $i; 
+ echo $PASSWORD | gpg --armour --detach-sign --passphrase-fd 0 --sign $i;
 done;
 
-for i in distributions/*.zip; do 
+for i in target/distributions/*.zip; do
  rm -f $i.asc
- echo $PASSWORD | gpg --armour --detach-sign --passphrase-fd 0 --sign $i; 
+ echo $PASSWORD | gpg --armour --detach-sign --passphrase-fd 0 --sign $i;
 done;
