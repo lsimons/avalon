1e5077d0ac1f9cbe0343217181526ef6323ffff8
mcconnell <mcconnell@13f79535-47bb-0310-9956-ffa450edef68>
2003-06-19 16:57:05 +0000

    Initial revision
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@17418 13f79535-47bb-0310-9956-ffa450edef68

---
+++ avalon-components/connection-impl/src/java/org/apache/avalon/cornerstone/blocks/connection/DefaultConnectionManager.java
@@ -0,0 +1,199 @@
+/*
+
+ ============================================================================
+                   The Apache Software License, Version 1.1
+ ============================================================================
+
+ Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modifica-
+ tion, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of  source code must  retain the above copyright  notice,
+    this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ 3. The end-user documentation included with the redistribution, if any, must
+    include  the following  acknowledgment:  "This product includes  software
+    developed  by the  Apache Software Foundation  (http://www.apache.org/)."
+    Alternately, this  acknowledgment may  appear in the software itself,  if
+    and wherever such third-party acknowledgments normally appear.
+
+ 4. The names "Jakarta", "Apache Avalon", "Avalon Cornerstone", "Avalon
+    Framework" and "Apache Software Foundation"  must not be used to endorse
+    or promote products derived  from this  software without  prior written
+    permission. For written permission, please contact apache@apache.org.
+
+ 5. Products  derived from this software may not  be called "Apache", nor may
+    "Apache" appear  in their name,  without prior written permission  of the
+    Apache Software Foundation.
+
+ THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
+ APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
+ DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
+ ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
+ (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ This software  consists of voluntary contributions made  by many individuals
+ on  behalf of the Apache Software  Foundation. For more  information on the
+ Apache Software Foundation, please see <http://www.apache.org/>.
+
+*/
+
+package org.apache.avalon.cornerstone.blocks.connection;
+
+import java.net.ServerSocket;
+import java.util.HashMap;
+import org.apache.avalon.cornerstone.services.connection.ConnectionHandlerFactory;
+import org.apache.avalon.cornerstone.services.connection.ConnectionManager;
+import org.apache.avalon.cornerstone.services.threads.ThreadManager;
+import org.apache.avalon.excalibur.thread.ThreadPool;
+import org.apache.avalon.framework.activity.Disposable;
+import org.apache.avalon.framework.logger.AbstractLogEnabled;
+import org.apache.avalon.framework.service.ServiceException;
+import org.apache.avalon.framework.service.ServiceManager;
+import org.apache.avalon.framework.service.Serviceable;
+
+/**
+ * This is the service through which ConnectionManagement occurs.
+ *
+ * @author <a href="mailto:dev@avalon.apache.org">Avalon Development Team</a>
+ */
+public class DefaultConnectionManager
+    extends AbstractLogEnabled
+    implements ConnectionManager, Serviceable, Disposable
+{
+    private final HashMap m_connections = new HashMap();
+    private ThreadManager m_threadManager;
+
+    /**
+     */
+    public void service( final ServiceManager serviceManager )
+        throws ServiceException
+    {
+        m_threadManager = (ThreadManager)serviceManager.lookup( ThreadManager.ROLE );
+    }
+
+    public void dispose()
+    {
+        if( getLogger().isDebugEnabled() )
+        {
+            getLogger().debug( "disposal" );
+        }
+        final String[] names = (String[])m_connections.keySet().toArray( new String[ 0 ] );
+        for( int i = 0; i < names.length; i++ )
+        {
+            try
+            {
+                disconnect( names[ i ] );
+            }
+            catch( final Exception e )
+            {
+                final String message = "Error disconnecting " + names[ i ];
+                getLogger().warn( message, e );
+            }
+        }
+    }
+
+    /**
+     * Start managing a connection.
+     * Management involves accepting connections and farming them out to threads
+     * from pool to be handled.
+     *
+     * @param name the name of connection
+     * @param socket the ServerSocket from which to
+     * @param handlerFactory the factory from which to aquire handlers
+     * @param threadPool the thread pool to use
+     * @exception Exception if an error occurs
+     */
+    public synchronized void connect( String name,
+                                      ServerSocket socket,
+                                      ConnectionHandlerFactory handlerFactory,
+                                      ThreadPool threadPool )
+        throws Exception
+    {
+        if( null != m_connections.get( name ) )
+        {
+            final String message = "Connection already exists with name " + name;
+            throw new IllegalArgumentException( message );
+        }
+
+        //Make sure timeout is specified for socket.
+        if( 0 == socket.getSoTimeout() )
+        {
+            socket.setSoTimeout( 500 );
+        }
+
+        final Connection runner =
+            new Connection( socket, handlerFactory, threadPool );
+        setupLogger( runner );
+        m_connections.put( name, runner );
+        threadPool.execute( runner );
+    }
+
+    /**
+     * Start managing a connection.
+     * This is similar to other connect method except that it uses default thread pool.
+     *
+     * @param name the name of connection
+     * @param socket the ServerSocket from which to
+     * @param handlerFactory the factory from which to aquire handlers
+     * @exception Exception if an error occurs
+     */
+    public void connect( String name,
+                         ServerSocket socket,
+                         ConnectionHandlerFactory handlerFactory )
+        throws Exception
+    {
+        connect( name, socket, handlerFactory, m_threadManager.getDefaultThreadPool() );
+    }
+
+    /**
+     * This shuts down all handlers and socket, waiting for each to gracefully shutdown.
+     *
+     * @param name the name of connection
+     * @exception Exception if an error occurs
+     */
+    public void disconnect( final String name )
+        throws Exception
+    {
+        disconnect( name, false );
+    }
+
+    /**
+     * This shuts down all handlers and socket.
+     * If tearDown is true then it will forcefully shutdown all connections and try
+     * to return as soon as possible. Otherwise it will behave the same as
+     * void disconnect( String name );
+     *
+     * @param name the name of connection
+     * @param tearDown if true will forcefully tear down all handlers
+     * @exception Exception if an error occurs
+     */
+    public synchronized void disconnect( final String name, final boolean tearDown )
+        throws Exception
+    {
+        final Connection connection = (Connection)m_connections.remove( name );
+
+        if( connection != null )
+        {
+            //TODO: Stop ignoring tearDown
+            connection.dispose();
+        }
+        else
+        {
+            final String error =
+                "Invalid request for the disconnection of an unrecognized connection name: "
+                + name;
+            throw new IllegalArgumentException( error );
+        }
+    }
+}
---
+++ avalon-components/sockets-impl/src/java/org/apache/avalon/cornerstone/blocks/sockets/DefaultSocketManager.java
@@ -0,0 +1,238 @@
+/*
+
+ ============================================================================
+                   The Apache Software License, Version 1.1
+ ============================================================================
+
+ Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modifica-
+ tion, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of  source code must  retain the above copyright  notice,
+    this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ 3. The end-user documentation included with the redistribution, if any, must
+    include  the following  acknowledgment:  "This product includes  software
+    developed  by the  Apache Software Foundation  (http://www.apache.org/)."
+    Alternately, this  acknowledgment may  appear in the software itself,  if
+    and wherever such third-party acknowledgments normally appear.
+
+ 4. The names "Jakarta", "Apache Avalon", "Avalon Cornerstone", "Avalon
+    Framework" and "Apache Software Foundation"  must not be used to endorse
+    or promote products derived  from this  software without  prior written
+    permission. For written permission, please contact apache@apache.org.
+
+ 5. Products  derived from this software may not  be called "Apache", nor may
+    "Apache" appear  in their name,  without prior written permission  of the
+    Apache Software Foundation.
+
+ THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
+ APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
+ DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
+ ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
+ (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ This software  consists of voluntary contributions made  by many individuals
+ on  behalf of the Apache Software  Foundation. For more  information on the
+ Apache Software Foundation, please see <http://www.apache.org/>.
+
+*/
+
+package org.apache.avalon.cornerstone.blocks.sockets;
+
+import java.util.HashMap;
+import org.apache.avalon.cornerstone.services.sockets.ServerSocketFactory;
+import org.apache.avalon.cornerstone.services.sockets.SocketFactory;
+import org.apache.avalon.cornerstone.services.sockets.SocketManager;
+import org.apache.avalon.framework.CascadingException;
+import org.apache.avalon.framework.activity.Initializable;
+import org.apache.avalon.framework.configuration.Configurable;
+import org.apache.avalon.framework.configuration.Configuration;
+import org.apache.avalon.framework.configuration.ConfigurationException;
+import org.apache.avalon.framework.container.ContainerUtil;
+import org.apache.avalon.framework.context.Context;
+import org.apache.avalon.framework.context.Contextualizable;
+import org.apache.avalon.framework.logger.AbstractLogEnabled;
+
+/**
+ * Implementation of SocketManager.
+ *
+ * @author <a href="mailto:fede@apache.org">Federico Barbieri</a>
+ * @author <a href="mailto:peter at apache.org">Peter Donald</a>
+ */
+public class DefaultSocketManager
+    extends AbstractLogEnabled
+    implements SocketManager, Contextualizable, Configurable, Initializable
+{
+    protected final HashMap m_serverSockets = new HashMap();
+    protected final HashMap m_sockets = new HashMap();
+
+    protected Context m_context;
+    protected Configuration m_configuration;
+
+    public void contextualize( final Context context )
+    {
+        m_context = context;
+    }
+
+    /**
+     * Configure the SocketManager.
+     *
+     * @param configuration the Configuration
+     * @exception ConfigurationException if an error occurs
+     */
+    public void configure( final Configuration configuration )
+        throws ConfigurationException
+    {
+        m_configuration = configuration;
+    }
+
+    public void initialize()
+        throws Exception
+    {
+        final Configuration[] serverSockets =
+            m_configuration.getChild( "server-sockets" ).getChildren( "factory" );
+
+        for( int i = 0; i < serverSockets.length; i++ )
+        {
+            final Configuration element = serverSockets[ i ];
+            final String name = element.getAttribute( "name" );
+            final String className = element.getAttribute( "class" );
+
+            setupServerSocketFactory( name, className, element );
+        }
+
+        final Configuration[] clientSockets =
+            m_configuration.getChild( "client-sockets" ).getChildren( "factory" );
+
+        for( int i = 0; i < clientSockets.length; i++ )
+        {
+            final Configuration element = clientSockets[ i ];
+            final String name = element.getAttribute( "name" );
+            final String className = element.getAttribute( "class" );
+
+            setupClientSocketFactory( name, className, element );
+        }
+    }
+
+    protected void setupServerSocketFactory( final String name,
+                                             final String className,
+                                             final Configuration configuration )
+        throws Exception
+    {
+        final Object object = createFactory( name, className, configuration );
+
+        if( !( object instanceof ServerSocketFactory ) )
+        {
+            throw new Exception( "Error creating factory " + name +
+                                 " with class " + className + " as " +
+                                 "it does not implement the correct " +
+                                 "interface (ServerSocketFactory)" );
+        }
+
+        m_serverSockets.put( name, object );
+    }
+
+    protected void setupClientSocketFactory( final String name,
+                                             final String className,
+                                             final Configuration configuration )
+        throws Exception
+    {
+        final Object object = createFactory( name, className, configuration );
+
+        if( !( object instanceof SocketFactory ) )
+        {
+            throw new Exception( "Error creating factory " + name +
+                                 " with class " + className + " as " +
+                                 "it does not implement the correct " +
+                                 "interface (SocketFactory)" );
+        }
+
+        m_sockets.put( name, object );
+    }
+
+    protected Object createFactory( final String name,
+                                    final String className,
+                                    final Configuration configuration )
+        throws Exception
+    {
+        Object factory = null;
+
+        try
+        {
+            final ClassLoader classLoader =
+                Thread.currentThread().getContextClassLoader();
+            factory = classLoader.loadClass( className ).newInstance();
+        }
+        catch( final Throwable e )
+        {
+            final String error = 
+              "Error creating factory with class " + className;
+            getLogger().error( "## CLASSLOADER: " + Thread.currentThread().getContextClassLoader() );
+            throw new CascadingException( error, e );
+        }
+
+        ContainerUtil.enableLogging( factory, getLogger() );
+        ContainerUtil.contextualize( factory, m_context );
+        ContainerUtil.configure( factory, configuration );
+        ContainerUtil.initialize( factory );
+
+        return factory;
+    }
+
+    /**
+     * Retrieve a server socket factory by name.
+     *
+     * @param name the name of server socket factory
+     * @return the ServerSocketFactory
+     * @exception Exception if server socket factory is not available
+     */
+    public ServerSocketFactory getServerSocketFactory( String name )
+        throws Exception
+    {
+        final ServerSocketFactory factory = (ServerSocketFactory)m_serverSockets.get( name );
+
+        if( null != factory )
+        {
+            return factory;
+        }
+        else
+        {
+            throw new Exception( "Unable to locate server socket factory " +
+                                 "named " + name );
+        }
+    }
+
+    /**
+     * Retrieve a client socket factory by name.
+     *
+     * @param name the name of client socket factory
+     * @return the SocketFactory
+     * @exception Exception if socket factory is not available
+     */
+    public SocketFactory getSocketFactory( final String name )
+        throws Exception
+    {
+        final SocketFactory factory = (SocketFactory)m_sockets.get( name );
+
+        if( null != factory )
+        {
+            return factory;
+        }
+        else
+        {
+            throw new Exception( "Unable to locate client socket factory " +
+                                 "named " + name );
+        }
+    }
+}
