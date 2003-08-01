02cda2b0a090469deedde7aaf9b069078b010a44
vgritsenko <vgritsenko@13f79535-47bb-0310-9956-ffa450edef68>
2003-08-01 01:04:50 +0000

    * Add more xpath tests * Package access to test documents * zap tabs
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@17591 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-sandbox/excalibur-old-materials/xmlutil/src/test/org/apache/excalibur/xml/xpath/test/XPathTestCase.java
+++ avalon-sandbox/excalibur-old-materials/xmlutil/src/test/org/apache/excalibur/xml/xpath/test/XPathTestCase.java
@@ -61,21 +61,23 @@ import org.apache.excalibur.xml.xpath.XPathProcessor;
 import org.apache.excalibur.xml.xpath.PrefixResolver;
 import org.xml.sax.InputSource;
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import java.io.StringReader;
 
 public class XPathTestCase extends ExcaliburTestCase
 {
     /** A small test document. */
-    private static final String CONTENT1 =
+    static final String CONTENT1 =
         "<?xml version=\"1.0\"?>" +
         "<test:root xmlns:test=\"http://localhost/test\">" +
             "<test:element1/>" +
             "<test:element2/>" +
         "</test:root>";
 
-    /** Second testdocument, has a different namespace than {@link #CONTENT1}. */
-    private static final String CONTENT2 =
+    /** Second test document, has a different namespace than {@link #CONTENT1}. */
+    static final String CONTENT2 =
         "<?xml version=\"1.0\"?>" +
         "<test:root xmlns:test=\"http://localhost/test2\">" +
             "<test:element1/>" +
@@ -96,14 +98,34 @@ public class XPathTestCase extends ExcaliburTestCase
             Document document1 = parser.parseDocument(new InputSource(new StringReader(CONTENT1)));
             Document document2 = parser.parseDocument(new InputSource(new StringReader(CONTENT2)));
 
-            // test with a namespace prefix configured in the component configuration
-            String testExpr = "count(/test:root/*)";
-            Number number = processor.evaluateAsNumber(document1, testExpr);
+            // 1. Test single node expression
+            String expr = "/test:root/test:element1";
+            Node node = processor.selectSingleNode(document1, expr);
+            assertNotNull("Must select <test:element1/> node, but got null", node);
+            assertEquals("Must select <test:element1/> node", Node.ELEMENT_NODE, node.getNodeType());
+            assertEquals("Must select <test:element1/> node", "element1", node.getLocalName());
+
+            // 2. Test single node expression with no expected result
+            expr = "/test:root/test:element3";
+            node = processor.selectSingleNode(document1, expr);
+            assertNull("Must be null", node);
+
+            // 3. Test multiple node expression
+            expr = "/test:root/test:*";
+            NodeList list = processor.selectNodeList(document1, expr);
+            assertNotNull("Must select two nodes, but got null", list);
+            assertEquals("Must select two nodes", 2, list.getLength());
+            assertEquals("Must select <test:element1/> node", "element1", list.item(0).getLocalName());
+            assertEquals("Must select <test:element2/> node", "element2", list.item(1).getLocalName());
+
+            // 4. Test with a namespace prefix configured in the component configuration
+            expr = "count(/test:root/*)";
+            Number number = processor.evaluateAsNumber(document1, expr);
             assertEquals(2, number.intValue());
 
-            // test with a custom prefix resolver using a different document in a different namespace,
+            // 5. Test with a custom prefix resolver using a different document in a different namespace,
             // to be sure the custom prefix resolver is used
-            number = processor.evaluateAsNumber(document2, testExpr, new PrefixResolver() {
+            number = processor.evaluateAsNumber(document2, expr, new PrefixResolver() {
                 public String prefixToNamespace(String prefix)
                 {
                     if (prefix.equals("test"))
@@ -113,11 +135,31 @@ public class XPathTestCase extends ExcaliburTestCase
             });
             assertEquals(2, number.intValue());
 
+            // 6. Test boolean
+            expr = "count(/test:root/*) = 2";
+            boolean bool = processor.evaluateAsBoolean(document1, expr);
+            assertEquals(true, bool);
+
+            // 7. Test expression in the root element context
+            expr = "/test:root/test:element1";
+            node = processor.selectSingleNode(document1.getDocumentElement(), expr);
+            assertNotNull("Must select <test:element1/> node, but got null", node);
+            assertEquals("Must select <test:element1/> node", Node.ELEMENT_NODE, node.getNodeType());
+            assertEquals("Must select <test:element1/> node", "element1", node.getLocalName());
+
+            // 8. Test expression in the child node context
+            node = processor.selectSingleNode(document1.getDocumentElement().getFirstChild(), expr);
+            assertNotNull("Must select <test:element1/> node, but got null", node);
+            assertEquals("Must select <test:element1/> node", Node.ELEMENT_NODE, node.getNodeType());
+            assertEquals("Must select <test:element1/> node", "element1", node.getLocalName());
+
         } finally {
-            if (parser != null)
+            if (parser != null) {
                 manager.release((Component)parser);
-            if (processor != null)
+            }
+            if (processor != null) {
                 manager.release((Component)processor);
+            }
         }
     }
 }
