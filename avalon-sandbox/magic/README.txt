29414a520570d2b29dc718d28c2c0b292fe3b6c3
leosimons <leosimons@13f79535-47bb-0310-9956-ffa450edef68>
2003-08-16 15:43:53 +0000

    Adding initial cut of a working Avalon2PicoAdapter.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@17653 13f79535-47bb-0310-9956-ffa450edef68

---
+++ avalon-sandbox/magic/README.txt
@@ -0,0 +1,30 @@
+           ============================================
+           Avalon-Magic: your ultimate interop solution
+           ============================================
+
+=======================================================================
+ Magic?
+
+Avalon-Magic allows you to exchange components between various
+container models and apis. The initial goal is to support interop
+between XWork/WebWork2, Avalon and PicoContainer/NanoContainer.
+
+=======================================================================
+ How does it work?
+
+By using reflection, JDK 1.3 dynamic proxies and other tricks.
+
+=======================================================================
+ How do I build it?
+
+Get maven, then type 'maven'.
+
+=======================================================================
+ Plans?
+
+No. This is a sandbox project.
+
+=======================================================================
+ Docs?
+
+No. Please add to the wiki for us.
---
+++ avalon-sandbox/magic/impl/src/test/org/apache/avalon/magic/test/impl/AssertionLogger.java
@@ -0,0 +1,152 @@
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 1997-2003 The Apache Software Foundation. All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *    "This product includes software developed by the
+ *    Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software
+ *    itself, if and wherever such third-party acknowledgments
+ *    normally appear.
+ *
+ * 4. The names "Avalon", and "Apache Software Foundation"
+ *    must not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation. For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+package org.apache.avalon.magic.test.impl;
+
+import org.apache.avalon.framework.logger.Logger;
+
+/**
+ *
+ *
+ * @author <a href="mail at leosimons dot com">Leo Simons</a>
+ * @version $Id: AssertionLogger.java,v 1.1 2003/08/16 15:43:53 leosimons Exp $
+ */
+public class AssertionLogger implements Logger
+{
+    boolean infoCalled = false;
+    private Avalon2PicoAdapterTestCase m_avalon2PicoAdapterTestCase;
+
+    public AssertionLogger( Avalon2PicoAdapterTestCase avalon2PicoAdapterTestCase )
+    {
+        m_avalon2PicoAdapterTestCase = avalon2PicoAdapterTestCase;
+    }
+
+    public boolean isInfoCalled()
+    {
+        return infoCalled;
+    }
+
+    public void debug( String message )
+    {
+    }
+
+    public void debug( String message, Throwable throwable )
+    {
+    }
+
+    public boolean isDebugEnabled()
+    {
+        return true;
+    }
+
+    public void info( String message )
+    {
+        infoCalled = true;
+    }
+
+    public void info( String message, Throwable throwable )
+    {
+        infoCalled = true;
+    }
+
+    public boolean isInfoEnabled()
+    {
+        return true;
+    }
+
+    public void warn( String message )
+    {
+    }
+
+    public void warn( String message, Throwable throwable )
+    {
+    }
+
+    public boolean isWarnEnabled()
+    {
+        return true;
+    }
+
+    public void error( String message )
+    {
+    }
+
+    public void error( String message, Throwable throwable )
+    {
+    }
+
+    public boolean isErrorEnabled()
+    {
+        return true;
+    }
+
+    public void fatalError( String message )
+    {
+    }
+
+    public void fatalError( String message, Throwable throwable )
+    {
+    }
+
+    public boolean isFatalErrorEnabled()
+    {
+        return true;
+    }
+
+    public Logger getChildLogger( String name )
+    {
+        return this;
+    }
+
+}
