e7d4f39c2c2ed792ec309829b1d2e8f9d35ccbe5
leosutic <leosutic@13f79535-47bb-0310-9956-ffa450edef68>
2003-08-17 15:48:27 +0000

    Not needed anymore
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@17655 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-sandbox/attributes/api/src/java/org/apache/avalon/attributes/ClassLoaderScanner.java
+++
@@ -1,8 +0,0 @@
-package org.apache.avalon.attributes;
-
-import java.util.Collection;
-
-public interface ClassLoaderScanner {
-    public Class getClassLoaderClass ();
-    public Collection getClasses (ClassLoader cl);
-}
