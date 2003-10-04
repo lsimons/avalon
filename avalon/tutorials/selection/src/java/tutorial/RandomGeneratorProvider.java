0d41e979276601bce8d29cc3100210c64caebf44
mcconnell <mcconnell@13f79535-47bb-0310-9956-ffa450edef68>
2003-10-04 09:52:24 +0000

    Addition of profile selection tutorial content.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@17897 13f79535-47bb-0310-9956-ffa450edef68

---
+++ avalon/tutorials/selection/src/java/tutorial/RandomGeneratorProvider.java
@@ -0,0 +1,46 @@
+
+package tutorial;
+
+import java.util.Random;
+
+import org.apache.avalon.framework.configuration.Configuration;
+import org.apache.avalon.framework.configuration.ConfigurationException;
+import org.apache.avalon.framework.configuration.Configurable;
+import org.apache.avalon.framework.logger.AbstractLogEnabled;
+
+/**
+ * An implementation of a random number generator.
+ * @avalon.component name="random-provider" lifestyle="singleton"
+ * @avalon.service type="tutorial.RandomGenerator"
+ */
+public class RandomGeneratorProvider extends AbstractLogEnabled 
+  implements Configurable, RandomGenerator
+{
+
+    private Random m_random = null;
+
+   /**
+    * Configuration of the component by the container.  The 
+    * implementation get a child element named 'source' and 
+    * assigns the value of the element to a local variable.
+    *
+    * @param config the component configuration
+    * @exception ConfigurationException if a configuration error occurs
+    */
+    public void configure( Configuration config ) throws ConfigurationException
+    {
+        getLogger().info( "configuration stage" );
+        long seed = config.getChild( "seed" ).getValueAsLong( 0 );
+        getLogger().info( "seed: " + seed );
+        m_random = new Random( System.currentTimeMillis() * seed );
+    }
+
+   /**
+    * Return a random integer
+    * @return the random number
+    */
+    public int getRandom()
+    {
+        return m_random.nextInt();
+    }
+}
