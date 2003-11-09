493ac36d2366e2696dbf3efe93776f292928a247
unknown <unknown@13f79535-47bb-0310-9956-ffa450edef68>
2003-11-09 14:48:32 +0000

    This commit was manufactured by cvs2svn to create branch 'LSD'.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/branches/LSD@18144 13f79535-47bb-0310-9956-ffa450edef68

---
+++ avalon-excalibur/thread/src/java/org/apache/avalon/excalibur/thread/impl/DefaultThreadPool.java
@@ -0,0 +1,168 @@
+/*
+
+ ============================================================================
+                   The Apache Software License, Version 1.1
+ ============================================================================
+
+ Copyright (C) 1999-2002 The Apache Software Foundation. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modifica-
+ tion, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of  source code must  retain the above copyright  notice,
+    this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ 3. The end-user documentation included with the redistribution, if any, must
+    include  the following  acknowledgment:  "This product includes  software
+    developed  by the  Apache Software Foundation  (http://www.apache.org/)."
+    Alternately, this  acknowledgment may  appear in the software itself,  if
+    and wherever such third-party acknowledgments normally appear.
+
+ 4. The names "Jakarta", "Apache Avalon", "Avalon Framework" and
+    "Apache Software Foundation"  must not be used to endorse or promote
+    products derived  from this  software without  prior written
+    permission. For written permission, please contact apache@apache.org.
+
+ 5. Products  derived from this software may not  be called "Apache", nor may
+    "Apache" appear  in their name,  without prior written permission  of the
+    Apache Software Foundation.
+
+ THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
+ APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
+ DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
+ ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
+ (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ This software  consists of voluntary contributions made  by many individuals
+ on  behalf of the Apache Software  Foundation. For more  information on the
+ Apache Software Foundation, please see <http://www.apache.org/>.
+
+*/
+
+package org.apache.avalon.excalibur.thread.impl;
+
+import org.apache.avalon.excalibur.pool.ObjectFactory;
+import org.apache.avalon.excalibur.pool.SoftResourceLimitingPool;
+import org.apache.avalon.excalibur.thread.ThreadPool;
+import org.apache.excalibur.thread.ThreadControl;
+import org.apache.avalon.framework.activity.Disposable;
+import org.apache.avalon.framework.activity.Executable;
+import org.apache.avalon.framework.container.ContainerUtil;
+import org.apache.avalon.framework.logger.LogEnabled;
+import org.apache.avalon.framework.logger.LogKitLogger;
+import org.apache.avalon.framework.logger.Loggable;
+import org.apache.avalon.framework.logger.Logger;
+
+/**
+ * This class is the public frontend for the thread pool code.
+ *
+ * @author <a href="mailto:dev@avalon.apache.org">Avalon Development Team</a>
+ * @author <a href="mailto:peter at apache.org">Peter Donald</a>
+ */
+public class DefaultThreadPool
+    extends ThreadGroup
+    implements ObjectFactory, Loggable, LogEnabled, Disposable, ThreadPool
+{
+    private final BasicThreadPool m_pool;
+    private SoftResourceLimitingPool m_underlyingPool;
+
+    public DefaultThreadPool( final int capacity )
+        throws Exception
+    {
+        this( "Worker Pool", capacity );
+    }
+
+    public DefaultThreadPool( final String name,
+                              final int capacity )
+        throws Exception
+    {
+        super( name );
+        m_underlyingPool = new SoftResourceLimitingPool( this, capacity );
+        m_pool = new BasicThreadPool( this, name, m_underlyingPool );
+    }
+
+    public DefaultThreadPool( final String name,
+                              final int min,
+                              final int max )
+        throws Exception
+    {
+        super( name );
+        m_underlyingPool = new SoftResourceLimitingPool( this, min, max );
+        m_pool = new BasicThreadPool( this, name, m_underlyingPool );
+    }
+
+    public void setLogger( final org.apache.log.Logger logger )
+    {
+        enableLogging( new LogKitLogger( logger ) );
+    }
+
+    public void enableLogging( final Logger logger )
+    {
+        ContainerUtil.enableLogging( m_pool, logger );
+    }
+
+    public void dispose()
+    {
+        m_pool.dispose();
+    }
+
+    public Object newInstance()
+    {
+        return m_pool.newInstance();
+    }
+
+    public void decommission( final Object object )
+    {
+        m_pool.decommission( object );
+    }
+
+    public Class getCreatedClass()
+    {
+        return m_pool.getCreatedClass();
+    }
+
+    /**
+     * Run work in separate thread.
+     * Return a valid ThreadControl to control work thread.
+     *
+     * @param work the work to be executed.
+     * @return the ThreadControl
+     */
+    public ThreadControl execute( final Executable work )
+    {
+        return m_pool.execute( work );
+    }
+
+    /**
+     * Run work in separate thread.
+     * Return a valid ThreadControl to control work thread.
+     *
+     * @param work the work to be executed.
+     * @return the ThreadControl
+     */
+    public ThreadControl execute( final Runnable work )
+    {
+        return m_pool.execute( work );
+    }
+
+    /**
+     * Run work in separate thread.
+     * Return a valid ThreadControl to control work thread.
+     *
+     * @param work the work to be executed.
+     * @return the ThreadControl
+     */
+    public ThreadControl execute( final org.apache.excalibur.thread.Executable work )
+    {
+        return m_pool.execute( work );
+    }
+}
