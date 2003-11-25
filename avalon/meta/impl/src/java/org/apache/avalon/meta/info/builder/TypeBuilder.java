436efc14bd1dc08e5ffc5486d8393fe5790468b9
mcconnell <mcconnell@13f79535-47bb-0310-9956-ffa450edef68>
2003-11-25 23:58:55 +0000

    Fix missing resource argument when xinfo not found.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@18200 13f79535-47bb-0310-9956-ffa450edef68

--- avalon/meta/impl/src/java/org/apache/avalon/meta/info/builder/TypeBuilder.java
+++ avalon/meta/impl/src/java/org/apache/avalon/meta/info/builder/TypeBuilder.java
@@ -69,7 +69,7 @@ import org.xml.sax.InputSource;
  * <p><image src="doc-files/TypeBuilder.gif" border="0"/></p>
  *
  * @author <a href="mailto:dev@avalon.apache.org">Avalon Development Team</a>
- * @version $Revision: 1.1 $ $Date: 2003/09/24 08:15:25 $
+ * @version $Revision: 1.2 $ $Date: 2003/11/25 23:58:55 $
  */
 public final class TypeBuilder
 {
@@ -182,7 +182,7 @@ public final class TypeBuilder
         if( null == inputStream )
         {
             final String message =
-                REZ.getString( "builder.missing-info.error" );
+                REZ.getString( "builder.missing-info.error", path );
             throw new Exception( message );
         }
 
