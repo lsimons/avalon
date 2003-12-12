99b809bb07d07089c11239f048f012e0880fa838
mcconnell <mcconnell@13f79535-47bb-0310-9956-ffa450edef68>
2003-12-12 13:38:50 +0000

    Correct bug in bat file that was overriding the merlin.dir value.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@18374 13f79535-47bb-0310-9956-ffa450edef68

--- avalon/merlin/site/src/bin/merlin.bat
+++ avalon/merlin/site/src/bin/merlin.bat
@@ -5,7 +5,7 @@ if "%MERLIN_HOME%" == "" set MERLIN_HOME=%USERPROFILE%\.merlin
 set MERLIN_CMD_LINE_ARGS=%*
 set MERLIN_BOOTSTRAP_JAR=%MERLIN_HOME%\bin\lib\merlin-cli-3.2-dev.jar
 set MERLIN_EXT_DIR=%MERLIN_HOME%\ext
-java -Djava.security.policy=%MERLIN_HOME%\bin\security.policy -Dmerlin.dir=%MERLIN_HOME% -Djava.ext.dirs=%MERLIN_EXT_DIR% -jar %MERLIN_BOOTSTRAP_JAR% %MERLIN_CMD_LINE_ARGS%
+java -Djava.security.policy=%MERLIN_HOME%\bin\security.policy -Djava.ext.dirs=%MERLIN_EXT_DIR% -jar %MERLIN_BOOTSTRAP_JAR% %MERLIN_CMD_LINE_ARGS%
 goto EndOfScript
 
 :EndOfScript
