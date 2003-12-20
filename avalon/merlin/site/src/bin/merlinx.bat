f5751afb9c106224ef781286e34ec227ffc6ca32
mcconnell <mcconnell@13f79535-47bb-0310-9956-ffa450edef68>
2003-12-20 00:16:00 +0000

    Properly correct merlinx command line parmeters.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@18437 13f79535-47bb-0310-9956-ffa450edef68

--- avalon/merlin/site/src/bin/merlinx.bat
+++ avalon/merlin/site/src/bin/merlinx.bat
@@ -2,15 +2,15 @@
 
 if "%MERLIN_HOME%" == "" set MERLIN_HOME=%USERPROFILE%\.merlin
 
-set MERLIN_SYSTEM_REPOSITORY=%USERPROFILE%\.maven\repository
+set WORKING_REPOSITORY=%USERPROFILE%\.maven\repository
 if "%MAVEN_HOME_LOCAL%" == "" goto DoneMerlinSystem
-set MERLIN_SYSTEM_REPOSITORY=%MAVEN_HOME_LOCAL%\repository
+set WORKING_REPOSITORY=%MAVEN_HOME_LOCAL%\repository
 :DoneMerlinSystem
 
 set MERLIN_CMD_LINE_ARGS=%*
 set MERLIN_BOOTSTRAP_JAR=%MERLIN_HOME%\bin\lib\merlin-cli-3.2.jar
 set MERLIN_EXT_DIR=%MERLIN_HOME%\ext
-java -Djava.security.policy=%MERLIN_HOME%\bin\security.policy -Djava.ext.dirs=%MERLIN_EXT_DIR% -jar %MERLIN_BOOTSTRAP_JAR% %MERLIN_CMD_LINE_ARGS% -system %MERLIN_SYSTEM_REPOSITORY% 
+java -Djava.security.policy=%MERLIN_HOME%\bin\security.policy -Djava.ext.dirs=%MERLIN_EXT_DIR% -jar %MERLIN_BOOTSTRAP_JAR% %MERLIN_CMD_LINE_ARGS% -system %WORKING_REPOSITORY% -repository %WORKING_REPOSITORY% 
 goto :EndOfScript
 
 :EndOfScript
