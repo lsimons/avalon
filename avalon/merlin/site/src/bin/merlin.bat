58439f2d955ceef371dd4830bf3982fa18d21dc5
mcconnell <mcconnell@13f79535-47bb-0310-9956-ffa450edef68>
2003-12-21 23:36:55 +0000

    Update the windows bat files to remove the java extensions directory override and add a generic MERLIN_JVMARGS value.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@18459 13f79535-47bb-0310-9956-ffa450edef68

--- avalon/merlin/site/src/bin/merlin.bat
+++ avalon/merlin/site/src/bin/merlin.bat
@@ -1,11 +1,8 @@
 @echo off
 if "%MERLIN_HOME%" == "" set MERLIN_HOME=%USERPROFILE%\.merlin
-
-:MerlinRuntime
 set MERLIN_CMD_LINE_ARGS=%*
 set MERLIN_BOOTSTRAP_JAR=%MERLIN_HOME%\bin\lib\merlin-cli-3.2.jar
-set MERLIN_EXT_DIR=%MERLIN_HOME%\ext
-java -Djava.security.policy=%MERLIN_HOME%\bin\security.policy -Djava.ext.dirs=%MERLIN_EXT_DIR% -jar %MERLIN_BOOTSTRAP_JAR% %MERLIN_CMD_LINE_ARGS%
+set MERLIN_SECURITY_POLICY=-Djava.security.policy=%MERLIN_HOME%\bin\security.policy
+java %MERLIN_SECURITY_POLICY% %MERLIN_JVMARGS% -jar %MERLIN_BOOTSTRAP_JAR% %MERLIN_CMD_LINE_ARGS%
 goto EndOfScript
-
 :EndOfScript
--- avalon/merlin/site/src/bin/merlinx.bat
+++ avalon/merlin/site/src/bin/merlinx.bat
@@ -1,16 +1,12 @@
 @echo off
-
 if "%MERLIN_HOME%" == "" set MERLIN_HOME=%USERPROFILE%\.merlin
-
 set WORKING_REPOSITORY=%USERPROFILE%\.maven\repository
 if "%MAVEN_HOME_LOCAL%" == "" goto DoneMerlinSystem
 set WORKING_REPOSITORY=%MAVEN_HOME_LOCAL%\repository
 :DoneMerlinSystem
-
 set MERLIN_CMD_LINE_ARGS=%*
 set MERLIN_BOOTSTRAP_JAR=%MERLIN_HOME%\bin\lib\merlin-cli-3.2.jar
-set MERLIN_EXT_DIR=%MERLIN_HOME%\ext
-java -Djava.security.policy=%MERLIN_HOME%\bin\security.policy -Djava.ext.dirs=%MERLIN_EXT_DIR% -jar %MERLIN_BOOTSTRAP_JAR% %MERLIN_CMD_LINE_ARGS% -system %WORKING_REPOSITORY% -repository %WORKING_REPOSITORY% 
+set MERLIN_SECURITY_POLICY=-Djava.security.policy=%MERLIN_HOME%\bin\security.policy
+java %MERLIN_SECURITY_POLICY% %MERLIN_JVMARGS% -jar %MERLIN_BOOTSTRAP_JAR% -system %WORKING_REPOSITORY% -repository %WORKING_REPOSITORY% %MERLIN_CMD_LINE_ARGS% 
 goto :EndOfScript
-
 :EndOfScript
