70771b315c9937c49a404c01b58d21a23d974fd9
mcconnell <mcconnell@13f79535-47bb-0310-9956-ffa450edef68>
2003-12-29 22:56:19 +0000

    Bump minor versions to reflect block add/remove resolution.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@18527 13f79535-47bb-0310-9956-ffa450edef68

--- avalon/merlin/site/src/bin/merlinx.bat
+++ avalon/merlin/site/src/bin/merlinx.bat
@@ -5,7 +5,7 @@ if "%MAVEN_HOME_LOCAL%" == "" goto DoneMerlinSystem
 set WORKING_REPOSITORY=%MAVEN_HOME_LOCAL%\repository
 :DoneMerlinSystem
 set MERLIN_CMD_LINE_ARGS=%*
-set MERLIN_BOOTSTRAP_JAR=%MERLIN_HOME%\bin\lib\merlin-cli-3.2.jar
+set MERLIN_BOOTSTRAP_JAR=%MERLIN_HOME%\bin\lib\merlin-cli-3.2.1.jar
 set MERLIN_SECURITY_POLICY=-Djava.security.policy=%MERLIN_HOME%\bin\security.policy
 java %MERLIN_SECURITY_POLICY% %MERLIN_JVM_OPTS% -jar %MERLIN_BOOTSTRAP_JAR% -system %WORKING_REPOSITORY% -repository %WORKING_REPOSITORY% %MERLIN_CMD_LINE_ARGS% 
 goto :EndOfScript
