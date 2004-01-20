ab3170f9c7e8c8f2c49d5d7ce7e9d1e68365197a
niclas <niclas@13f79535-47bb-0310-9956-ffa450edef68>
2004-01-20 03:57:10 +0000

    SYncing with Steve to find problem.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@18778 13f79535-47bb-0310-9956-ffa450edef68

---
+++ avalon/activation/impl/src/test/org/apache/avalon/activation/appliance/grant/components/AnotherTestComponent.java
@@ -0,0 +1,94 @@
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2002 The Apache Software Foundation. All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *    "This product includes software developed by the
+ *    Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software
+ *    itself, if and wherever such third-party acknowledgments
+ *    normally appear.
+ *
+ * 4. The names "Jakarta", "Avalon", and "Apache Software Foundation"
+ *    must not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation. For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+package org.apache.avalon.activation.appliance.grant.components;
+
+import org.apache.avalon.framework.logger.AbstractLogEnabled;
+import org.apache.avalon.framework.service.Serviceable;
+import org.apache.avalon.framework.service.ServiceException;
+import org.apache.avalon.framework.service.ServiceManager;
+
+/**
+ * This is a component that can be tested relative a set of 
+ * assigned permissions.
+ *
+ * @author <a href="mailto:dev@avalon.apache.org">Avalon Development Team</a>
+ * @avalon.component name="anothertest" lifestyle="singleton"
+ * @avalon.service type="org.apache.avalon.activation.appliance.grant.components.AnotherTestService"
+ */
+public class AnotherTestComponent extends AbstractLogEnabled 
+  implements AnotherTestService, Serviceable
+{
+    private TestService m_TestService;
+    
+    /**
+     * @avalon.dependency type="org.apache.avalon.activation.appliance.grant.components.TestService" key="TestService"
+     **/
+    public void service( ServiceManager man )
+        throws ServiceException
+    {
+        m_TestService = (TestService) man.lookup( "org.apache.avalon.activation.appliance.grant.components.TestService" ); 
+    }
+    
+    public String getJavaVersion()
+    {
+        return m_TestService.getJavaVersion();
+    }
+
+    public void setJavaVersion( String newVersion )
+    {
+        m_TestService.setJavaVersion( newVersion );
+    }
+}
