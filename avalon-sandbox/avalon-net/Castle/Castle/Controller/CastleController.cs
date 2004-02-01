766782668843ae3eaf3d2819e191accbc97915a9
hammett <hammett@13f79535-47bb-0310-9956-ffa450edef68>
2004-02-01 00:35:55 +0000

    Licence updated to 2.0
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@18851 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-sandbox/avalon-net/Castle/Castle/Controller/CastleController.cs
+++ avalon-sandbox/avalon-net/Castle/Castle/Controller/CastleController.cs
@@ -1,53 +1,21 @@
-// ============================================================================
-//                   The Apache Software License, Version 1.1
-// ============================================================================
+// Copyright 2004 Apache Software Foundation
 // 
-// Copyright (C) 2002-2003 The Apache Software Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 // 
-// Redistribution and use in source and binary forms, with or without modifica-
-// tion, are permitted provided that the following conditions are met:
+//     http://www.apache.org/licenses/LICENSE-2.0
 // 
-// 1. Redistributions of  source code must  retain the above copyright  notice,
-//    this list of conditions and the following disclaimer.
-// 
-// 2. Redistributions in binary form must reproduce the above copyright notice,
-//    this list of conditions and the following disclaimer in the documentation
-//    and/or other materials provided with the distribution.
-// 
-// 3. The end-user documentation included with the redistribution, if any, must
-//    include  the following  acknowledgment:  "This product includes  software
-//    developed  by the  Apache Software Foundation  (http://www.apache.org/)."
-//    Alternately, this  acknowledgment may  appear in the software itself,  if
-//    and wherever such third-party acknowledgments normally appear.
-// 
-// 4. The names "Jakarta", "Avalon", "Excalibur" and "Apache Software Foundation"  
-//    must not be used to endorse or promote products derived from this  software 
-//    without  prior written permission. For written permission, please contact 
-//    apache@apache.org.
-// 
-// 5. Products  derived from this software may not  be called "Apache", nor may
-//    "Apache" appear  in their name,  without prior written permission  of the
-//    Apache Software Foundation.
-// 
-// THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
-// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-// FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
-// APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
-// INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
-// DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
-// OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
-// ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
-// (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
-// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// 
-// This software  consists of voluntary contributions made  by many individuals
-// on  behalf of the Apache Software  Foundation. For more  information on the 
-// Apache Software Foundation, please see <http://www.apache.org/>.
-// ============================================================================
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 namespace Apache.Avalon.Castle.Controller
 {
 	using System;
+	using System.Collections;
 	using System.Configuration;
 
 	using Apache.Avalon.Castle.Controller.Config;
@@ -71,6 +39,8 @@ namespace Apache.Avalon.Castle.Controller
 		/// </summary>
 		private CastleConfig config;
 
+		private ArrayList startupList = new ArrayList();
+
 		protected ILogger logger = Logger.LoggerFactory.GetLogger("CastleController");
 
 		public CastleController()
@@ -84,6 +54,8 @@ namespace Apache.Avalon.Castle.Controller
 			logger.Debug("Create");
 
 			InstantiateAndRegisterComponents();
+
+			StartQueueComponents();
 		}
 
 		[ManagedOperation]
@@ -97,7 +69,7 @@ namespace Apache.Avalon.Castle.Controller
 		{
 			logger.Debug("Stop");
 
-			// Uninstall();
+			Undeploy();
 		}
 
 		private void InstantiateAndRegisterComponents()
@@ -116,6 +88,12 @@ namespace Apache.Avalon.Castle.Controller
 			}
 
 			Instantiate(component);
+		}
+
+		private void Undeploy()
+		{
+			StopQueueComponents();
+			DestroyQueueComponents();
 		}
 
 		/// <summary>
@@ -154,12 +132,101 @@ namespace Apache.Avalon.Castle.Controller
 		{
 			SetupAttributes(name, component);
 			SetupDependencies(name, component);
+			CreateComponent(name);
+			QueueStart(name);
+		}
 
-			logger.Debug("Invoking Create on {0}", name);
-			server.Invoke(name, "Create", null, null);
+		private void StartQueueComponents()
+		{
+			for(int i=0; i < startupList.Count; i++)
+			{
+				StartComponent( startupList[i] as ManagedObjectName );
+			}
+		}
 
-			logger.Debug("Invoking Start on {0}", name);
-			server.Invoke(name, "Start", null, null);
+		private void StopQueueComponents()
+		{
+			object[] components = startupList.ToArray();
+			Array.Reverse( components );
+			for(int i=0; i < components.Length; i++)
+			{
+				StopComponent( components[i] as ManagedObjectName );
+			}
+		}
+
+		private void DestroyQueueComponents()
+		{
+			object[] components = startupList.ToArray();
+			Array.Reverse( components );
+			for(int i=0; i < components.Length; i++)
+			{
+				DestroyComponent( components[i] as ManagedObjectName );
+			}
+		}
+
+		private void QueueStart(ManagedObjectName name)
+		{
+			startupList.Add(name);
+		}
+
+		private void CreateComponent(ManagedObjectName name)
+		{
+			try
+			{
+				logger.Debug("Invoking Create on {0}", name);
+
+				server.Invoke(name, "Create", null, null);
+			}
+			catch(Exception e)
+			{
+				logger.Error("Exception '{0}' invoking 'Create' on '{1}'", e.Message, name);
+
+				throw e;
+			}
+		}
+
+		private void StartComponent(ManagedObjectName name)
+		{
+			try
+			{
+				logger.Debug("Invoking Start on {0}", name);
+
+				server.Invoke(name, "Start", null, null);
+			}
+			catch(Exception e)
+			{
+				logger.Error("Exception '{0}' invoking 'Start' on '{1}'", e.Message, name);
+
+				throw e;
+			}
+		}
+
+		private void StopComponent(ManagedObjectName name)
+		{
+			try
+			{
+				logger.Debug("Invoking Stop on {0}", name);
+
+				server.Invoke(name, "Stop", null, null);
+			}
+			catch(Exception e)
+			{
+				logger.Error("Ignoring Exception '{0}' invoking 'Stop' on '{1}'", e.Message, name);
+			}
+		}
+
+		private void DestroyComponent(ManagedObjectName name)
+		{
+			try
+			{
+				logger.Debug("Invoking Destroy on {0}", name);
+
+				server.Invoke(name, "Destroy", null, null);
+			}
+			catch(Exception e)
+			{
+				logger.Error("Ignoring Exception '{0}' invoking 'Destroy' on '{1}'", e.Message, name);
+			}
 		}
 
 		private void SetupAttributes(ManagedObjectName name, ComponentDescriptor component)
