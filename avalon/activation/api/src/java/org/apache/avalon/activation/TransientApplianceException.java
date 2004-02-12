dd50a3662e2d6f9d6080629f1657109e999f2e58
mcconnell <mcconnell@13f79535-47bb-0310-9956-ffa450edef68>
2004-02-12 00:23:21 +0000

    Add a transient appliance exception - thrown when a resolve request is received during the commissioning process.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@18929 13f79535-47bb-0310-9956-ffa450edef68

---
+++ avalon/activation/api/src/java/org/apache/avalon/activation/TransientApplianceException.java
@@ -0,0 +1,74 @@
+/* 
+ * Copyright 2004 Apache Software Foundation
+ * Licensed  under the  Apache License,  Version 2.0  (the "License");
+ * you may not use  this file  except in  compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed  under the  License is distributed on an "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
+ * implied.
+ * 
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.avalon.activation;
+
+/**
+ * Exception to indicate that there was a transient exception.
+ *
+ * @author <a href="mailto:dev@avalon.apache.org">Avalon Development Team</a>
+ * @version $Revision: 1.1 $ $Date: 2004/02/12 00:23:21 $
+ */
+public class TransientApplianceException extends ApplianceException
+{
+     private final long m_delay;
+
+    /**
+     * Construct a new <code>TransientApplianceException</code> instance.
+     *
+     * @param delay the expected dalay
+     */
+    public TransientApplianceException( final long delay )
+    {
+        this( null, delay );
+    }
+
+    /**
+     * Construct a new <code>TransientApplianceException</code> instance.
+     *
+     * @param delay the expected dalay
+     * @param cause the root cause of the exception
+     * @param message the exception message
+     */
+    public TransientApplianceException( String message, long delay )
+    {
+        this( message, null, delay );
+    }
+
+    /**
+     * Construct a new <code>TransientApplianceException</code> instance.
+     *
+     * @param message the exception message
+     * @param throwable the root cause of the exception
+     * @param delay the projected delay
+     */
+    public TransientApplianceException( String message, Throwable cause, long delay )
+    {
+        super( message, cause );
+        m_delay = delay;
+    }
+
+   /**
+    * Return the projected delay.
+    * @return the delay in milliseconds
+    */
+    public long getDelay()
+    {
+        return m_delay;
+    }
+}
+
