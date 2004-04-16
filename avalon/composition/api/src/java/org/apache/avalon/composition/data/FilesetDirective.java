ddb743fcfc07beee9a21c952a343c8e0e6a8a648
tbennett <tbennett@13f79535-47bb-0310-9956-ffa450edef68>
2004-04-16 19:55:11 +0000

    Adds an exclude directive to the classloader/classpath fileset directive and provides a getter method in the FilesetDirective to obtain the list of excluded file(s)
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@19710 13f79535-47bb-0310-9956-ffa450edef68

--- avalon/composition/api/src/java/org/apache/avalon/composition/data/FilesetDirective.java
+++ avalon/composition/api/src/java/org/apache/avalon/composition/data/FilesetDirective.java
@@ -22,19 +22,21 @@ import java.io.Serializable;
 /**
  * <p>A fileset directive is a scoped defintion of a set of files.  A fileset
  * a structurally defined as a base directory and a set of relative filenames
- * represented as include directives.</p>
+ * represented as include directives and/or exclude directives.</p>
  *
  * <p><b>XML</b></p>
  * <pre>
  *   &lt;fileset dir="<font color="darkred">lib</font>"&gt;
  *     &lt;include name="<font color="darkred">avalon-framework.jar</font>"/&gt;
  *     &lt;include name="<font color="darkred">logkit.jar</font>"/&gt;
+ *     &lt;exclude name="<font color="darkred">servlet.jar</font>"/&gt;
  *   &lt;/dirset&gt;
  * </pre>
  *
  * @see IncludeDirective
+ * @see ExcludeDirective
  * @author <a href="mailto:dev@avalon.apache.org">Avalon Development Team</a>
- * @version $Revision: 1.3 $ $Date: 2004/02/24 22:18:21 $
+ * @version $Revision: 1.4 $ $Date: 2004/04/16 19:55:11 $
  */
 public class FilesetDirective implements Serializable
 {
@@ -49,6 +51,11 @@ public class FilesetDirective implements Serializable
     private final IncludeDirective[] m_includes;
 
     /**
+     * The set of exclude directives.
+     */
+    private final ExcludeDirective[] m_excludes;
+
+    /**
      * Create a FilesetDirective instance.
      *
      * @param base the base directory path against which includes are evaluated
@@ -56,8 +63,21 @@ public class FilesetDirective implements Serializable
      */
     public FilesetDirective( final String base, final IncludeDirective[] includes )
     {
+        this(base, includes, null);
+    }
+
+    /**
+     * Create a FilesetDirective instance.
+     *
+     * @param base the base directory path against which includes are evaluated
+     * @param includes the set of includes to include in the fileset
+     */
+    public FilesetDirective( final String base, final IncludeDirective[] includes,
+            final ExcludeDirective[] excludes)
+    {
         m_base = base;
         m_includes = includes;
+        m_excludes = excludes;
     }
 
     /**
@@ -78,5 +98,15 @@ public class FilesetDirective implements Serializable
     public IncludeDirective[] getIncludes()
     {
         return m_includes;
+    }
+
+    /**
+     * Return the set of exclude directives.
+     *
+     * @return the exclude set
+     */
+    public ExcludeDirective[] getExcludes()
+    {
+        return m_excludes;
     }
 }
