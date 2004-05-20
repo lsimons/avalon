4a02a9cfbe31058b2d9d306fe0e31ab0a7872634
unknown <unknown@13f79535-47bb-0310-9956-ffa450edef68>
2004-05-20 02:46:52 +0000

    This commit was manufactured by cvs2svn to create tag 'META_1_4_0_FINAL'.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/tags/META_1_4_0_FINAL@20085 13f79535-47bb-0310-9956-ffa450edef68

--- avalon/repository/cli/bin/repository.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-if "%AVALON_HOME%" == "" set AVALON_HOME=%USERPROFILE%\.avalon
-set AVALON_CMD_LINE_ARGS=%*
-set AVALON_BOOTSTRAP_JAR=%AVALON_HOME%\repository\avalon-repository\jars\avalon-repository-cli-@AVALON_CLI_VERSION@.jar
-set AVALON_SECURITY_POLICY=-Djava.security.policy=%AVALON_HOME%\bin\security.policy
-java %AVALON_SECURITY_POLICY% %AVALON_JVM_OPTS% -jar %AVALON_BOOTSTRAP_JAR% %AVALON_CMD_LINE_ARGS%
-goto EndOfScript
-:EndOfScript
