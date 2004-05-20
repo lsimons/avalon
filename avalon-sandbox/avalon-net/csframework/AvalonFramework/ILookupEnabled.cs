28a6a6ab6bf262b3a1a5f4fd452107b795228f61
unknown <unknown@13f79535-47bb-0310-9956-ffa450edef68>
2004-05-20 02:30:11 +0000

    This commit was manufactured by cvs2svn to create tag 'SNAPSHOT_20031214'.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/tags/SNAPSHOT_20031214@18409 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-sandbox/avalon-net/csframework/AvalonFramework/ILookupEnabled.cs
+++
@@ -1,49 +0,0 @@
-// ============================================================================
-//                   The Apache Software License, Version 1.1
-// ============================================================================
-// 
-// Copyright (C) 2002-2003 The Apache Software Foundation. All rights reserved.
-// 
-// Redistribution and use in source and binary forms, with or without modifica-
-// tion, are permitted provided that the following conditions are met:
-// 
-// 1. Redistributions of  source code must  retain the above copyright  notice,
-//    this list of conditions and the following disclaimer.
-// 
-// 2. Redistributions in binary form must reproduce the above copyright notice,
-//    this list of conditions and the following disclaimer in the documentation
-//    and/or other materials provided with the distribution.
-// 
-// 3. The end-user documentation included with the redistribution, if any, must
-//    include  the following  acknowledgment:  "This product includes  software
-//    developed  by the  Apache Software Foundation  (http://www.apache.org/)."
-//    Alternately, this  acknowledgment may  appear in the software itself,  if
-//    and wherever such third-party acknowledgments normally appear.
-// 
-// 4. The names "Jakarta", "Avalon", "Excalibur" and "Apache Software Foundation"  
-//    must not be used to endorse or promote products derived from this  software 
-//    without  prior written permission. For written permission, please contact 
-//    apache@apache.org.
-// 
-// 5. Products  derived from this software may not  be called "Apache", nor may
-//    "Apache" appear  in their name,  without prior written permission  of the
-//    Apache Software Foundation.
-// 
-// THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
-// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-// FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
-// APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
-// INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
-// DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
-// OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
-// ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
-// (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
-// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// 
-// This software  consists of voluntary contributions made  by many individuals
-// on  behalf of the Apache Software  Foundation. For more  information on the 
-// Apache Software Foundation, please see <http://www.apache.org/>.
-// ============================================================================
-
-namespace Apache.Avalon.Framework{	using System;
-	/// <summary>	/// A Serviceable is a class that need to connect to software components	/// or obtain other context information using a name, thus not depending	/// on particular implementations but on behavioral interfaces. The	/// contract surrounding a ILookupEnabled is that it is a user. The	/// ILookupEnabled is able to use Objects managed by the ILookupManager	/// it was initialized with. As part of the contract with the system, the	/// instantiating entity (container) must call the enableLookups method	/// before the ILookupEnabled can be considered valid.	/// </summary>	public interface ILookupEnabled	{		/// <summary>		/// Pass the ILookupManager to the ILookupEnabled. The ILookupEnabled		/// implementation should use the specified ILookupManager to acquire		/// the components it needs for execution.		/// </summary>		/// <param name="manager">The lookup manager</param>		/// <exception cref="LookupException">If a required object could not be found.</exception>		void EnableLookups( ILookupManager manager );	}}
--- avalon-site/site/download.cgi
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /www/www.apache.org/dyn/mirrors
-/www/www.apache.org/dyn/mirrors/mirrors.cgi $*
