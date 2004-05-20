c56a00e4b225749809bd15387b7d1e563d8c84db
unknown <unknown@13f79535-47bb-0310-9956-ffa450edef68>
2004-05-20 02:24:03 +0000

    This commit was manufactured by cvs2svn to create tag 'STORE_0_6_RC1'.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/tags/STORE_0_6_RC1@17724 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-components/release.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/sh
-
-export RSYNC_RSH=ssh
-
-ssh leosimons@cvs.apache.org mkdir -p ~/public_html/dist-candidates/avalon
-cd target/www.apache.org/dist/avalon
-rsync -vRrlpt --ignore-existing * leosimons@cvs.apache.org:~/public_html/dist-candidates/avalon
--- avalon-phoenix/lib/container/README.txt
+++
@@ -1,1 +0,0 @@
-This where you place all the jars that should only be accessible to the container.
