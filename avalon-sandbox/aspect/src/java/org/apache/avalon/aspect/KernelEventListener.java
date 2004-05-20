ef0cfb0c9129e287c8a78bf47c74afee99a3445f
unknown <unknown@13f79535-47bb-0310-9956-ffa450edef68>
2004-05-20 02:46:59 +0000

    This commit was manufactured by cvs2svn to create tag 'COMPOSITION_2_0_0_FINAL'.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/tags/COMPOSITION_2_0_0_FINAL@20092 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-sandbox/aspect/src/java/org/apache/avalon/aspect/KernelEventListener.java
+++
@@ -1,6 +0,0 @@
-package org.apache.avalon.aspect;
-
-public interface KernelEventListener {
-    public void handlerAdded (String key, Handler handler);
-    public void aspectAdded (String key, Aspect aspect);
-}
