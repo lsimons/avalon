3e06597668cc83a061b5ade4f5a825e27e2e1de1
unknown <unknown@13f79535-47bb-0310-9956-ffa450edef68>
2004-05-20 02:46:33 +0000

    This commit was manufactured by cvs2svn to create tag 'FMW_4-2-0-RC2'.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/tags/FMW_4-2-0-RC2@20054 13f79535-47bb-0310-9956-ffa450edef68

--- avalon-sandbox/aspect/src/java/org/apache/avalon/aspect/LifecycleInterceptor.java
+++
@@ -1,6 +0,0 @@
-package org.apache.avalon.aspect;
-
-public interface LifecycleInterceptor {
-    public Object interceptCreation (Object instance);
-    public Object interceptAccess (String accessor, Object instance);
-}
