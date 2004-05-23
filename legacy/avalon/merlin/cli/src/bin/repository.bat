6076373e53dc739ea75515c606db9f248149d823
niclas <niclas@13f79535-47bb-0310-9956-ffa450edef68>
2004-05-23 08:13:59 +0000

    Legacy is consuming too much cycles for checkouts and other svn works. Moving it into a parallel of trunk, and then move across the pieces later.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@20239 13f79535-47bb-0310-9956-ffa450edef68

--- legacy/avalon/merlin/cli/src/bin/repository.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-if "%MERLIN_HOME%" == "" set MERLIN_HOME=%USERPROFILE%\.merlin
-if "%REPOSITORY_HOME%" == "" set REPOSITORY_HOME=%MERLIN_HOME%
-set CMD_LINE_ARGS=%*
-set BOOTSTRAP_JAR=%REPOSITORY_HOME%\system\avalon-repository\jars\avalon-repository-cli-@AVALON_CLI_VERSION@.jar
-set SECURITY_POLICY=-Djava.security.policy=%REPOSITORY_HOME%\bin\security.policy
-java %SECURITY_POLICY% %REPOSITORY_JVM_OPTS% -jar %BOOTSTRAP_JAR% %CMD_LINE_ARGS%
-goto EndOfScript
-:EndOfScript
