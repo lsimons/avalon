e54b59b61249f0ea2709e813771312b538140190
mcconnell <mcconnell@13f79535-47bb-0310-9956-ffa450edef68>
2004-06-09 13:50:34 +0000

    add teste
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@20947 13f79535-47bb-0310-9956-ffa450edef68

---
+++ runtime/meta/test/teste/src/main/org/apache/avalon/test/teste/TestE.java
@@ -0,0 +1,50 @@
+/* 
+ * Copyright 2004 Apache Software Foundation
+ * Licensed  under the  Apache License,  Version 2.0  (the "License");
+ * you may not use  this file  except in  compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed  under the  License is distributed on an "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
+ * implied.
+ * 
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.avalon.test.teste;
+
+import org.apache.avalon.framework.activity.Initializable;
+import org.apache.avalon.framework.logger.AbstractLogEnabled;
+import org.apache.avalon.framework.logger.Logger;
+import org.apache.avalon.framework.service.Serviceable;
+import org.apache.avalon.framework.service.ServiceManager;
+import org.apache.avalon.framework.service.ServiceException;
+
+/**
+ * @avalon.component name="component-e" lifestyle="singleton"
+ * @avalon.service type="org.apache.avalon.test.teste.E"
+ */
+public class TestE extends AbstractLogEnabled
+  implements Initializable, E
+{
+    public void initialize() throws Exception
+    {
+        getLogger().info( "hello from E" );
+    }
+
+   /**
+    * @avalon.dependency key= "a" type="org.apache.avalon.test.testa.A"
+    */
+    public void service( ServiceManager manager ) throws ServiceException
+    {
+        getLogger().info( "service stage" );
+        Logger logger = getLogger().getChildLogger( "service" );
+        logger.info( "lookup A" );
+        manager.lookup( "a" );
+        logger.info( "ok" );
+    }
+}
