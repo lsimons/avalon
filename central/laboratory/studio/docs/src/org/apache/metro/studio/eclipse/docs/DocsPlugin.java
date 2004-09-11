de7e9b04f709de50de338e0ac073621fd1439089
niclas <niclas@13f79535-47bb-0310-9956-ffa450edef68>
2004-09-11 20:14:40 +0000

    Added 'docs/' as a project.
    git-svn-id: https://svn.apache.org/repos/asf/avalon/trunk@45917 13f79535-47bb-0310-9956-ffa450edef68

--- central/laboratory/studio/docs/src/org/apache/metro/studio/eclipse/docs/DocsPlugin.java
+++ central/laboratory/studio/docs/src/org/apache/metro/studio/eclipse/docs/DocsPlugin.java
@@ -1,3 +1,19 @@
+/*
+ *     Copyright 2004. The Apache Software Foundation.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License. 
+ *  
+ */
 package org.apache.metro.studio.eclipse.docs;
 
 import java.util.MissingResourceException;
@@ -8,49 +24,63 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 /**
  * The main plugin class to be used in the desktop.
  */
-public class DocsPlugin extends AbstractUIPlugin {
-	//The shared instance.
-	private static DocsPlugin plugin;
-	//Resource bundle.
-	private ResourceBundle resourceBundle;
-	
-	/**
-	 * The constructor.
-	 */
-	public DocsPlugin() {
-		super();
-		plugin = this;
-		try {
-			resourceBundle   = ResourceBundle.getBundle("org.apache.metro.studio.eclipse.docs.DocsPluginResources");
-		} catch (MissingResourceException x) {
-			resourceBundle = null;
-		}
-	}
+public class DocsPlugin extends AbstractUIPlugin 
+{
+    //The shared instance.
+    private static DocsPlugin plugin;
+    
+    //Resource bundle.
+    private ResourceBundle resourceBundle;
+    
+    /**
+     * The constructor.
+     */
+    public DocsPlugin() 
+    {
+        super();
+        plugin = this;
+        
+        try 
+        {
+            resourceBundle = ResourceBundle.getBundle("org.apache.metro.studio.eclipse.docs.DocsPluginResources");
+        } catch( MissingResourceException x ) 
+        {
+            resourceBundle = null;
+        }
+    }
 
-	/**
-	 * Returns the shared instance.
-	 */
-	public static DocsPlugin getDefault() {
-		return plugin;
-	}
+    /**
+     * Returns the shared instance.
+     */
+    public static DocsPlugin getDefault() 
+    {
+        return plugin;
+    }
 
-	/**
-	 * Returns the string from the plugin's resource bundle,
-	 * or 'key' if not found.
-	 */
-	public static String getResourceString(String key) {
-		ResourceBundle bundle = DocsPlugin.getDefault().getResourceBundle();
-		try {
-			return (bundle != null) ? bundle.getString(key) : key;
-		} catch (MissingResourceException e) {
-			return key;
-		}
-	}
+    /**
+     * Returns the string from the plugin's resource bundle,
+     * or 'key' if not found.
+     */
+    public static String getResourceString(String key) 
+    {
+        ResourceBundle bundle = DocsPlugin.getDefault().getResourceBundle();
+        try 
+        {
+            if( bundle == null )
+                return key;
+                
+            return bundle.getString( key );
+        } catch( MissingResourceException e ) 
+        {
+            return key;
+        }
+    }
 
-	/**
-	 * Returns the plugin's resource bundle,
-	 */
-	public ResourceBundle getResourceBundle() {
-		return resourceBundle;
-	}
+    /**
+     * Returns the plugin's resource bundle,
+     */
+    public ResourceBundle getResourceBundle() 
+    {
+        return resourceBundle;
+    }
 }
